248961f2c0f5aa732f80f0cb422317f3b0aeb8a3
Jeroen Dekkers <jeroen@dekkers.ch>
2022-10-19 17:55:00 +0200

    Fix black

--- account/admin.py
+++ account/admin.py
@@ -15,7 +15,18 @@ class KATUserAdmin(UserAdmin):
     )
     fieldsets = (
         (None, {"fields": ("email", "password")}),
-        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
     add_fieldsets = (
