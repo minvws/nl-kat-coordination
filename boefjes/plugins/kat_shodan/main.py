8848b0a4a3d6b5d1ad7d9f3ffe45b2a06235f0aa
zcrt <115991818+zcrt@users.noreply.github.com>
2022-11-25 10:53:17 +0100

    [shodan] log no info instead of failing boefje

--- boefjes/plugins/kat_shodan/main.py
+++ boefjes/plugins/kat_shodan/main.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from typing import Tuple, Union
 
 import shodan
@@ -8,10 +9,15 @@ from boefjes.job_models import BoefjeMeta
 
 
 def run(boefje_meta: BoefjeMeta) -> Tuple[BoefjeMeta, Union[bytes, str]]:
-
     api = shodan.Shodan(getenv("SHODAN_API"))
     input_ = boefje_meta.arguments["input"]
     ip = input_["address"]
-    results = api.host(ip)
+    results = {}
+    try:
+        results = api.host(ip)
+    except shodan.APIError as e:
+        if e.args[0] != "No information available for that IP.":
+            raise
+        logging.info(e)
 
     return boefje_meta, json.dumps(results)
