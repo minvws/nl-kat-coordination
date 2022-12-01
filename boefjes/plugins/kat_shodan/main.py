b78ebc94a7be160f5bb3f2a0115d64be0db754aa
zcrt <115991818+zcrt@users.noreply.github.com>
2022-12-01 15:41:14 +0100

    :zap: do not request private IPs from Shodan

--- boefjes/plugins/kat_shodan/main.py
+++ boefjes/plugins/kat_shodan/main.py
@@ -5,6 +5,8 @@ from typing import Tuple, Union
 import shodan
 
 from os import getenv
+from ipaddress import ip_address
+
 from boefjes.job_models import BoefjeMeta
 
 
@@ -13,11 +15,15 @@ def run(boefje_meta: BoefjeMeta) -> Tuple[BoefjeMeta, Union[bytes, str]]:
     input_ = boefje_meta.arguments["input"]
     ip = input_["address"]
     results = {}
-    try:
-        results = api.host(ip)
-    except shodan.APIError as e:
-        if e.args[0] != "No information available for that IP.":
-            raise
-        logging.info(e)
+
+    if ip_address(ip).is_private:
+        logging.info("Private IP requested, I will not forward this to Shodan.")
+    else:
+        try:
+            results = api.host(ip)
+        except shodan.APIError as e:
+            if e.args[0] != "No information available for that IP.":
+                raise
+            logging.info(e)
 
     return boefje_meta, json.dumps(results)
