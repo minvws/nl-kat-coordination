8a44bcefdd8201b527aa4b06b569054f1cca9e39
Donny Peeters <donny@bitestreams.com>
2022-12-19 10:11:59 +0100

    Add missing dummy boefjes for tests

---
+++ tests/modules/dummy_bad_normalizer_dict_structure/normalize.py
@@ -0,0 +1,12 @@
+from typing import Union, Iterator
+
+from octopoes.models import OOI
+
+from boefjes.job_models import NormalizerMeta
+
+
+def run(normalizer_meta: NormalizerMeta, raw: Union[bytes, str]) -> Iterator[OOI]:
+    yield {
+        "I": "write",
+        "bad": "normalizers",
+    }
