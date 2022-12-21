7254a821bbdef34ead2a3f329f19a2f7125d3473
zcrt <115991818+zcrt@users.noreply.github.com>
2022-12-21 11:14:12 +0100

    :safety_vest: add Nmap -p validation.

--- boefjes/plugins/kat_nmap_ports/main.py
+++ boefjes/plugins/kat_nmap_ports/main.py
@@ -1,3 +1,5 @@
+import re
+
 from os import getenv
 from ipaddress import ip_address, IPv6Address
 from typing import List, Tuple, Union
@@ -15,7 +17,7 @@ def run_nmap(args: List[str]) -> str:
     return client.containers.run(NMAP_IMAGE, args, remove=True).decode()
 
 
-def build_nmap_arguments(host: str, ports: str):
+def build_nmap_arguments(host: str, ports: str) -> str:
     """Build nmap arguments from the hosts IP with the required ports."""
     ip = ip_address(host)
     args = ["nmap", "-T4", "-Pn", "-r", "-v10", "-sV", "-sS", "-sU", f"-p{ports}"]
@@ -26,13 +28,37 @@ def build_nmap_arguments(host: str, ports: str):
     return args
 
 
-def run(boefje_meta: BoefjeMeta) -> List[Tuple[set, Union[bytes, str]]]:
-    """Build the required Nmap arguments and return results to the normalizer."""
-    input_ = boefje_meta.arguments["input"]
-    host = input_["address"]
+def get_ports(
+    valid=re.compile(
+        "\\s*([TUSP]:)?(\\d+|[a-z*?]+|(\\[?\\d*-\\d*\\]?))(,(([TUSP]:)|\\s)?(\\d+|[a-z*?]+|(\\[?\\d*-\\d*\\]?)))*"
+    ),
+) -> str:
+    """Returns ports argument if valid. Double slashes are for flake8 W605.
+
+    A valid port is:
+    - a single port (set of digits) {22}
+        - Regex: \\d+
+    - a port range (optional digits separated by hypen, optionally bracketed) {[80-]}
+        - Regex: (\\[?\\d*-\\d*\\]?)
+    - a valid Nmap protocol (alfanumeric wildcarded lowercase) {https*}
+        - Regex: [a-z*?]+
+
+    There needs to be at least one valid port. Multiple ports can be separated by a comma (',').
+    A port can be preceded by 'T:', 'U:', 'S:' or 'P:' to specify a protocol
+        - Regex: ([TUSP]:)?
+    There can be spaces between the flag and its arguments.
+        - Regex: \\s*
+
+    Regex: "\\s*([TUSP]:)?(\\d+|[a-z*?]+|(\\[?\\d*-\\d*\\]?))(,(([TUSP]:)|\\s)?(\\d+|[a-z*?]+|(\\[?\\d*-\\d*\\]?)))*"
+
+    See also: https://nmap.org/book/port-scanning-options.html.
+    """
     ports = getenv("PORTS")
-    if ports is None:
-        raise ValueError('"PORTS" argument is not specified.')
-    # Maybe more validation on the ports variable is needed here but
-    # it is quite flexible according to the Nmap documentation.
-    return [(set(), run_nmap(build_nmap_arguments(host, ports)))]
+    assert ports is not None, '"PORTS" argument is not specified.'
+    assert valid.fullmatch(ports) is not None, f'Invalid PORTS argument "{ports}"'
+    return ports
+
+
+def run(boefje_meta: BoefjeMeta) -> List[Tuple[set, Union[bytes, str]]]:
+    """Build Nmap arguments and return results to normalizer."""
+    return [(set(), run_nmap(build_nmap_arguments(host=boefje_meta.arguments["input"]["address"], ports=get_ports())))]
