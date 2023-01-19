89bd3adf011821e83aa5a1d78503207f8026d60c
Roelof Korporaal <roelof.korporaal@gmail.com>
2023-01-19 12:13:04 +0100

    Make toggle target scoped so it is capable of handling multiple instances on one page (#170)

--- assets/js/checkboxToggler.js
+++ assets/js/checkboxToggler.js
@@ -1,7 +1,7 @@
 const toggle_all_btn = document.querySelectorAll(".toggle-all");
 for (var i = 0; i < toggle_all_btn.length; i++) {
-  var toggle_target = toggle_all_btn[i].dataset.toggleTarget;
-  toggle_all_btn[i].addEventListener("click", function () {
+  toggle_all_btn[i].addEventListener("click", function (event) {
+    var toggle_target = event.target.dataset.toggleTarget;
     toggleCheckboxes(toggle_target, !this.classList.contains('toggle-on'));
     this.classList.toggle('toggle-on');
   })
