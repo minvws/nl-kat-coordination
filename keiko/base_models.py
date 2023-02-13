aabc1f64231739e02c515a350d05d628996a82d6
Jesse Lisser <Lisser@users.noreply.github.com>
2023-02-13 11:10:34 +0100

    Version in pdf (#26)

--- keiko/base_models.py
+++ keiko/base_models.py
@@ -2,11 +2,15 @@
 
 from pydantic import BaseModel, Extra
 
+from keiko.version import __version__
+
 # pylint: disable=too-few-public-methods
 
 
 class DataShapeBase(BaseModel):
     """Base model for data shapes."""
+
+    keiko_version: str = __version__
 
     class Config:
         """Pydantic config for DataShapeBase."""
