f0d8920dfa7d1ffefaa6a460e63663bc26f093e7
JP Bruins Slot <jpbruinsslot@gmail.com>
2023-02-13 10:22:18 +0100

    Add tests for api server remove orgs (#68)
    * Add test cases
    
    * Add more assertions

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -2,6 +2,7 @@ import unittest
 from unittest import mock
 
 import scheduler
+from fastapi.testclient import TestClient
 from scheduler import config, models, repositories
 from tests.factories import OrganisationFactory
 
@@ -27,32 +28,61 @@ class AppTestCase(unittest.TestCase):
 
         self.app = scheduler.App(self.mock_ctx)
 
+        self.client = TestClient(self.app.server.api)
+
     @mock.patch("scheduler.context.AppContext.services.katalogus.get_organisations")
     @mock.patch("scheduler.context.AppContext.services.katalogus.get_organisation")
     def test_monitor_orgs_add(self, mock_get_organisation, mock_get_organisations):
         """Test that when a new organisation is added, a new scheduler is created"""
+        # Arrange
         mock_get_organisations.return_value = [self.organisation]
         mock_get_organisation.return_value = self.organisation
 
-        # Two schedulers should have been created
+        # Act
         self.app.monitor_organisations()
+
+        # Assert: two schedulers should have been created
         self.assertEqual(2, len(self.app.schedulers.keys()))
+        self.assertEqual(2, len(self.app.server.schedulers.keys()))
+
+        response = self.client.get("/schedulers")
+        self.assertEqual(2, len(response.json()))
 
     @mock.patch("scheduler.context.AppContext.services.katalogus.get_organisations")
     @mock.patch("scheduler.context.AppContext.services.katalogus.get_organisation")
     def test_monitor_orgs_remove(self, mock_get_organisation, mock_get_organisations):
         """Test that when an organisation is removed, the scheduler is removed"""
-
+        # Arrange
         mock_get_organisations.return_value = [self.organisation]
         mock_get_organisation.return_value = self.organisation
 
-        # Two schedulers should have been created
+        # Act
         self.app.monitor_organisations()
+
+        # Assert: two schedulers should have been created
         self.assertEqual(2, len(self.app.schedulers.keys()))
 
+        response = self.client.get("/schedulers")
+        self.assertEqual(2, len(response.json()))
+
+        response = self.client.get("/queues")
+        self.assertEqual(2, len(response.json()))
+
+        # Arrange
         mock_get_organisations.return_value = []
         mock_get_organisation.return_value = None
 
-        # Two schedulers should have been removed
+        # Act
         self.app.monitor_organisations()
+
+        # Assert
         self.assertEqual(0, len(self.app.schedulers.keys()))
+        self.assertEqual(0, len(self.app.server.schedulers.keys()))
+
+        response = self.client.get("/schedulers")
+        self.assertEqual(0, len(response.json()))
+        self.assertEqual([], response.json())
+
+        response = self.client.get("/queues")
+        self.assertEqual(0, len(response.json()))
+        self.assertEqual([], response.json())
