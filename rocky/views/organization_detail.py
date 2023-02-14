75df4aa5ca84e85c65e37dcdb5392dbf07fbe304
Roelof Korporaal <roelof.korporaal@gmail.com>
2023-02-14 17:18:09 +0100

    Add missing indemnification error message on organization detail page (#249)

--- rocky/views/organization_detail.py
+++ rocky/views/organization_detail.py
@@ -1,3 +1,6 @@
+from django.contrib import messages
+from account.mixins import OrganizationView
+from django.utils.translation import gettext_lazy as _
 from django_otp.decorators import otp_required
 from two_factor.views.utils import class_view_decorator
 
@@ -5,5 +8,10 @@ from rocky.views.organization_member_list import OrganizationMemberListView
 
 
 @class_view_decorator(otp_required)
-class OrganizationDetailView(OrganizationMemberListView):
+class OrganizationDetailView(OrganizationMemberListView, OrganizationView):
     template_name = "organizations/organization_detail.html"
+
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+        if not self.indemnification_present:
+            messages.add_message(self.request, messages.ERROR, _("Indemnification is not set for this organization."))
