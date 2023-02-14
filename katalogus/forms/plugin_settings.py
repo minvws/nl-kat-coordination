1635c6ba8213a38ec658a84dbc091101c60bfc1b
Jeroen Dekkers <jeroen@dekkers.ch>
2023-02-14 22:16:26 +0100

    Fix flake8 error (#251)

--- katalogus/forms/plugin_settings.py
+++ katalogus/forms/plugin_settings.py
@@ -40,7 +40,8 @@ class PluginSchemaForm(forms.Form):
 
 
 class PluginSettingAddEditForm(forms.Form):
-    """Form for adding a single setting or multiple settings, use setting name to populate schema field propertis into form field."""
+    """Form for adding a single setting or multiple settings, use setting name
+    to populate schema field propertis into form field."""
 
     error_messages = {
         "required": _("This field is required."),
