0434397f78102b92b476ff290bc035ada11d9f3c
Patrick Darwinkel <patrick@code050.nl>
2023-02-20 11:43:58 +0100

    Update and remove more files

--- octopoes/.ci/health_check.py
+++ octopoes/.ci/health_check.py
@@ -1,7 +1,15 @@
+"""Health check script for the integration tests."""
+
 import sys
 import urllib.request
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    if response.status != 200:
-        exit(1)
+
+def check_health(url: str) -> None:
+    """Check if the server is up and running."""
+    with urllib.request.urlopen(url) as response:
+        if response.status != 200:
+            sys.exit(1)
+
+
+if __name__ == "__main__":
+    check_health(sys.argv[1])
