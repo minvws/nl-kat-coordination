039cd1e3236d05c5248456b3afb9f9493c720778
Patrick Darwinkel <patrick@code050.nl>
2023-02-21 16:12:21 +0100

    Initial migration from `nl-kat-rocky`

---
+++ .ci/init-user-db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Reference: https://hub.docker.com/_/postgres
+
+set -e
+
+psql -v ON_ERROR_STOP=1 <<-EOSQL
+	CREATE USER ${!APP_DB_USER} WITH ENCRYPTED PASSWORD '${!APP_DB_PASSWORD}';
+	CREATE DATABASE ${!APP_DB};
+	ALTER DATABASE ${!APP_DB} OWNER TO ${!APP_DB_USER};
+EOSQL
