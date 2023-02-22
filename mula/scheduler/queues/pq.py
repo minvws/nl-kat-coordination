5f018f3c18964701a28b078e7e38f1f41c1a37c4
JP Bruins Slot <jpbruinsslot@gmail.com>
2023-02-22 12:11:30 +0100

    Format with black

--- mula/scheduler/queues/pq.py
+++ mula/scheduler/queues/pq.py
@@ -8,9 +8,13 @@ from typing import Any, Dict, List, Optional
 import pydantic
 from scheduler import models, repositories
 
-from .errors import (InvalidPrioritizedItemError, NotAllowedError,
-                     PrioritizedItemNotFoundError, QueueEmptyError,
-                     QueueFullError)
+from .errors import (
+    InvalidPrioritizedItemError,
+    NotAllowedError,
+    PrioritizedItemNotFoundError,
+    QueueEmptyError,
+    QueueFullError,
+)
 
 
 class PriorityQueue(abc.ABC):
