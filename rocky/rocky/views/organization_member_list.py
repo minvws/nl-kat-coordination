b192db6ef422eb1670054782bc7659ed6a7b144b
TwistMeister <roelof.korporaal@gmail.com>
2023-03-01 12:22:30 +0100

    Change "blocked" filter to "status" filter

--- rocky/rocky/views/organization_member_list.py
+++ rocky/rocky/views/organization_member_list.py
@@ -30,24 +30,20 @@ class OrganizationMemberListView(
 
     def get_queryset(self):
         queryset = self.model.objects.filter(organization=self.organization)
-        if "blocked_status_filter" in self.request.GET:
-            blocked_status_filter = self.request.GET.getlist("blocked_status_filter", [])
-            queryset = self.filter_queryset(queryset, blocked_status_filter)
+        if "client_status" in self.request.GET:
+            status_filter = self.request.GET.getlist("client_status", [])
+            queryset = self.filter_queryset(queryset, status_filter)
         for member in queryset:
             if member.status == OrganizationMember.STATUSES.BLOCKED:
                 member.blocked = True
         return queryset
 
     def filter_queryset(self, queryset, blocked_status_filter):
-        result = []
-        if "blocked" in blocked_status_filter:
-            result.extend([member for member in queryset if member.status == OrganizationMember.STATUSES.BLOCKED])
-        if "unblocked" in blocked_status_filter:
-            result.extend([member for member in queryset if not member.status == OrganizationMember.STATUSES.BLOCKED])
-        return result
+        return [member for member in queryset if member.status in blocked_status_filter]
 
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
+        self.filters_active = self.get_filters_active()
 
     def post(self, request, *args, **kwargs):
         if "action" not in self.request.POST:
@@ -81,23 +77,20 @@ class OrganizationMemberListView(
         except RequestException as exception:
             messages.add_message(self.request, messages.ERROR, f"{action} failed: '{exception}'")
 
-    def get_blocked_filters(self):
+    def get_filters_active(self):
+        return self.request.GET.getlist("client_status", [])
+
+    def get_checkbox_filters(self):
         return [
             {
-                "label": "Suspended",
-                "value": "blocked",
-                "checked": "blocked_status_filter" not in self.request.GET
-                or "blocked" in self.request.GET.getlist("blocked_status_filter", []),
-            },
-            {
-                "label": "Not suspended",
-                "value": "unblocked",
-                "checked": "blocked_status_filter" not in self.request.GET
-                or "unblocked" in self.request.GET.getlist("blocked_status_filter", []),
-            },
+                "label": choice[0],
+                "value": choice[1],
+                "checked": not self.filters_active or choice[0] in self.filters_active,
+            }
+            for choice in OrganizationMember.STATUSES.choices
         ]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["blocked_filters"] = self.get_blocked_filters()
+        context["checkbox_filters"] = self.get_checkbox_filters()
         return context
