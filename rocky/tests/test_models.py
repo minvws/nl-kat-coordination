bd458f7459846e2bb7d8995e8f6642d4868ac71c
Jesse Lisser <jesselisser@gmail.com>
2023-03-22 10:10:52 +0100

    refactor(organization lists): add test for user organizations cache

--- rocky/tests/test_models.py
+++ rocky/tests/test_models.py
@@ -5,3 +5,9 @@ def test_organizationtag_cssclass():
     tag = OrganizationTag(color="blue-dark", border_type="dashed")
 
     assert tag.css_class == "tags-blue-dark dashed"
+
+
+def test_user_organization_cache(my_user, django_assert_num_queries):
+    with django_assert_num_queries(1):
+        assert len(my_user.organizations) == 1
+        assert len(my_user.organizations) == 1
