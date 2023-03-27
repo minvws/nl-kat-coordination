22492a247e304beb74fd3deae7274fe353f24064
Riccardo ten Cate <r.tencate77@gmail.com>
2023-03-27 11:34:30 +0200

    Hostname to HostnameHTTPURL

--- boefjes/boefjes/plugins/kat_nuclei/main.py
+++ boefjes/boefjes/plugins/kat_nuclei/main.py
@@ -9,8 +9,13 @@ NUCLEI_IMAGE = "projectdiscovery/nuclei:latest"
 
 def run(boefje_meta: BoefjeMeta) -> List[Tuple[set, Union[bytes, str]]]:
     client = docker.from_env()
-    input_ = boefje_meta.arguments["input"]
-    url = input_["name"]
+    hostnameHTTPUrl = boefje_meta.arguments["input"]
+    
+    hostname = hostnameHTTPUrl["netloc"]["name"]
+    port = hostnameHTTPUrl["port"]
+
+    url = f"{hostname}:{port}"
+   
 
     output = client.containers.run(
         NUCLEI_IMAGE,
