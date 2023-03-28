e4e82ac24f1d01bb64cbe8aa135af8b76ad49816
Riccardo ten Cate <r.tencate77@gmail.com>
2023-03-28 16:49:52 +0200

    HostnameHTTPURL and Hostname input objects

--- boefjes/boefjes/plugins/kat_nuclei/main.py
+++ boefjes/boefjes/plugins/kat_nuclei/main.py
@@ -1,22 +1,27 @@
 from typing import Tuple, Union, List
-
 import docker
-
 from boefjes.job_models import BoefjeMeta
+
 
 NUCLEI_IMAGE = "projectdiscovery/nuclei:latest"
 
 
+def verify_hostname_meta(input):
+    # if the input object is HostnameHTTPURL then the hostname is located in netloc
+    if "netloc" in input and "name" in input["netloc"]:
+        netloc_name = input["netloc"]["name"]
+        port = input.get("port")
+        return f"{netloc_name}:{port}"
+    else:
+        # otherwise the Hostname input object is used
+        return input["name"]
+
+
 def run(boefje_meta: BoefjeMeta) -> List[Tuple[set, Union[bytes, str]]]:
     client = docker.from_env()
-    hostnameHTTPUrl = boefje_meta.arguments["input"]
-    
-    hostname = hostnameHTTPUrl["netloc"]["name"]
-    port = hostnameHTTPUrl["port"]
 
-    url = f"{hostname}:{port}"
-   
-
+    # Checks if the url is of object HostnameHTTPURL or Hostname
+    url = verify_hostname_meta(boefje_meta.arguments["input"])
     output = client.containers.run(
         NUCLEI_IMAGE,
         ["-t", "/root/nuclei-templates/cves/", "-u", url, "-json"],
