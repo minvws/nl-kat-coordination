df72b9819c3432a827e789f428d3fb35e4ebdbf3
Jeroen Dekkers <jeroen@dekkers.ch>
2023-04-05 23:10:18 +0200

    Add missing migration and unit test for missing migrations
    Add missing migration in tools
    
    Add a unit test that checks for missing migration
    
    Also bump django-password-validators tot 1.7.1 which has my fix for
    DEFAULT_AUTO_FIELD so Django doesn't want to create a migration with a
    different auto field.

---
+++ rocky/tests/test_migrations.py
@@ -0,0 +1,15 @@
+from io import StringIO
+
+import pytest
+
+from django.core.management import call_command
+
+pytestmark = pytest.mark.django_db
+
+
+def test_for_missing_migrations():
+    output = StringIO()
+    call_command("makemigrations", no_input=True, dry_run=True, stdout=output)
+    lines = output.getvalue().strip().splitlines()
+    assert len(lines) == 3
+    assert lines[0] == "Migrations for 'two_factor':"
