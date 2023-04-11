a3b69e11dcda5faaa21c815e1552ce10816d8e6c
Donny Peeters <46660228+Donnype@users.noreply.github.com>
2023-04-11 21:37:50 +0200

    Accept "No changes detected" as well in migrations unit test (#710)

--- rocky/tests/test_migrations.py
+++ rocky/tests/test_migrations.py
@@ -11,5 +11,12 @@ def test_for_missing_migrations():
     output = StringIO()
     call_command("makemigrations", no_input=True, dry_run=True, stdout=output)
     lines = output.getvalue().strip().splitlines()
-    assert len(lines) == 3
-    assert lines[0] == "Migrations for 'two_factor':"
+
+    # The outcome of makemigrations here depends on having run `makemigrations` in your local environment, which creates
+    # a missing migration for two_factor (also see https://github.com/jazzband/django-two-factor-auth/issues/611).
+
+    if len(lines) == 1:
+        assert lines[0] == "No changes detected"
+    else:
+        assert len(lines) == 3
+        assert lines[0] == "Migrations for 'two_factor':"
