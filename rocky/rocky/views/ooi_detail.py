e1de9dd96547fe4328cc1043f837dcb6b7fe2688
Jesse Lisser <jesselisser@gmail.com>
2023-04-12 12:14:33 +0200

    revert rocky rendering of explanation

--- rocky/rocky/views/ooi_detail.py
+++ rocky/rocky/views/ooi_detail.py
@@ -181,6 +181,4 @@ class OOIDetailView(
             "scan_history_page",
         ]
 
-        context["explanation"] = self.octopoes_api_connector.get_explanation(self.ooi.reference)
-
         return context
