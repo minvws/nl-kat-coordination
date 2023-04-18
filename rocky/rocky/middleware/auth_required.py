3255a972ee4db1bc3f74ccd9f0ffb01bf0d1e8bd
Jeroen Dekkers <jeroen@dekkers.ch>
2023-04-18 22:27:40 +0200

    Fix tests

--- rocky/rocky/middleware/auth_required.py
+++ rocky/rocky/middleware/auth_required.py
@@ -8,18 +8,26 @@ def AuthRequiredMiddleware(get_response):
         email_recovery_path = reverse("recover_email")
         password_recovery_path = reverse("password_reset")
         home_path = reverse("landing_page")
-        lang_path = "/i18n/setlang/"
+        lang_path = reverse("set_language")
         privacy_statement = reverse("privacy_statement")
 
         if not request.user.is_authenticated:
-            if not request.path.startswith("/account/reset/") and request.path not in (
-                "/",
-                home_path,
-                login_path,
-                lang_path,
-                email_recovery_path,
-                password_recovery_path,
-                privacy_statement,
+            if (
+                not request.path.startswith("/account/reset/")
+                # There won't be a request.user if auth tokens are used, but
+                # Django REST framework will make sure that there is an
+                # authenticated user with out DEFAULT_PERMISSION_CLASSES setting
+                and not request.path.startswith("/api/")
+                and request.path
+                not in (
+                    "/",
+                    home_path,
+                    login_path,
+                    lang_path,
+                    email_recovery_path,
+                    password_recovery_path,
+                    privacy_statement,
+                )
             ):
                 return redirect(login_path)
 
