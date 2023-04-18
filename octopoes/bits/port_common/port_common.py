16a40c218d49f6b2eaa819e4ff79e1f7d1ca8321
Jeroen Dekkers <jeroen@dekkers.ch>
2023-04-18 15:22:01 +0200

    Differentiate between TCP and UDP in port bits
    Also add port 465 and 587 SMTP ports to common ports
    
    Fixes #741

--- octopoes/bits/port_common/port_common.py
+++ octopoes/bits/port_common/port_common.py
@@ -2,9 +2,24 @@ from typing import Iterator, List
 
 from octopoes.models import OOI
 from octopoes.models.ooi.findings import Finding, KATFindingType
-from octopoes.models.ooi.network import IPPort
+from octopoes.models.ooi.network import IPPort, Protocol
 
-COMMON_TCP_PORTS = [25, 53, 110, 143, 993, 995, 80, 443]
+COMMON_TCP_PORTS = [
+    25,  # SMTP
+    53,  # DNS
+    80,  # HTTP
+    110,  # POP3
+    143,  # IMAP
+    443,  # HTTPS
+    465,  # SMTPS
+    587,  # SMTP (message submmission)
+    993,  # IMAPS
+    995,  # POP3S
+]
+
+COMMON_UDP_PORTS = [
+    53,  # DNS
+]
 
 
 def run(
@@ -12,11 +27,14 @@ def run(
     additional_oois: List,
 ) -> Iterator[OOI]:
     port = input_ooi.port
-    if port in COMMON_TCP_PORTS:
+    protocol = input_ooi.protocol
+    if (protocol == Protocol.TCP and port in COMMON_TCP_PORTS) or (
+        protocol == Protocol.UDP and port in COMMON_UDP_PORTS
+    ):
         kat = KATFindingType(id="KAT-OPEN-COMMON-PORT")
         yield kat
         yield Finding(
             finding_type=kat.reference,
             ooi=input_ooi.reference,
-            description=f"Port {port} is a common port and found to be open.",
+            description=f"Port {port}/{protocol.value} is a common port and found to be open.",
         )
