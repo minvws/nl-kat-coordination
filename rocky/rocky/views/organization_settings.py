4986d5d99c7540b5352c0850bbe24e5cdbb84a34
Rieven <rsnmartis@gmail.com>
2023-04-20 14:08:06 +0200

    Fix some views and added tests

--- rocky/rocky/views/organization_settings.py
+++ rocky/rocky/views/organization_settings.py
@@ -1,9 +1,11 @@
 from django.views.generic import TemplateView
 from django_otp.decorators import otp_required
 from tools.view_helpers import OrganizationDetailBreadcrumbsMixin
+from account.mixins import RockyPermissionRequiredMixin
 from two_factor.views.utils import class_view_decorator
 
 
 @class_view_decorator(otp_required)
-class OrganizationSettingsView(OrganizationDetailBreadcrumbsMixin, TemplateView):
+class OrganizationSettingsView(RockyPermissionRequiredMixin, OrganizationDetailBreadcrumbsMixin, TemplateView):
     template_name = "organizations/organization_settings.html"
+    permission_required = "tools.view_organization"
