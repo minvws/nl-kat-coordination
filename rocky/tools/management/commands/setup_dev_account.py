06d74026eb3b2af7fd47b99a8e4c9c3122c798b6
Rieven <rsnmartis@gmail.com>
2023-04-21 15:24:06 +0200

    Add delete oois perm

--- rocky/tools/management/commands/setup_dev_account.py
+++ rocky/tools/management/commands/setup_dev_account.py
@@ -47,16 +47,13 @@ class Command(BaseCommand):
                 "add_organizationmember",
                 "change_organization",
                 "change_organizationmember",
+                "can_delete_oois",
             ]
         )
         self.group_admin.permissions.set(admin_permissions)
 
         redteam_permissions = self.get_permissions(
-            [
-                "can_scan_organization",
-                "can_enable_disable_boefje",
-                "can_set_clearance_level",
-            ]
+            ["can_scan_organization", "can_enable_disable_boefje", "can_set_clearance_level", "can_delete_oois"]
         )
         self.group_redteam.permissions.set(redteam_permissions)
         logging.info(Fore.GREEN + " ROCKY HAS BEEN SETUP SUCCESSFULLY")
