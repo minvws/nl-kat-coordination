113a210b2e6bede508f0d042d81738cbc8e2fb47
Rieven <rsnmartis@gmail.com>
2023-04-21 15:36:45 +0200

    Fix migration file

--- rocky/tools/migrations/0033_alter_organization_options.py
+++ rocky/tools/migrations/0033_alter_organization_options.py
@@ -4,14 +4,21 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('tools', '0032_alter_organizationmember_user'),
+        ("tools", "0032_alter_organizationmember_user"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='organization',
-            options={'permissions': (('can_switch_organization', 'Can switch organization'), ('can_scan_organization', 'Can scan organization'), ('can_enable_disable_boefje', 'Can enable or disable boefje'), ('can_set_clearance_level', 'Can set clearance level'), ('can_delete_oois', 'Can delete oois'))},
+            name="organization",
+            options={
+                "permissions": (
+                    ("can_switch_organization", "Can switch organization"),
+                    ("can_scan_organization", "Can scan organization"),
+                    ("can_enable_disable_boefje", "Can enable or disable boefje"),
+                    ("can_set_clearance_level", "Can set clearance level"),
+                    ("can_delete_oois", "Can delete oois"),
+                )
+            },
         ),
     ]
