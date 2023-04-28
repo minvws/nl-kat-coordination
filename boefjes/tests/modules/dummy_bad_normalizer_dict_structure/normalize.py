0292241fb490de47ad88f189da8bc69ebfa1f48a
kshitiz305 <kshitiz305@live.com>
2023-04-28 18:32:52 +0530

    Normalizers should be defined as generators
    Signed-off-by: kshitiz305 <kshitiz305@live.com>

--- boefjes/tests/modules/dummy_bad_normalizer_dict_structure/normalize.py
+++ boefjes/tests/modules/dummy_bad_normalizer_dict_structure/normalize.py
@@ -1,10 +1,10 @@
-from typing import Iterator, Union
+from typing import Iterable, Union
 
 from boefjes.job_models import NormalizerMeta
 from octopoes.models import OOI
 
 
-def run(normalizer_meta: NormalizerMeta, raw: Union[bytes, str]) -> Iterator[OOI]:
+def run(normalizer_meta: NormalizerMeta, raw: Union[bytes, str]) -> Iterable[OOI]:
     yield {
         "I": "write",
         "bad": "normalizers",
