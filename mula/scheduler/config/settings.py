97b6774db1c1a7ce30b2bc4c5d89c827bcf13a02
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-04-28 13:14:38 +0200

    Add OpenTelemetry tracing to Mula

--- mula/scheduler/config/settings.py
+++ mula/scheduler/config/settings.py
@@ -31,6 +31,7 @@ class Settings(BaseSettings):
     host_mutation: str = Field(..., env="SCHEDULER_RABBITMQ_DSN")
     host_raw_data: str = Field(..., env="SCHEDULER_RABBITMQ_DSN")
     host_normalizer_meta: str = Field(..., env="SCHEDULER_RABBITMQ_DSN")
+    span_export_grpc_endpoint: str = Field(..., env="SPAN_EXPORT_GRPC_ENDPOINT")
 
     # Queue settings (0 is infinite)
     pq_maxsize: int = Field(1000, env="SCHEDULER_PQ_MAXSIZE")
