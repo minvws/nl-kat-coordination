0e322a3601f66f26cb16ef8f8b9c1f3821ab198a
Donny Peeters <donny@bitestreams.com>
2023-05-01 15:28:30 +0200

    Add query objects to generate XTDB queries

---
+++ octopoes/octopoes/xtdb/query.py
@@ -0,0 +1,87 @@
+from dataclasses import dataclass, field
+from typing import List, Type, Union
+
+from octopoes.models import OOI
+from octopoes.models.types import get_abstract_types, get_relations
+
+
+class InvalidField(ValueError):
+    pass
+
+
+@dataclass
+class Query:
+    result_ooi_type: Type[OOI]
+
+    _where_clauses: List[str] = field(default_factory=list)
+
+    def where(self, ooi_type: Type[OOI], **kwargs) -> "Query":
+        for field_name, value in kwargs.items():
+            self._where_field_is(ooi_type, field_name, value)
+
+        return self
+
+    def format(self) -> str:
+        return "\n" + self._compile(separator="\n    ") + "\n"
+
+    def _where_field_is(self, ooi_type: Type[OOI], field_name: str, value: Union[Type[OOI], str]) -> None:
+        abstract_types = get_abstract_types()
+
+        if field_name not in ooi_type.__fields__:
+            raise InvalidField(f'"{field_name}" is not a field of {ooi_type.get_object_type()}')
+
+        if isinstance(value, str):
+            self._add_where_statement(ooi_type, field_name, f'"{value}"')
+            return
+
+        if not isinstance(value, type):
+            raise InvalidField(f"value '{value}' should be a string or an OOI Type")
+
+        if not issubclass(value, OOI):
+            raise InvalidField(f"{value} is not an OOI")
+
+        if field_name not in get_relations(ooi_type):
+            raise InvalidField(f'"{field_name}" is not a relation of {ooi_type.get_object_type()}')
+
+        if ooi_type in abstract_types:
+            self._add_or_statement(ooi_type, field_name, value.get_object_type())
+            return
+
+        self._add_where_statement(ooi_type, field_name, value.get_object_type())
+
+    def _add_where_statement(self, ooi_type: Type[OOI], field_name: str, to_alias: str) -> None:
+        from_alias = ooi_type.get_object_type()
+
+        self._where_clauses.append(self._relationship(from_alias, from_alias, field_name, to_alias))
+
+    def _add_or_statement(self, ooi_type: Type[OOI], field_name: str, to_alias: str) -> None:
+        self._where_clauses.append(
+            self._or_statement(
+                ooi_type.get_object_type(),
+                ooi_type.__subclasses__(),
+                field_name,
+                to_alias,
+            )
+        )
+
+    def _or_statement(self, from_alias: str, concrete_types: List[Type[OOI]], field_name: str, to_alias: str) -> str:
+        relationships = [
+            self._relationship(from_alias, concrete_type.get_object_type(), field_name, to_alias)
+            for concrete_type in concrete_types
+        ]
+
+        return f"(or {' '.join(relationships)} )"
+
+    def _relationship(self, from_alias: str, field_type: str, field_name: str, to_alias: str) -> str:
+        return f"[ {from_alias} :{field_type}/{field_name} {to_alias} ]"
+
+    def _compile_where_clauses(self, *, separator=" ") -> str:
+        return separator + separator.join(self._where_clauses)
+
+    def _compile(self, *, separator=" ") -> str:
+        where_clauses = self._compile_where_clauses(separator=separator)
+
+        return f"{{:query {{:find [(pull {self.result_ooi_type.get_object_type()} [*])] :where [{where_clauses}]}}}}"
+
+    def __str__(self) -> str:
+        return self._compile()
---
+++ octopoes/tests/test_query.py
@@ -0,0 +1,79 @@
+import pytest
+
+from octopoes.models.ooi.findings import Finding
+from octopoes.models.ooi.network import IPAddress, IPPort, Network
+from octopoes.xtdb.query import InvalidField, Query
+
+
+def test_basic_field_where_clause():
+    assert (
+        str(Query(Network).where(Network, name="test"))
+        == '{:query {:find [(pull Network [*])] :where [ [ Network :Network/name "test" ]]}}'
+    )
+
+
+def test_reference_field_where_clause():
+    assert (
+        str(Query(Network).where(Finding, ooi=Network))
+        == "{:query {:find [(pull Network [*])] :where [ [ Finding :Finding/ooi Network ]]}}"
+    )
+
+
+def test_invalid_fields_name():
+    with pytest.raises(InvalidField) as ctx:
+        Query(Network).where(Network, ooi=Finding)
+
+    assert ctx.exconly() == 'octopoes.xtdb.query.InvalidField: "ooi" is not a field of Network'
+
+    with pytest.raises(InvalidField) as ctx:
+        Query(Network).where(Network, abc="def")
+
+    assert ctx.exconly() == 'octopoes.xtdb.query.InvalidField: "abc" is not a field of Network'
+
+
+def test_invalid_field_types():
+    with pytest.raises(InvalidField) as ctx:
+        Query(Network).where(Finding, ooi=3)
+
+    assert ctx.exconly() == "octopoes.xtdb.query.InvalidField: value '3' should be a string or an OOI Type"
+
+    with pytest.raises(InvalidField) as ctx:
+        Query(Network).where(Finding, ooi=InvalidField)
+
+    assert ctx.exconly() == "octopoes.xtdb.query.InvalidField: <class 'octopoes.xtdb.query.InvalidField'> is not an OOI"
+
+    with pytest.raises(InvalidField) as ctx:
+        Query(Network).where(Network, name=Network)
+
+    assert ctx.exconly() == 'octopoes.xtdb.query.InvalidField: "name" is not a relation of Network'
+
+
+def test_allow_string_for_foreign_keys():
+    assert (
+        str(Query(Network).where(Finding, ooi="Network|internet"))
+        == "{:query {:find [(pull Network [*])] :where [ [ Finding :Finding/ooi \"Network|internet\" ]]}}"
+    )
+
+
+def test_big_multiple_direction_query():
+    query = (
+        Query(Finding)
+        .where(Finding, ooi=Network, finding_type="KATFindingType|KAT-500")
+        .where(Finding, ooi=Network)
+        .where(IPAddress, network=Network)
+        .where(IPPort, address=IPAddress)
+        .where(IPPort, primary_key="IPPort|internet|2400:cb00:2049:1::a29f:1b48|tcp|23")
+    )
+
+    assert (
+        query.format()
+        == """
+{:query {:find [(pull Finding [*])] :where [
+    [ Finding :Finding/ooi Network ]
+    [ Finding :Finding/finding_type "KATFindingType|KAT-500" ]
+    [ Finding :Finding/ooi Network ]
+    (or [ IPAddress :IPAddressV4/network Network ] [ IPAddress :IPAddressV6/network Network ] )
+    [ IPPort :IPPort/address IPAddress ]
+    [ IPPort :IPPort/primary_key "IPPort|internet|2400:cb00:2049:1::a29f:1b48|tcp|23" ]]}}
+"""
+    )
