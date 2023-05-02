4e2ebb6062aea7e46001fc81c3105e0e4bdfde30
Noam Blitz <noamblitz@gmail.com>
2023-05-02 13:43:50 +0200

    Add findingtype hydration boefje and normalizer

---
+++ boefjes/boefjes/plugins/kat_kat_finding_types/normalize.py
@@ -0,0 +1,23 @@
+import json
+from typing import Iterable, Union
+
+from boefjes.job_models import NormalizerMeta
+from octopoes.models import OOI, Reference
+from octopoes.models.ooi.findings import Finding, KATFindingType
+
+
+def run(normalizer_meta: NormalizerMeta, raw: Union[bytes, str]) -> Iterable[OOI]:
+    boefje_meta = normalizer_meta.raw_data.boefje_meta
+    data = json.loads(raw.decode())
+
+    pk = boefje_meta.input_ooi
+    website_reference = Reference.from_str(pk)
+
+    if not data["green"]:
+        ft = KATFindingType(id="KAT-NO-GREEN-HOSTING")
+        yield ft
+        yield Finding(
+            finding_type=ft.reference,
+            ooi=website_reference,
+            description="This server is not running in a 'green' datacenter according to the Green Web Foundation.",
+        )
