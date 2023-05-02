da765ede9f6252edb47e16c07e1ee6be924902b4
Noam Blitz <noamblitz@gmail.com>
2023-05-02 15:47:13 +0200

    added bit recalculation to rocky

--- rocky/tools/management/commands/setup_dev_account.py
+++ rocky/tools/management/commands/setup_dev_account.py
@@ -46,6 +46,7 @@ class Command(BaseCommand):
                 "add_organizationmember",
                 "change_organization",
                 "change_organizationmember",
+                "recalculate_bits",
             ]
         )
         self.group_admin.permissions.set(admin_permissions)
