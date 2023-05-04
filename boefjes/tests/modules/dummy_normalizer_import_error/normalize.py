824e9112b013aec4f54e5d3a9903e73039891a3d
Donny Peeters <donny@bitestreams.com>
2023-05-04 11:09:29 +0200

    Remove redundant function block for tests

--- boefjes/tests/modules/dummy_normalizer_import_error/normalize.py
+++ boefjes/tests/modules/dummy_normalizer_import_error/normalize.py
@@ -1,7 +1,1 @@
 from octopoes.bad import wrong  # noqa
-
-
-def run(normalizer_meta: NormalizerMeta, raw: Union[bytes, str]) -> Iterator[OOI]:  # noqa
-    network = Network(name=raw.decode())  # noqa
-
-    yield network  # noqa
