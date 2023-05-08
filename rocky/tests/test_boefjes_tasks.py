77c93ce4491bcfff9fcd784e7f85abc496f240ad
Jeroen Dekkers <jeroen@dekkers.ch>
2023-05-08 16:34:33 +0200

    Fix tests

--- rocky/tests/test_boefjes_tasks.py
+++ rocky/tests/test_boefjes_tasks.py
@@ -24,7 +24,6 @@ def test_boefjes_tasks(rf, client_member, mocker, lazy_task_list_empty):
                 status=None,
                 min_created_at=None,
                 max_created_at=None,
-                input_ooi=None,
             )
         ]
     )
@@ -48,7 +47,6 @@ def test_tasks_view_simple(rf, client_member, mocker, lazy_task_list_with_boefje
                 status=None,
                 min_created_at=None,
                 max_created_at=None,
-                input_ooi=None,
             )
         ]
     )
