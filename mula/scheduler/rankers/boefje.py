212111cfe85b55d8ae288a772a7652d03b66f739
JP Bruins Slot <jpbruinsslot@gmail.com>
2023-05-17 09:07:16 +0200

    Fix ranking

--- mula/scheduler/rankers/boefje.py
+++ mula/scheduler/rankers/boefje.py
@@ -1,4 +1,3 @@
-import math
 import random
 from datetime import datetime, timedelta, timezone
 from typing import Any
@@ -11,49 +10,35 @@ class BoefjeRanker(Ranker):
     MAX_DAYS = 7
 
     def rank(self, obj: Any) -> int:
-        """
-        When a task hasn't run in a while it needs to be run sooner. We want
+        """When a task hasn't run in a while it needs to be run sooner. We want
         a task to get a priority of 3 when `max_days` days are gone by, and
-        thus it should have a lower bound of 3 for every day past those
-        `max_days`. This is because priority 1 is reserved for rocky tasks,
-        priority 2 is reserved for tasks that have not run yet for created
-        ooi's.
+        thus it should have a lower bound of 3 for every task that has run past
+        those`max_days`.
 
-        So everything before those `max_days` to the grace period are given a
-        priority from `max_priority` of the queue, up from 3. We calculate
-        the priority starting from the `grace_period`, so we subtract that
-        from our time delta between now and the `last_run` of the object
-        onwards, since it everything before the `grace_period` won't be
-        regarded anyway. So from that initial point (the end of the
-        `grace_period`, x=0) we want to place the priority the lowest, thus
-        the last place in the queue) which is our `maxsize`.
+        3 has been chosen as a lower bound because new tasks that have not yet
+        run before will get the priority of 2. And tasks created by the user
+        (from rocky) will get a priority of 1.
 
-        Since we want to have a lower bound of a priority of 3, we will use
-        an exponential decay function in decreasing form.
+        Before the end of those `max_days` we want to prioritize a task within
+        a range from 3 to the maximum value of `max_priority`.
         """
+        max_priority = self.MAX_PRIORITY
+        max_days_in_seconds = self.MAX_DAYS * (60 * 60 * 24)
+        grace_period = timedelta(seconds=self.ctx.config.pq_populate_grace_period)
+
         # New tasks that have not yet run before
         if obj.prior_tasks is None or not obj.prior_tasks:
             return 2
 
-        max_priority = self.MAX_PRIORITY
-        grace_period = timedelta(seconds=self.ctx.config.pq_populate_grace_period)
-
-        # How many days after grace period should the priority be 3? We want
-        # to have tasks that are not run for 7 days to have a priority of 3.
-        max_days = self.MAX_DAYS * (60 * 60 * 24)
-
-        # Check how long since the grace period has passed
-        run_since_grace_period = ((datetime.now(timezone.utc) - obj.prior_tasks[0].modified_at) - grace_period).seconds
-
-        # Makes sure that we don't have tasks that are still in the grace
-        # period
-        if run_since_grace_period < 0:
+        # Make sure that we don't have tasks that are still in the grace period
+        time_since_grace_period = ((datetime.now(timezone.utc) - obj.prior_tasks[0].modified_at) - grace_period).seconds
+        if time_since_grace_period < 0:
             return -1
 
-        # Rank the task based on how long it has been since the last run
-        y = max_priority * math.pow(math.e, -(math.log(max_priority) / max_days) * run_since_grace_period) + 2
+        if time_since_grace_period >= max_days_in_seconds:
+            return 3
 
-        return int(y)
+        return int(3 + (max_priority - 3) * (1 - time_since_grace_period / max_days_in_seconds))
 
 
 class BoefjeRankerTimeBased(Ranker):
