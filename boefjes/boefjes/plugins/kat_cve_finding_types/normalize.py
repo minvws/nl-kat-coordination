6b52fbfa9af75532392cf3a475aa28c82b8029e5
Noam Blitz <noamblitz@gmail.com>
2023-05-22 16:15:10 +0300

    add cve finding types hydration boefje and normalizer

---
+++ boefjes/boefjes/plugins/kat_cve_finding_types/normalize.py
@@ -0,0 +1,43 @@
+import json
+import logging
+from typing import Iterable, Union
+
+from boefjes.job_models import NormalizerMeta
+from octopoes.models import OOI
+from octopoes.models.ooi.findings import CVEFindingType, RiskLevelSeverity
+
+logger = logging.getLogger(__name__)
+
+
+SEVERITY_SCORE_LOOKUP = {
+    RiskLevelSeverity.CRITICAL: 10.0,
+    RiskLevelSeverity.HIGH: 8.9,
+    RiskLevelSeverity.MEDIUM: 6.9,
+    RiskLevelSeverity.LOW: 3.9,
+    RiskLevelSeverity.RECOMMENDATION: 0.0,
+}
+
+
+def get_risk_level(severity_score):
+    for risk_level, score in SEVERITY_SCORE_LOOKUP.items():
+        if severity_score >= score:
+            return risk_level
+    return None
+
+
+def run(normalizer_meta: NormalizerMeta, raw: Union[bytes, str]) -> Iterable[OOI]:
+    cve_finding_type_id = normalizer_meta.raw_data.boefje_meta.arguments["input"]["id"]
+    data = json.loads(raw)
+
+    descriptions = data["cve"]["description"]["description_data"]
+    english_description = [description for description in descriptions if description["lang"] == "en"][0]
+
+    risk_score = data["impact"]["baseMetricV3"]["cvssV3"]["baseScore"]
+    risk_severity = get_risk_level(risk_score)
+
+    yield CVEFindingType(
+        id=cve_finding_type_id,
+        description=english_description["value"],
+        risk_severity=risk_severity,
+        risk_score=risk_score,
+    )
