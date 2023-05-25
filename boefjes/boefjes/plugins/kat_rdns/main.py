089d99fd3937b17b466334e762415dc1118b8a7e
Robin Visser <robinvisser92@live.nl>
2023-05-25 22:19:09 +0200

    Add reverse DNS boefje
    Creating RDNS Boefje
    
    Also adding DNSPTRRecord functionality.
    
    Update description.md
    
    formatting changes
    
    formatting and adding in-addr.arpa to identifier.
    
    returning none and adding in-addr.arpa
    
    Update description.md
    
    Update boefje.json
    
    updating spaces
    
    Fix in-addr.arpa format and formatting.
    
    Update records.py
    
    Update records.py
    
    supporting netblock
    
    formatting
    
    formatting
    
    Update boefje.json
    
    Update boefje.json
    
    Revert "Update boefje.json"
    
    This reverts commit ec89a56727834fe0e046adec97de9767b355cad3.
    
    remove netblock ipv6
    
    reverse dns improvements
    
    Update boefjes/boefjes/plugins/kat_rdns/main.py
    
    Co-authored-by: ammar92 <ammar.abdulamir@gmail.com>
    
    Update main.py
    
    add no answer result
    
    handle nxdomain and no answer

---
+++ boefjes/boefjes/plugins/kat_rdns/main.py
@@ -0,0 +1,24 @@
+from typing import List, Tuple, Union
+
+import dns
+from dns.resolver import Answer
+
+from boefjes.config import settings
+from boefjes.job_models import BoefjeMeta
+
+
+def run(boefje_meta: BoefjeMeta) -> List[Tuple[set, Union[bytes, str]]]:
+    """return results to normalizer."""
+    ip = boefje_meta.arguments["input"]["address"]
+
+    try:
+        resolver = dns.resolver.Resolver()
+        resolver.nameservers = [settings.remote_ns]
+        reverse_ip = dns.reversename.from_address(ip)
+        answer: Answer = resolver.resolve(reverse_ip, "PTR")
+        result = f"RESOLVER: {answer.nameserver}\n{answer.response}"
+        return [(set(), result)]
+    except dns.resolver.NXDOMAIN:
+        return [(set(), "NXDOMAIN")]
+    except (dns.resolver.Timeout, dns.resolver.NoAnswer):
+        return [(set(), "NoAnswer")]
