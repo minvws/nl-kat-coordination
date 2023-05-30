aefdc4db9a575dd0781b9ac2c39483b67c2c2235
Donny Peeters <donny@bitestreams.com>
2023-05-30 12:12:24 +0200

    Use new bulk operations for scan_profiles in Rocky as well.

--- rocky/account/mixins.py
+++ rocky/account/mixins.py
@@ -1,12 +1,10 @@
 from datetime import datetime, timezone
-from typing import Optional
+from typing import List, Optional
 
 from django.conf import settings
-from django.contrib import messages
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.core.exceptions import PermissionDenied
 from django.http import Http404
-from django.utils.translation import gettext_lazy as _
 from django.views import View
 from tools.models import Indemnification, Organization, OrganizationMember
 
@@ -14,7 +12,6 @@ from octopoes.connector.octopoes import OctopoesAPIConnector
 from octopoes.models import DeclaredScanProfile, Reference, ScanLevel
 from rocky.exceptions import (
     AcknowledgedClearanceLevelTooLowException,
-    ClearanceLevelTooLowException,
     IndemnificationNotPresentException,
     TrustedClearanceLevelTooLowException,
 )
@@ -126,42 +123,21 @@ class OrganizationView(View):
         return True
 
     def raise_clearance_level(self, ooi_reference: Reference, level: int) -> bool:
-        try:
-            self.verify_raise_clearance_level(level)
-            self.octopoes_api_connector.save_scan_profile(
-                DeclaredScanProfile(reference=ooi_reference, level=ScanLevel(level)),
-                datetime.now(timezone.utc),
-            )
-        except IndemnificationNotPresentException as exc:
-            messages.add_message(
-                self.request,
-                messages.ERROR,
-                _(
-                    "Could not raise clearance level of %s to L%s. \
-                    Indemnification not present at organization %s."
-                )
-                % (
-                    ooi_reference.human_readable,
-                    level,
-                    self.organization.name,
-                ),
-            )
-            raise exc
-        except ClearanceLevelTooLowException as exc:
-            messages.add_message(
-                self.request,
-                messages.ERROR,
-                _(
-                    "Could not raise clearance level of %s to L%s. \
-                    You acknowledged a clearance level of %s."
-                )
-                % (
-                    ooi_reference.human_readable,
-                    level,
-                    self.organization_member.acknowledged_clearance_level,
-                ),
-            )
-            raise exc
+        self.verify_raise_clearance_level(level)
+        self.octopoes_api_connector.save_scan_profile(
+            DeclaredScanProfile(reference=ooi_reference, level=ScanLevel(level)),
+            datetime.now(timezone.utc),
+        )
+
+        return True
+
+    def raise_clearance_levels(self, ooi_references: List[Reference], level: int) -> bool:
+        self.verify_raise_clearance_level(level)
+        self.octopoes_api_connector.save_many_scan_profiles(
+            [DeclaredScanProfile(reference=reference, level=ScanLevel(level)) for reference in ooi_references],
+            datetime.now(timezone.utc),
+        )
+
         return True
 
 
