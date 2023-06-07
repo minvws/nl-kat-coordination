b17e3e3c087e61b469576aee9bac23a8167afa47
Donny Peeters <donny@bitestreams.com>
2023-06-07 11:31:43 +0200

    Re-use Rabbitmq channels as well

--- bytes/bytes/rabbitmq.py
+++ bytes/bytes/rabbitmq.py
@@ -1,5 +1,5 @@
 import logging
-from functools import cached_property
+from functools import lru_cache
 
 import pika
 import pika.exceptions
@@ -14,48 +14,28 @@ logger = logging.getLogger(__name__)
 class RabbitMQEventManager(EventManager):
     def __init__(self, queue_uri: str):
         self.queue_uri = queue_uri
-        self.connection = self.connect()
+        self.connection = get_connection(self.queue_uri)
+        self.channel = self.connection.channel()
 
     def publish(self, event: Event) -> None:
         event_data = event.json()
         queue_name = self._queue_name(event)
 
         logger.debug("Publishing event: %s", event_data)
-
-        channel = self.connection.channel()
-        channel.queue_declare(queue_name)
+        self.channel.queue_declare(queue_name)
 
         try:
-            channel.basic_publish(
-                "",
-                queue_name,
-                event_data.encode(),
-            )
+            self.channel.basic_publish("", queue_name, event_data.encode())
         except pika.exceptions.ConnectionClosed:
-            logger.exception("RabbitMQ connection was closed: retrying")
-            self.connect()
-            channel.basic_publish(
-                "",
-                queue_name,
-                event_data.encode(),
-            )
+            logger.exception("RabbitMQ connection was closed: retrying with a new connection.")
+
+            get_connection.cache_clear()
+            self.connection = get_connection(self.queue_uri)
+            self.channel = self.connection.channel()
+
+            self.channel.basic_publish("", queue_name, event_data.encode())
 
         logger.info("Published event [event_id=%s] to queue %s", event.event_id, queue_name)
-
-    def connect(self) -> pika.BlockingConnection:
-        connection = self.get_rabbitmq_connection
-
-        if connection.is_closed:
-            del self.get_rabbitmq_connection
-            connection = self.get_rabbitmq_connection
-
-        return connection
-
-    @cached_property
-    def get_rabbitmq_connection(self) -> pika.BlockingConnection:
-        logger.info("Connecting to RabbitMQ")
-
-        return pika.BlockingConnection(pika.URLParameters(self.queue_uri))
 
     @staticmethod
     def _queue_name(event: Event) -> str:
@@ -65,6 +45,15 @@ class RabbitMQEventManager(EventManager):
 class NullManager(EventManager):
     def publish(self, event: Event) -> None:
         pass
+
+
+@lru_cache(maxsize=1)
+def get_connection(queue_uri: str) -> pika.BlockingConnection:
+    logger.info("Connecting to RabbitMQ")
+    connection = pika.BlockingConnection(pika.URLParameters(queue_uri))
+    logger.info("Connected to RabbitMQ")
+
+    return connection
 
 
 def create_event_manager() -> EventManager:
