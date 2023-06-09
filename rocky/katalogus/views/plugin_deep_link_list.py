21da97986be7b79709fb52a2255983371eb7c773
Rieven <rsnmartis@gmail.com>
2023-06-09 16:23:50 +0200

    Fix dynamic link creation

--- rocky/katalogus/views/plugin_deep_link_list.py
+++ rocky/katalogus/views/plugin_deep_link_list.py
@@ -7,15 +7,11 @@ from django.views.generic import ListView
 from tools.view_helpers import Breadcrumb, get_ooi_url
 
 from katalogus.models import OrganizationPlugin
-from rocky.views.mixins import OctopoesView
+from rocky.views.mixins import SingleOOIMixin
 from rocky.views.ooi_view import BaseOOIBreadcrumbs
 
 
-def replace_link_id_param(link: str, key: str) -> str:
-    return link.replace("[id]", key)
-
-
-class PluginDeepLinkDetailedListView(BaseOOIBreadcrumbs, OctopoesView, ListView):
+class PluginDeepLinkDetailedListView(BaseOOIBreadcrumbs, SingleOOIMixin, ListView):
     """
     Listing deep links of a specific OOI related to an OOI-Type.
     Each OOI has a specific key/id to build the link, for example CVE codes in link.
@@ -28,11 +24,11 @@ class PluginDeepLinkDetailedListView(BaseOOIBreadcrumbs, OctopoesView, ListView)
     def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
         ooi_id = self.request.GET.get("ooi_id", None)
         self.ooi = self.get_single_ooi(ooi_id)
+        self.ooi_props = self.get_ooi_properties(self.ooi)
         return super().get(request, *args, **kwargs)
 
     def get_queryset(self):
         queryset = []
-
         plugins = OrganizationPlugin.objects.filter(
             organization=self.organization, plugin__ooi_type=self.ooi.ooi_type, enabled=True
         )
@@ -40,11 +36,17 @@ class PluginDeepLinkDetailedListView(BaseOOIBreadcrumbs, OctopoesView, ListView)
             queryset.append(
                 {
                     "name": plugin.plugin.name,
-                    "link_text": replace_link_id_param(plugin.plugin.content, self.ooi.natural_key),
-                    "link": replace_link_id_param(plugin.plugin.link, self.ooi.natural_key),
+                    "link_text": self.replace_params_with_ooi_props(plugin.plugin.content),
+                    "link": self.replace_params_with_ooi_props(plugin.plugin.link),
                 }
             )
         return queryset
+
+    def replace_params_with_ooi_props(self, target: str) -> str:
+        for key, value in self.ooi_props.items():
+            if value:
+                target = target.replace("[" + key + "]", str(value))
+        return target
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
