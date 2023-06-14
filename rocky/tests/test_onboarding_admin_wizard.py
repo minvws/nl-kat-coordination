3f8f3373cc3be2c57dd75110b0f6d892817d817b
Rieven <rsnmartis@gmail.com>
2023-06-14 16:44:16 +0200

    Fix tests

--- rocky/tests/test_onboarding_admin_wizard.py
+++ rocky/tests/test_onboarding_admin_wizard.py
@@ -14,18 +14,21 @@ from tests.conftest import setup_request
 
 
 def test_admin_onboarding_registration(rf, superuser_member, admin_member, redteam_member, client_member):
+    """
+    This onboarding is before an organization has been created and it is only visible for superusers.
+    """
     response_superuser = OnboardingIntroductionRegistrationView.as_view()(
         setup_request(rf.get("step_introduction_registration"), superuser_member.user),
         organization_code=superuser_member.organization.code,
     )
 
-    response_admin = OnboardingIntroductionRegistrationView.as_view()(
-        setup_request(rf.get("step_introduction_registration"), admin_member.user),
-        organization_code=admin_member.organization.code,
-    )
-
     assert response_superuser.status_code == 200
-    assert response_admin.status_code == 200
+
+    with pytest.raises(PermissionDenied):
+        OnboardingIntroductionRegistrationView.as_view()(
+            setup_request(rf.get("step_introduction_registration"), admin_member.user),
+            organization_code=admin_member.organization.code,
+        )
 
     with pytest.raises(PermissionDenied):
         OnboardingIntroductionRegistrationView.as_view()(
