3a210bd8b4b7dae84f20c122e682323ec6b1d15c
Noam Blitz <noamblitz@gmail.com>
2023-06-15 11:26:21 +0200

    use valid ipaddress

--- octopoes/tests/test_bit_ports.py
+++ octopoes/tests/test_bit_ports.py
@@ -6,7 +6,7 @@ from octopoes.models.ooi.network import IPAddressV4, IPPort
 
 
 def test_port_classification_tcp_80():
-    address = IPAddressV4(address="fake", network="fake")
+    address = IPAddressV4(address="8.8.8.8", network="fake")
     port = IPPort(address=address.reference, protocol="tcp", port=80)
     results = list(run_port_classification(address, [port], {}))
 
@@ -14,7 +14,7 @@ def test_port_classification_tcp_80():
 
 
 def test_port_classification_udp_53():
-    address = IPAddressV4(address="fake", network="fake")
+    address = IPAddressV4(address="8.8.8.8", network="fake")
     port = IPPort(address=address.reference, protocol="tcp", port=53)
     results = list(run_port_classification(address, [port], {}))
 
@@ -22,7 +22,7 @@ def test_port_classification_udp_53():
 
 
 def test_port_classification_tcp_22():
-    address = IPAddressV4(address="fake", network="fake")
+    address = IPAddressV4(address="8.8.8.8", network="fake")
     port = IPPort(address=address.reference, protocol="tcp", port=22)
     results = list(run_port_classification(address, [port], {}))
 
@@ -33,7 +33,7 @@ def test_port_classification_tcp_22():
 
 
 def test_port_classification_tcp_5432():
-    address = IPAddressV4(address="fake", network="fake")
+    address = IPAddressV4(address="8.8.8.8", network="fake")
     port = IPPort(address=address.reference, protocol="tcp", port=5432)
     results = list(run_port_classification(address, [port], {}))
 
@@ -44,7 +44,7 @@ def test_port_classification_tcp_5432():
 
 
 def test_port_classification_tcp_12345():
-    address = IPAddressV4(address="fake", network="fake")
+    address = IPAddressV4(address="8.8.8.8", network="fake")
     port = IPPort(address=address.reference, protocol="tcp", port=12345)
     results = list(run_port_classification(address, [port], {}))
 
@@ -55,7 +55,7 @@ def test_port_classification_tcp_12345():
 
 
 def test_port_classification_udp_80():
-    address = IPAddressV4(address="fake", network="fake")
+    address = IPAddressV4(address="8.8.8.8", network="fake")
     port = IPPort(address=address.reference, protocol="tcp", port=80)
     results = list(run_port_classification(address, [port], {}))
 
