61da7d7b0c414312986f207183d62fd85c346b53
Noam Blitz <noamblitz@gmail.com>
2023-06-21 14:17:17 +0200

    use local xml for cwes

--- boefjes/boefjes/plugins/kat_cwe_finding_types/main.py
+++ boefjes/boefjes/plugins/kat_cwe_finding_types/main.py
@@ -1,15 +1,29 @@
 import json
+import xml.etree.ElementTree as ET
 from typing import List, Tuple, Union
 
-from cwe import Database
-
 from boefjes.job_models import BoefjeMeta
+
+FINDING_TYPE_PATH = "boefjes/plugins/kat_cwe_finding_types/cwec_v4.11.xml"
 
 
 def run(boefje_meta: BoefjeMeta) -> List[Tuple[set, Union[bytes, str]]]:
     cwe_id = boefje_meta.arguments["input"]["id"]
 
-    db = Database()
-    weakness = db.get(cwe_id.split("-")[1])
+    root = ET.parse(FINDING_TYPE_PATH)
+    root = root.getroot()
 
-    return [(set(), json.dumps(weakness.to_dict()))]
+    # Define the XML namespace
+    namespace = {"ns": "http://cwe.mitre.org/cwe-6"}
+
+    # Find the Weakness element with the specified CWE ID
+    xpath = f".//ns:Weakness[@ID='{cwe_id.split('-')[1]}']"
+    weakness_elem = root.find(xpath, namespace)
+
+    # Retrieve the name and description
+    name = weakness_elem.get("Name")
+    description = weakness_elem.find("ns:Description", namespace).text
+
+    data = {"name": name, "description": description}
+
+    return [(set(), json.dumps(data))]
