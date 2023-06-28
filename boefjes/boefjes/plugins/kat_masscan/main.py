78b9963ec02e929336aa938357309897ea743bbf
zcrt <115991818+zcrt@users.noreply.github.com>
2023-06-28 11:31:14 +0200

    :fire: add masscan for large IP-ranges

---
+++ boefjes/boefjes/plugins/kat_masscan/main.py
@@ -0,0 +1,120 @@
+import io
+import logging
+import os
+import tarfile
+from typing import ByteString, Generator, List, Tuple, Union
+
+import docker
+
+from boefjes.job_models import BoefjeMeta
+
+
+###############################################################################
+# Identical to Webpage Capture boefje.
+###############################################################################
+class TarStream(io.RawIOBase):
+    """Wrapper around generator to feed tarfile.
+
+    Based on:
+    - https://stackoverflow.com/questions/39155958/how-do-i-read-a-tarfile-from-a-generator
+    - https://stackoverflow.com/questions/6657820/how-to-convert-an-iterable-to-a-stream/6658949
+    """
+
+    def __init__(self, stream: Generator):
+        """Store the generator in the TarStream class."""
+        self.bytes_left = None
+        self.stream = stream
+        self.able_to_read = bool(stream)
+        super().__init__()
+
+    def reader(self) -> io.BufferedReader:
+        """Return the bufferedreader for this TarStream."""
+        return io.BufferedReader(self)
+
+    def readable(self) -> bool:
+        """Returns whether the generator stream is (still) readable."""
+        return self.able_to_read
+
+    def readinto(self, memory_view: ByteString) -> int:
+        """Read the generator. Returns 0 when done. Output is stored in memory_view."""
+        try:
+            chunk = self.bytes_left or next(self.stream)
+        except StopIteration:
+            self.able_to_read = False
+            return 0
+        view_len = len(memory_view)
+        output, self.bytes_left = chunk[:view_len], chunk[view_len:]
+        outlen = len(output)
+        memory_view[:outlen] = output
+        return outlen
+
+
+def get_file_from_container(container: docker.models.containers.Container, path: str) -> bytes:
+    """Returns a file from a docker container."""
+    try:
+        stream, _ = container.get_archive(path)
+    except docker.errors.NotFound:
+        logging.warning(
+            "[Masscan] %s not found in container %s %s",
+            path,
+            container.short_id,
+            container.image.tags,
+        )
+        return None
+
+    f = tarfile.open(mode="r|", fileobj=TarStream(stream).reader())
+    tarobject = f.next()
+    if tarobject.name == os.path.basename(path):
+        return f.extractfile(tarobject).read()
+
+
+###############################################################################
+
+
+def build_image(client: docker.DockerClient, tag: str) -> str:
+    """Builds Docker container from Dockerfile in kat_masscan folder."""
+    output = client.images.build(path="boefjes/plugins/kat_masscan/", tag=tag, quiet=False)
+    logging.info("Build image: %s", output)
+    return tag
+
+
+def run_masscan(target_ip, tmp_path: str = "/tmp/output.json") -> Tuple[bytes]:
+    """Run Playwright in Docker."""
+    client = docker.from_env()
+
+    # Build image if it does not exist yet.
+    known_tags = [tag for tag_list in [c.tags for c in client.images.list()] for tag in tag_list]
+    image_tag = "nl-kat-masscan-0"
+    if image_tag + ":latest" not in known_tags:
+        image_tag = build_image(client, image_tag)
+
+    # Scan according to arguments.
+    port_range = os.getenv("PORTS", "53,80-82,443")
+    max_rate = os.getenv("MAX_RATE", 100)
+    logging.info("Starting container %s to run masscan...", image_tag)
+    res = client.containers.run(
+        image=image_tag,
+        command=[f"-p{port_range}", "--max-rate", max_rate, "-oJ", tmp_path, target_ip],
+        detach=True,
+    )
+    res.wait()
+    logging.debug(res.logs())
+    output = get_file_from_container(container=res, path=tmp_path)
+
+    # Do not crash the boefje if the output is known, instead log a warning.
+    try:
+        res.remove()
+    except Exception as e:
+        logging.warning(e)
+
+    return output
+
+
+def run(boefje_meta: BoefjeMeta) -> List[Tuple[set, Union[bytes, str]]]:
+    """Creates webpage and takes capture using Playwright container."""
+    input_ = boefje_meta.arguments["input"]
+    ip_range = f"{input_['start_ip']['address']}/{str(input_['mask'])}"
+    m_run = run_masscan(target_ip=ip_range)
+    logging.info("Received a response with length %d", len(m_run))
+
+    return [(set(), m_run)]
