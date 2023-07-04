2581a1536f46af5a7389ae9f8c3c2da745fd3c61
ring-ring-ring <76487016+ring-ring-ring@users.noreply.github.com>
2023-07-04 13:09:01 +0200

    typo

--- docs/source/utilities/scripts/status_openkat.sh
+++ docs/source/utilities/scripts/status_openkat.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 if [[ ${1} == "" ]]; then
-	echo "Usage ./status_openkat.sh [proces]" 
+	echo "Usage ./status_openkat.sh [process]" 
 else 
 	echo "Status ${1}:"
 	sudo systemctl status $1
