69837032e3b2fe871c56e3e2495318be6a6624e4
Donny Peeters <donny@bitestreams.com>
2023-07-10 20:12:07 +0200

    Decode raw

--- boefjes/boefjes/plugins/kat_answer_parser/normalize.py
+++ boefjes/boefjes/plugins/kat_answer_parser/normalize.py
@@ -10,6 +10,9 @@ logger = logging.getLogger(__name__)
 
 def run(normalizer_meta: NormalizerMeta, raw: Union[bytes, str]) -> Iterable[OOI]:
     if "/bit/port-classification" in [mime_type["value"] for mime_type in normalizer_meta.raw_data.mime_types]:
+        if isinstance(raw, bytes):
+            raw = raw.decode()
+
         yield Config(
             ooi=normalizer_meta.raw_data.boefje_meta.input_ooi, bit_id="/bit/port-classification", config={"ports": raw}
         )
