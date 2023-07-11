1a3683f709fd754c4c8a223a23a95ce264906786
Donny Peeters <donny.peeters@hotmail.com>
2023-07-11 15:40:47 +0200

    Remove callable from tests as well

--- boefjes/tests/conftest.py
+++ boefjes/tests/conftest.py
@@ -118,13 +118,12 @@ def item_handler(tmp_path: Path):
 
 @pytest.fixture
 def manager(item_handler: MockHandler, tmp_path: Path) -> SchedulerWorkerManager:
-    def client_factory():
-        return MockSchedulerClient(
-            get_dummy_data("scheduler/queues_response.json"),
-            2 * [get_dummy_data("scheduler/pop_response_boefje.json")]
-            + [get_dummy_data("scheduler/should_crash.json")],
-            [get_dummy_data("scheduler/pop_response_normalizer.json")],
-            tmp_path / "patch_task_log",
-        )
+    scheduler_client = MockSchedulerClient(
+        get_dummy_data("scheduler/queues_response.json"),
+        2 * [get_dummy_data("scheduler/pop_response_boefje.json")]
+        + [get_dummy_data("scheduler/should_crash.json")],
+        [get_dummy_data("scheduler/pop_response_normalizer.json")],
+        tmp_path / "patch_task_log",
+    )
 
-    return SchedulerWorkerManager(item_handler, client_factory, Settings(pool_size=1, poll_interval=0.01), "DEBUG")
+    return SchedulerWorkerManager(item_handler, scheduler_client, Settings(pool_size=1, poll_interval=0.01), "DEBUG")
