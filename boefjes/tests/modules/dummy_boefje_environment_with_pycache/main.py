c5df49a54a207a71e36d51512a5d14ad849d7680
Patrick Darwinkel <patrick@code050.nl>
2023-07-13 14:45:26 +0200

    Restore removed files

--- boefjes/tests/modules/dummy_boefje_environment_with_pycache/main.py
+++
@@ -1,8 +0,0 @@
-from os import environ
-from typing import List, Tuple, Union
-
-from boefjes.job_models import BoefjeMeta
-
-
-def run(boefje_meta: BoefjeMeta) -> List[Tuple[set, Union[bytes, str]]]:
-    return [(set(), repr(environ).encode())]
