173fb6781449151ec1e042415df1435291b52f85
Donny Peeters <donny@bitestreams.com>
2023-07-26 12:10:03 +0200

    Cleanup normalizer

--- boefjes/boefjes/plugins/kat_answer_parser/normalize.py
+++ boefjes/boefjes/plugins/kat_answer_parser/normalize.py
@@ -1,15 +1,14 @@
-import logging
 from typing import Iterable, Union
 
 from boefjes.job_models import NormalizerMeta
 from octopoes.models import OOI
 from octopoes.models.ooi.config import Config
 
-logger = logging.getLogger(__name__)
-
 
 def run(normalizer_meta: NormalizerMeta, raw: Union[bytes, str]) -> Iterable[OOI]:
-    if "/bit/port-classification" in [mime_type["value"] for mime_type in normalizer_meta.raw_data.mime_types]:
+    mime_types = [mime_type["value"] for mime_type in normalizer_meta.raw_data.mime_types]
+
+    if "/bit/port-classification" in mime_types:
         if isinstance(raw, bytes):
             raw = raw.decode()
 
