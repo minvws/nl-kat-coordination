13ba08a49e59ef80384076b169cb27a6a223ce98
Donny Peeters <donny@bitestreams.com>
2023-07-26 13:05:11 +0200

    Add a test and fix bit ids

--- boefjes/boefjes/plugins/kat_answer_parser/normalize.py
+++ boefjes/boefjes/plugins/kat_answer_parser/normalize.py
@@ -1,3 +1,4 @@
+import json
 from typing import Iterable, Union
 
 from boefjes.job_models import NormalizerMeta
@@ -8,10 +9,12 @@ from octopoes.models.ooi.config import Config
 def run(normalizer_meta: NormalizerMeta, raw: Union[bytes, str]) -> Iterable[OOI]:
     mime_types = [mime_type["value"] for mime_type in normalizer_meta.raw_data.mime_types]
 
-    if "/bit/port-classification" in mime_types:
+    if "/bit/port-classification-ip" in mime_types:
         if isinstance(raw, bytes):
             raw = raw.decode()
 
         yield Config(
-            ooi=normalizer_meta.raw_data.boefje_meta.input_ooi, bit_id="/bit/port-classification", config={"ports": raw}
+            ooi=normalizer_meta.raw_data.boefje_meta.input_ooi,
+            bit_id="/bit/port-classification-ip",
+            config=json.loads(raw),
         )
