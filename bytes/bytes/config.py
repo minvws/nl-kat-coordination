345658c6db87fc19b95c5d3d84845b362b9cb025
Patrick Darwinkel <patrick@code050.nl>
2023-07-31 13:54:55 +0200

    Update bytes pydantic settings

--- bytes/bytes/config.py
+++ bytes/bytes/config.py
@@ -2,7 +2,7 @@ from functools import lru_cache
 from pathlib import Path
 from typing import Optional
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field, PostgresDsn
 
 from bytes.models import EncryptionMiddleware, HashingAlgorithm, HashingRepositoryReference
 
@@ -11,32 +11,37 @@ BASE_DIR = Path(__file__).parent.parent
 
 class Settings(BaseSettings):
     secret: str
-    bytes_username: str
-    bytes_password: str
+    username: str
+    password: str
     queue_uri: Optional[str]
     log_cfg: Path = BASE_DIR / "dev.logging.conf"
 
-    bytes_db_uri: str
-    bytes_data_dir: Path = Path("/data")
+    db_uri: PostgresDsn = Field(
+        "postgresql://xx:xx@host:5432/bytes", description="Bytes Postgres DB URI", env="BYTES_DB"
+    )
+    data_dir: Path = Field("/data", description="Directory where Bytes stores its data")
 
-    bytes_log_file: str = "bytes.log"
-    access_token_expire_minutes: float = 15.0
-    bytes_folder_permission: str = "740"
-    bytes_file_permission: str = "640"
+    log_file: str = Field("bytes.log", description="Log file name and extension")
+    access_token_expire_minutes: float = Field(15.0, description="Access token expiration time in minutes")
+    folder_permission: str = Field("740", description="Unix file system permission for folders")
+    file_permission: str = Field("640", description="Unix file system permission for files")
 
     hashing_algorithm: HashingAlgorithm = HashingAlgorithm.SHA512
 
     ext_hash_repository: HashingRepositoryReference = HashingRepositoryReference.IN_MEMORY
-    pastebin_api_dev_key: str = ""
-    rfc3161_provider: Optional[str]
-    rfc3161_cert_file: Optional[Path]
+    pastebin_api_dev_key: str = Field("", description="API key for Pastebin")
+    rfc3161_provider: Optional[str] = Field("", description="URL of the RFC3161 provider")
+    rfc3161_cert_file: Optional[Path] = Field("", description="Path to the certificate of the RFC3161 provider")
 
     encryption_middleware: EncryptionMiddleware = EncryptionMiddleware.IDENTITY
-    kat_private_key_b64: str = ""
-    vws_public_key_b64: str = ""
+    private_key_b64: str = Field("", description="Private key for Bytes' storage in base64 format")
+    public_key_b64: str = Field("", description="Public key for Bytes' storage in base64 format")
 
-    span_export_grpc_endpoint: Optional[str]
-    bytes_metrics_ttl_seconds: int = 300
+    span_export_grpc_endpoint: Optional[str] = Field(None, env="SPAN_EXPORT_GRPC_ENDPOINT")
+    metrics_ttl_seconds: int = Field(300, description="Time to live for metrics in seconds")
+
+    class Config:
+        env_prefix = "BYTES_"
 
 
 @lru_cache
