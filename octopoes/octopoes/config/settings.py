bc32090d0eff5b01b68d01330ca1a22b57995e92
Patrick Darwinkel <patrick@code050.nl>
2023-07-31 14:05:17 +0200

    Update octopoes settings with pydantic typing

--- octopoes/octopoes/config/settings.py
+++ octopoes/octopoes/config/settings.py
@@ -4,7 +4,7 @@ from enum import Enum
 from pathlib import Path
 from typing import Optional, Set
 
-from pydantic import BaseSettings
+from pydantic import AmqpDsn, AnyHttpUrl, BaseSettings, Field
 
 from octopoes.models import ScanLevel, ScanProfileType
 from octopoes.models.ooi.findings import RiskLevelSeverity
@@ -20,19 +20,23 @@ class Settings(BaseSettings):
     """Application settings loaded from environment variables."""
 
     # Application settings
-    log_cfg: str = str(Path(__file__).parent.parent.parent / "logging.yml")  # todo: use Path type
+    log_cfg: str = str(Path(__file__).parent.parent.parent / "logging.yml")
 
     # External services settings
-    queue_uri: str = "amqp://guest:guest@rabbitmq:5672/%2fkat"
-    xtdb_uri: str = "http://crux:3000"
+    queue_uri: AmqpDsn = Field("amqp://", description="KAT queue URI", env="QUEUE_URI")
+    xtdb_uri: str = Field("http://crux:3000", description="XTDB API", env="XTDB_URI")
     xtdb_type: XTDBType = XTDBType.XTDB_MULTINODE
-    span_export_grpc_endpoint: Optional[str] = None
 
-    katalogus_api: str = "http://localhost:8003"
+    span_export_grpc_endpoint: Optional[str] = Field(None, env="SPAN_EXPORT_GRPC_ENDPOINT")
 
-    scan_level_recalculation_interval: int = 60
-    bits_enabled: Set[str] = set()
-    bits_disabled: Set[str] = set()
+    katalogus_api: AnyHttpUrl = Field("http://localhost:8003", description="Katalogus API URL", env="KATALOGUS_API")
+
+    scan_level_recalculation_interval: int = Field(60, description="Scan level recalculation interval in seconds")
+    bits_enabled: Set[str] = Field(set(), description="Explicitly enabled bits")
+    bits_disabled: Set[str] = Field(set(), description="Explicitly disabled bits")
+
+    class Config:
+        env_prefix = "OCTOPOES_"
 
 
 DEFAULT_SCAN_LEVEL_FILTER = {scan_level for scan_level in ScanLevel}
