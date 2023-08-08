e8d5743f3774b45062aee606cfb3586e27202f5a
Patrick Darwinkel <patrick@code050.nl>
2023-08-08 09:43:38 +0200

    Fix BASE_DIR import

--- boefjes/tests/stubs.py
+++ boefjes/tests/stubs.py
@@ -1,6 +1,6 @@
-from boefjes.config import settings
+from boefjes.config import BASE_DIR
 
 
 def get_dummy_data(filename: str) -> bytes:
-    path = settings.BASE_DIR / ".." / "tests" / "examples" / filename
+    path = BASE_DIR / ".." / "tests" / "examples" / filename
     return path.read_bytes()
