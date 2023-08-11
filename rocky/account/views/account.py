99a597e9c5fb777b6910d53105137f32397802cb
Rieven <rsnmartis@gmail.com>
2023-08-11 10:26:16 +0200

    Removed double context

--- rocky/account/views/account.py
+++ rocky/account/views/account.py
@@ -29,7 +29,6 @@ class AccountView(OOIClearanceMixin):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["member"] = self.organization_member
         context["breadcrumbs"] = [
             {"url": "", "text": "Account details"},
         ]
