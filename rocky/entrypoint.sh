6d57ed997036c5d69ff9ec2174b130c0fbebdeb7
Jeroen Dekkers <jeroen@dekkers.ch>
2023-09-15 10:00:22 +0200

    Add support for using granian instead of uWSGI

--- rocky/entrypoint.sh
+++ rocky/entrypoint.sh
@@ -8,4 +8,8 @@ if [ "$DATABASE_MIGRATION" = "1" ] || [  "$DATABASE_MIGRATION" = "true" ]; then
     python manage.py migrate --noinput
 fi
 
-exec "$@"
+if [ "$USE_GRANIAN" = "1" ] || [  "$USE_GRANIAN" = "true" ]; then
+    exec granian --workers "$GRANIAN_WORKERS" --threads "$GRANIAN_THREADS" --interface wsgi rocky.wsgi:application --host 0.0.0.0
+else
+    exec "$@"
+fi
