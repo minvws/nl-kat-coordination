f6328d282e8fef730123da8c6ac17c0dda337c83
Rieven <rsnmartis@gmail.com>
2023-09-20 14:39:20 +0200

    Fix currently filter on

--- rocky/rocky/views/ooi_list.py
+++ rocky/rocky/views/ooi_list.py
@@ -41,7 +41,7 @@ class OOIListView(BaseOOIListView, OctopoesView):
         context = super().get_context_data(**kwargs)
 
         context["active_filters"] = self.get_active_filters()
-        context["object_type_filters"] = OOITypeMultiCheckboxForm(self.request.GET)
+        context["ooi_type_form"] = OOITypeMultiCheckboxForm(self.request.GET)
         context["observed_at"] = self.get_observed_at()
         context["mandatory_fields"] = get_mandatory_fields(self.request, params=["observed_at"])
         context["select_oois_form"] = SelectOOIForm(
