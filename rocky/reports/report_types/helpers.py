753e14d60c81841a88db2e680a676f2546520c8c
Noam Blitz <noamblitz@gmail.com>
2023-09-21 16:40:15 +0200

    helpers

---
+++ rocky/reports/report_types/helpers.py
@@ -0,0 +1,31 @@
+from typing import List, Set, Type
+
+from octopoes.models import OOI, Reference
+from reports.report_types.definitions import Report
+from reports.report_types.dns_report.report import DNSReport
+from reports.report_types.tls_report.report import TLSReport
+
+REPORTS = [DNSReport, TLSReport]
+
+
+def get_ooi_types_with_report() -> Set[Type[OOI]]:
+    """
+    Get all OOI types that have a report
+    """
+    return {ooi_type for report in REPORTS for ooi_type in report.input_ooi_types}
+
+
+def get_report_types_for_ooi(ooi_pk: str) -> List[Type[Report]]:
+    """
+    Get all report types that can be generated for a given OOI
+    """
+    reference = Reference.from_str(ooi_pk)
+    ooi_type = reference.class_type
+    return [report for report in REPORTS if ooi_type in report.input_ooi_types]
+
+
+def get_report_types_for_oois(ooi_pks: List[str]) -> Set[Type[Report]]:
+    """
+    Get all report types that can be generated for a given list of OOIs
+    """
+    return {report for ooi_pk in ooi_pks for report in get_report_types_for_ooi(ooi_pk)}
