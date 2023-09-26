887d17dbae6028bb28bf628c7cff289cd70231cb
Jeroen Dekkers <jeroen@dekkers.ch>
2023-09-26 16:11:58 +0200

    Only start granian when uwsgi was requested

--- rocky/entrypoint.sh
+++ rocky/entrypoint.sh
@@ -8,7 +8,7 @@ if [ "$DATABASE_MIGRATION" = "1" ] || [  "$DATABASE_MIGRATION" = "true" ]; then
     python manage.py migrate --noinput
 fi
 
-if [ "$USE_GRANIAN" = "1" ] || [  "$USE_GRANIAN" = "true" ]; then
+if [ "$1" == "uwsgi" ] && { [ "$USE_GRANIAN" = "1" ] || [[  "$USE_GRANIAN" = "true" ]]; }; then
     exec granian --workers "$GRANIAN_WORKERS" --threads "$GRANIAN_THREADS" --interface wsgi rocky.wsgi:application --host 0.0.0.0
 else
     exec "$@"
