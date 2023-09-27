5ac80c03a594ac7fb0dadaed51e6c97808cc8909
Rieven <rsnmartis@gmail.com>
2023-09-27 15:35:16 +0200

    Fix views

--- rocky/reports/views.py
+++ rocky/reports/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import TemplateView
+from katalogus.client import get_katalogus
 from tools.view_helpers import BreadcrumbsMixin
 
 from octopoes.models import Reference
@@ -54,24 +55,31 @@ class BaseReportView(ReportBreadcrumbs, OctopoesView):
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.ooi_selection = request.GET.getlist("ooi", [])
-        self.report_types = get_report_types_for_oois(self.ooi_selection)
         self.report_types_selection = request.GET.getlist("report_type", [])
+        self.katalogus_client = get_katalogus(self.organization.code)
 
     def get_report_types(self):
         return [
             {"id": report_type.id, "name": report_type.name, "description": report_type.description}
-            for report_type in self.report_types
+            for report_type in get_report_types_for_oois(self.ooi_selection)
         ]
 
     def get_reports_data(self):
         report_data = {}
-        for ooi in self.ooi_selection:
-            for report in self.report_types_selection:
-                report = get_report_by_id(report)
-                if Reference.from_str(ooi).class_type in report.input_ooi_types:
-                    data, template = report(self.octopoes_api_connector).generate_data(ooi)
-                    report_data[f"{report.name}|{str(ooi)}"] = {"data": data, "template": template}
+        if self.ooi_selection and self.report_types_selection:
+            for ooi in self.ooi_selection:
+                for report in self.report_types_selection:
+                    report = get_report_by_id(report)
+                    if Reference.from_str(ooi).class_type in report.input_ooi_types:
+                        data, template = report(self.octopoes_api_connector).generate_data(ooi)
+                        report_data[f"{report.name}|{str(ooi)}"] = {"data": data, "template": template}
         return report_data
+
+    def get_required_optional_plugins(self):
+        plugins = get_plugins_for_report_ids(self.report_types_selection)
+        for plugin, value in plugins.items():
+            plugins[plugin] = [self.katalogus_client.get_plugin(plugin_id) for plugin_id in value]
+        return plugins
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -79,9 +87,8 @@ class BaseReportView(ReportBreadcrumbs, OctopoesView):
         context["report_types"] = self.get_report_types()
         context["ooi_type_form"] = OOITypeMultiCheckboxForReportForm(self.request.GET)
         if self.report_types_selection:
-            context["plugins"] = get_plugins_for_report_ids(self.report_types_selection)
-        if self.ooi_selection and self.report_types_selection:
-            context["report_data"] = self.get_reports_data()
+            context["plugins"] = self.get_required_optional_plugins()
+        context["report_data"] = self.get_reports_data()
         return context
 
 
@@ -103,13 +110,10 @@ class ReportTypeSelectionView(BaseReportView, TemplateView):
     template_name = "report_type_selection.html"
     current_step = 2
 
-    def error_url(self):
-        return redirect(reverse("report_oois_selection", kwargs={"organization_code": self.organization.code}))
-
     def get(self, request, *args, **kwargs):
         if not self.ooi_selection:
             messages.add_message(self.request, messages.ERROR, _("Select at least one OOI to proceed."))
-            return self.error_url()
+            return redirect(reverse("report_oois_selection", kwargs={"organization_code": self.organization.code}))
         return super().get(request, *args, **kwargs)
 
 
@@ -128,4 +132,4 @@ class ReportView(BaseReportView, TemplateView):
     """
 
     template_name = "report.html"
-    current_step = 3
+    current_step = 4
