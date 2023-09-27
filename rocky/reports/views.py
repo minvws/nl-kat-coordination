cdd4c084e662c7e5666b437d21e90d26809c803e
Rieven <rsnmartis@gmail.com>
2023-09-27 14:58:34 +0200

    Fixed views

--- rocky/reports/views.py
+++ rocky/reports/views.py
@@ -1,9 +1,6 @@
 from logging import getLogger
-from typing import Any
 
-from account.mixins import OrganizationView
 from django.contrib import messages
-from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -51,21 +48,53 @@ class ReportBreadcrumbs(BreadcrumbsMixin):
         return breadcrumbs[: self.current_step]
 
 
-class ReportOOISelectionView(ReportBreadcrumbs, BaseOOIListView, OrganizationView):
-    template_name = "report_oois_selection.html"
+class BaseReportView(ReportBreadcrumbs, OctopoesView):
     ooi_types = get_ooi_types_with_report()
-    current_step = 1
 
-    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
-        return super().get(request, *args, **kwargs)
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+        self.ooi_selection = request.GET.getlist("ooi", [])
+        self.report_types = get_report_types_for_oois(self.ooi_selection)
+        self.report_types_selection = request.GET.getlist("report_type", [])
+
+    def get_report_types(self):
+        return [
+            {"id": report_type.id, "name": report_type.name, "description": report_type.description}
+            for report_type in self.report_types
+        ]
+
+    def get_reports_data(self):
+        report_data = {}
+        for ooi in self.ooi_selection:
+            for report in self.report_types_selection:
+                report = get_report_by_id(report)
+                if Reference.from_str(ooi).class_type in report.input_ooi_types:
+                    data, template = report(self.octopoes_api_connector).generate_data(ooi)
+                    report_data[f"{report.name}|{str(ooi)}"] = {"data": data, "template": template}
+        return report_data
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["object_type_filters"] = OOITypeMultiCheckboxForReportForm(self.request.GET)
+        context["oois"] = self.ooi_selection
+        context["report_types"] = self.get_report_types()
+        context["ooi_type_form"] = OOITypeMultiCheckboxForReportForm(self.request.GET)
+        if self.report_types_selection:
+            context["plugins"] = get_plugins_for_report_ids(self.report_types_selection)
+        if self.ooi_selection and self.report_types_selection:
+            context["report_data"] = self.get_reports_data()
         return context
 
 
-class ReportTypeSelectionView(ReportBreadcrumbs, OrganizationView, TemplateView):
+class ReportOOISelectionView(BaseReportView, BaseOOIListView):
+    """
+    Select OOIs from list to include in report.
+    """
+
+    template_name = "report_oois_selection.html"
+    current_step = 1
+
+
+class ReportTypeSelectionView(BaseReportView, TemplateView):
     """
     Shows all possible report types from a list of OOIs.
     Chooses report types to generate a report.
@@ -74,19 +103,8 @@ class ReportTypeSelectionView(ReportBreadcrumbs, OrganizationView, TemplateView)
     template_name = "report_type_selection.html"
     current_step = 2
 
-    def setup(self, request, *args, **kwargs):
-        super().setup(request, *args, **kwargs)
-        self.ooi_selection = request.GET.getlist("ooi", [])
-        self.report_types = get_report_types_for_oois(self.ooi_selection)
-
     def error_url(self):
         return redirect(reverse("report_oois_selection", kwargs={"organization_code": self.organization.code}))
-
-    def get_report_types(self):
-        return [
-            {"id": report_type.id, "name": report_type.name, "description": report_type.description}
-            for report_type in self.report_types
-        ]
 
     def get(self, request, *args, **kwargs):
         if not self.ooi_selection:
@@ -94,50 +112,20 @@ class ReportTypeSelectionView(ReportBreadcrumbs, OrganizationView, TemplateView)
             return self.error_url()
         return super().get(request, *args, **kwargs)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["oois"] = self.ooi_selection
-        context["report_types"] = self.get_report_types()
-        return context
 
+class ReportSetupScanView(BaseReportView, TemplateView):
+    """
+    Show required and optional plugins to start scans to generate OOIs to include in report.
+    """
 
-class ReportSetupScanView(ReportBreadcrumbs, OrganizationView, TemplateView):
     template_name = "report_setup_scan.html"
-
-    def setup(self, request, *args, **kwargs):
-        super().setup(request, *args, **kwargs)
-        self.oois = request.GET.getlist("ooi", [])
-        self.report_types = request.GET.getlist("report_type", [])
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["plugins"] = get_plugins_for_report_ids(self.report_types)
-        return context
-
-
-class ReportView(ReportBreadcrumbs, OctopoesView, TemplateView):
-    template_name = "report.html"
     current_step = 3
 
-    def setup(self, request, *args, **kwargs):
-        super().setup(request, *args, **kwargs)
-        self.oois = request.POST.getlist("ooi", [])
-        self.report_types = request.POST.getlist("report_type", [])
 
-    def get_reports_data(self):
-        report_data = {}
-        for ooi in self.oois:
-            for report in self.report_types:
-                report = get_report_by_id(report)
-                if Reference.from_str(ooi).class_type in report.input_ooi_types:
-                    data, template = report(self.octopoes_api_connector).generate_data(ooi)
-                    report_data[f"{report.name}|{str(ooi)}"] = {"data": data, "template": template}
-        return report_data
+class ReportView(BaseReportView, TemplateView):
+    """
+    Shows the report generated from OOIS and report types.
+    """
 
-    def post(self, request, *args, **kwargs):
-        return self.get(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["report_data"] = self.get_reports_data()
-        return context
+    template_name = "report.html"
+    current_step = 3
