7b06cae2239a941fd6898e2a53ed60eabea085a7
ammar <ammar.abdulamir@gmail.com>
2023-09-29 12:42:03 +0200

    Fixed some tests in keiko

--- keiko/keiko/settings.py
+++ keiko/keiko/settings.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     templates_folder: DirectoryPath = Field("templates", description="Folder containing the templates")
     glossaries_folder: DirectoryPath = Field("glossaries", description="Folder containing the glossaries")
     assets_folder: DirectoryPath = Field("assets", description="Folder containing the assets")
-    reports_folder: DirectoryPath = Field("/reports", description="Output folder containing the reports")
+    reports_folder: DirectoryPath = Field("reports", description="Output folder containing the reports")
 
     span_export_grpc_endpoint: Optional[AnyHttpUrl] = Field(
         None, description="OpenTelemetry endpoint", validation_alias="SPAN_EXPORT_GRPC_ENDPOINT"
