dff988a56bd258ff16b4fa4afaa6fd136eb927c8
Noam Blitz <noamblitz@gmail.com>
2023-10-02 18:59:32 +0200

    First version of tlsreport

--- rocky/reports/report_types/tls_report/report.py
+++ rocky/reports/report_types/tls_report/report.py
@@ -1,11 +1,13 @@
+from logging import getLogger
 from typing import List
 
 from django.utils.translation import gettext_lazy as _
 
-from octopoes.models import OOI
-from octopoes.models.ooi.dns.zone import Hostname
-from octopoes.models.ooi.service import IPService
+from octopoes.models import Reference
+from octopoes.models.ooi.service import IPService, TLSCipher
 from reports.report_types.definitions import Report
+
+logger = getLogger(__name__)
 
 
 class TLSReport(Report):
@@ -14,8 +16,20 @@ class TLSReport(Report):
     description: str = _("TLS reports assess the security of data encryption and transmission protocols.")
     required_boefjes: List = []
     optional_boefjes: List = []
-    input_ooi_types = {IPService, Hostname}
+    input_ooi_types = {IPService}
     html_template_path = "tls_report/report.html"
 
-    def generate_data(self, input_ooi: OOI):
-        return {"mock_oois": ["mock_ooi2", "mock_ooi3"]}, self.html_template_path
+    def generate_data(self, input_ooi: str):
+        suites = {}
+        ref = Reference.from_str(input_ooi)
+        tree = self.octopoes_api_connector.get_tree(ref, depth=3, types={TLSCipher}).store
+        for pk, ooi in tree.items():
+            logger.error(pk)
+            logger.error(ooi.ooi_type)
+            if ooi.ooi_type == "TLSCipher":
+                suites = ooi.suites
+
+        return {
+            "input_ooi": input_ooi,
+            "suites": suites,
+        }, self.html_template_path
