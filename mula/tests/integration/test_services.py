7514a5dfa4c916fc77d65b82eab028a8134c4a01
JP Bruins Slot <jpbruinsslot@gmail.com>
2023-10-05 17:51:56 +0200

    Start with external services tests

---
+++ mula/tests/integration/test_services.py
@@ -0,0 +1,19 @@
+import unittest
+
+from scheduler import config
+from scheduler.connectors import services
+
+
+class BytesTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.config = config.settings.Settings()
+        self.service_bytes = services.Bytes(
+            host=str(self.config.host_bytes),
+            user=self.config.host_bytes_user,
+            password=self.config.host_bytes_password,
+            source="scheduler_test",
+        )
+
+    def test_login(self):
+        self.service_bytes.login()
