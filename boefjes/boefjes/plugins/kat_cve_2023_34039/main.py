974926633f1a2884447a63b9fe892711046d7fd4
Jan Klopper <janklopper+underdark@gmail.com>
2023-10-10 18:32:24 +0200

    add remote executable call to make the ssh connectoin end immediately if connected.

--- boefjes/boefjes/plugins/kat_cve_2023_34039/main.py
+++ boefjes/boefjes/plugins/kat_cve_2023_34039/main.py
@@ -46,11 +46,14 @@ def run(boefje_meta: BoefjeMeta) -> List[Tuple[set, Union[str, bytes]]]:
                 "UserKnownHostsFile=/dev/null",
                 "-o",
                 "BatchMode=yes",
+                "exit"
                 "2>/dev/null",
             ]
             try:
                 ssh_command = " ".join(ssh_command)
                 coutput = os.system(ssh_command)
+                if couput not in (0, 32512): # 0, it worked, 32512 `exit` does not exists but we did connect
+                    continue
                 return [
                     (
                         set(),
