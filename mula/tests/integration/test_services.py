1a8d354bc2c674bd8bc5ac542539a43a946b4da9
JP Bruins Slot <jpbruinsslot@gmail.com>
2023-10-16 13:04:22 +0200

    Got containers to work

--- mula/tests/integration/test_services.py
+++ mula/tests/integration/test_services.py
@@ -2,13 +2,14 @@ import unittest
 
 from scheduler import config
 from scheduler.connectors import services
+from scheduler.utils import remove_trailing_slash
 
 
 class BytesTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.config = config.settings.Settings()
         self.service_bytes = services.Bytes(
-            host=str(self.config.host_bytes),
+            host=remove_trailing_slash(str(self.config.host_bytes)),
             user=self.config.host_bytes_user,
             password=self.config.host_bytes_password,
             source="scheduler_test",
