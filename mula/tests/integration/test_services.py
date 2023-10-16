69fd8597bb7fcbbefd1716a594ec8686d1007e0a
JP Bruins Slot <jpbruinsslot@gmail.com>
2023-10-16 17:15:15 +0200

    Made katalogus integration work

--- mula/tests/integration/test_services.py
+++ mula/tests/integration/test_services.py
@@ -17,3 +17,31 @@ class BytesTestCase(unittest.TestCase):
 
     def test_login(self):
         self.service_bytes.login()
+
+        # TODO: check headers
+
+    def test_login_expired_token(self):
+        pass
+
+    # TODO: do we want to mock the response here?
+    def test_get_last_run_boefje(self):
+        pass
+
+    # TODO: do we want to mock the response here?
+    def test_get_last_run_boefje_by_organisation_id(self):
+        pass
+
+
+class KatalogusTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.config = config.settings.Settings()
+        self.service_katalogus = services.Katalogus(
+            host=remove_trailing_slash(str(self.config.host_katalogus)),
+            source="scheduler_test",
+            cache_ttl=self.config.katalogus_cache_ttl,
+        )
+
+    def test_get_organisations(self):
+        orgs = self.service_katalogus.get_organisations()
+        self.assertIsInstance(orgs, list)
+        self.assertEqual(len(orgs), 0)
