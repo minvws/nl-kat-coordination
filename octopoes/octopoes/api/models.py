203825535c24103e7b7d654f8289a9e5d87e08de
ammar <ammar.abdulamir@gmail.com>
2023-10-17 22:34:15 +0200

    Removed unused import
    Signed-off-by: ammar <ammar.abdulamir@gmail.com>

--- octopoes/octopoes/api/models.py
+++ octopoes/octopoes/api/models.py
@@ -1,10 +1,10 @@
 import uuid
 from datetime import datetime
-from typing import Any, List, Optional, Annotated
+from typing import Any, List, Optional
 
 from pydantic import BaseModel, Field
 
-from octopoes.models import OOI, Reference
+from octopoes.models import Reference
 from octopoes.models.datetime import TimezoneAwareDatetime
 from octopoes.models.types import OOIType
 
