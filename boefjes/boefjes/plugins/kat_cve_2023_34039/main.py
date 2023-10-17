bbaaf9c09670e60e33786d105650d6ec1c4d17ca
Jeroen Dekkers <jeroen@dekkers.ch>
2023-10-17 17:18:07 +0200

    Fix import (#1917)

--- boefjes/boefjes/plugins/kat_cve_2023_34039/main.py
+++ boefjes/boefjes/plugins/kat_cve_2023_34039/main.py
@@ -17,7 +17,7 @@ import logging
 import os
 from typing import List, Tuple, Union
 
-from boefjes.boefjes.job_models import BoefjeMeta
+from boefjes.job_models import BoefjeMeta
 
 
 def run(boefje_meta: BoefjeMeta) -> List[Tuple[set, Union[str, bytes]]]:
