6f8dfb1fe8856c0baa6db8e6f2deb0c654e29fd2
JP Bruins Slot <jpbruinsslot@gmail.com>
2023-10-23 13:48:53 +0200

    Remove octopoes integration

--- mula/tests/integration/test_services.py
+++ mula/tests/integration/test_services.py
@@ -1,14 +1,13 @@
 import copy
 import time
 import unittest
-import urllib.parse
 from unittest import mock
 
 from scheduler import config, models, storage
 from scheduler.connectors import services
 from scheduler.utils import remove_trailing_slash
 
-from tests.factories import OrganisationFactory, PluginFactory
+from tests.factories import PluginFactory
 
 
 class BytesTestCase(unittest.TestCase):
@@ -26,15 +25,6 @@ class BytesTestCase(unittest.TestCase):
 
         self.assertIsNotNone(self.service_bytes.headers)
         self.assertIsNotNone(self.service_bytes.headers.get("Authorization"))
-
-    def test_login_token_refresh(self):
-        self.service_bytes.login()
-        initial_token = copy.deepcopy(self.service_bytes.headers.get("Authorization"))
-
-        self.service_bytes.login()
-        refresh_token = copy.deepcopy(self.service_bytes.headers.get("Authorization"))
-
-        self.assertNotEqual(initial_token, refresh_token)
 
     def test_expired_token_refresh(self):
         self.service_bytes.get_last_run_boefje(
@@ -55,14 +45,6 @@ class BytesTestCase(unittest.TestCase):
 
         self.assertNotEqual(initial_token, refresh_token)
 
-    # TODO: do we want to mock the response here?
-    def test_get_last_run_boefje(self):
-        pass
-
-    # TODO: do we want to mock the response here?
-    def test_get_last_run_boefje_by_organisation_id(self):
-        pass
-
 
 class KatalogusTestCase(unittest.TestCase):
     def setUp(self) -> None:
@@ -81,17 +63,12 @@ class KatalogusTestCase(unittest.TestCase):
         self.service_katalogus.organisations_normalizer_type_cache.reset()
         models.Base.metadata.drop_all(self.dbconn.engine)
 
-    def test_get_organisations(self):
-        orgs = self.service_katalogus.get_organisations()
-        self.assertIsInstance(orgs, list)
-        self.assertEqual(len(orgs), 0)
-
     @mock.patch("scheduler.connectors.services.Katalogus.get_organisations")
     def test_flush_organisations_plugin_cache(self, mock_get_organisations):
         # Mock
         mock_get_organisations.return_value = [
-             OrganisationFactory(id="org-1"),
-             OrganisationFactory(id="org-2"),
+            models.Organisation(id="org-1", name="org-1"),
+            models.Organisation(id="org-2", name="org-2"),
         ]
 
         # Act
@@ -118,8 +95,8 @@ class KatalogusTestCase(unittest.TestCase):
     def test_flush_organisations_boefje_type_cache(self, mock_get_organisations, mock_get_plugins_by_organisation):
         # Mock
         mock_get_organisations.return_value = [
-            OrganisationFactory(id="org-1"),
-            OrganisationFactory(id="org-2"),
+            models.Organisation(id="org-1", name="org-1"),
+            models.Organisation(id="org-2", name="org-2"),
         ]
 
         mock_get_plugins_by_organisation.return_value = [
@@ -146,8 +123,8 @@ class KatalogusTestCase(unittest.TestCase):
     def test_flush_organisations_normalizer_type_cache(self, mock_get_organisations, mock_get_plugins_by_organisation):
         # Mock
         mock_get_organisations.return_value = [
-            OrganisationFactory(id="org-1"),
-            OrganisationFactory(id="org-2"),
+            models.Organisation(id="org-1", name="org-1"),
+            models.Organisation(id="org-2", name="org-2"),
         ]
 
         mock_get_plugins_by_organisation.return_value = [
@@ -168,12 +145,6 @@ class KatalogusTestCase(unittest.TestCase):
         self.assertIsNotNone(self.service_katalogus.organisations_normalizer_type_cache.get("org-2"))
         self.assertIsNotNone(self.service_katalogus.organisations_normalizer_type_cache.get("org-2").get("Hostname"))
         self.assertEqual(len(self.service_katalogus.organisations_normalizer_type_cache.get("org-2").get("Hostname")), 2)
-
-    def test_get_plugins_by_org_id(self):
-        pass
-
-    def test_get_plugins_by_org_id_expired(self):
-        pass
 
     @mock.patch("scheduler.connectors.services.Katalogus.get_plugins_by_organisation")
     def test_get_new_boefjes_by_org_id(self, mock_get_plugins_by_organisation):
@@ -218,24 +189,3 @@ class KatalogusTestCase(unittest.TestCase):
         self.assertIsNotNone(self.service_katalogus.organisations_new_boefjes_cache.get("org-1").get("plugin-5"))
         self.assertEqual(len(new_boefjes), 1)
         self.assertEqual(new_boefjes[0].id, "plugin-5")
-
-
-class OctopoesTestCase(unittest.TestCase):
-    def setUp(self):
-        self.config = config.settings.Settings()
-        self.service_octopoes = services.Octopoes(
-            host=remove_trailing_slash(str(self.config.host_octopoes)),
-            source="scheduler_test",
-            orgs=[OrganisationFactory(id="org-1"), OrganisationFactory(id="org-2")],
-            timeout=5,
-        )
-
-    def test_is_host_available(self):
-        parsed_url = urllib.parse.urlparse(str(self.config.host_octopoes))
-        hostname, port = parsed_url.hostname, parsed_url.port
-        response = self.service_octopoes.is_host_available(hostname, port)
-        self.assertTrue(response)
-
-    def test_is_healthy(self):
-        response = self.service_octopoes.is_healthy()
-        self.assertTrue(response)
