116b9bcfe74b741d935f4bb2fc6ed5ec145f5715
JP Bruins Slot <jpbruinsslot@gmail.com>
2023-11-14 16:03:24 +0100

    Update benchmark script

--- mula/tests/scripts/benchmark.py
+++ mula/tests/scripts/benchmark.py
@@ -1,5 +1,7 @@
 import argparse
 import logging
+import subprocess
+import threading
 import time
 
 import psutil
@@ -7,6 +9,7 @@ import requests
 
 SCHEDULER_API = "http://localhost:8004"
 
+logger = logging.getLogger(__name__)
 
 def are_tasks_done():
     resp_tasks_stats = requests.get(
@@ -16,12 +19,12 @@ def are_tasks_done():
     try:
         resp_tasks_stats.raise_for_status()
     except requests.exceptions.HTTPError:
-        print("Error getting tasks")
+        logger.error("Error getting tasks")
         raise
 
     tasks_stats = resp_tasks_stats.json()
     if not tasks_stats:
-        print("No tasks found")
+        logger.debug("No tasks found")
         return False
 
     return all(tasks_stats[hour].get("queued") <= 0 for hour in tasks_stats)
@@ -35,21 +38,55 @@ def parse_stats():
     try:
         resp_tasks_stats.raise_for_status()
     except requests.exceptions.HTTPError:
-        print("Error getting tasks")
+        logger.error("Error getting tasks")
         raise
 
     tasks_stats = resp_tasks_stats.json()
     for hour in tasks_stats:
-        print(hour)
+        queued = tasks_stats[hour].get("queued")
+        running = tasks_stats[hour].get("running")
+        failed = tasks_stats[hour].get("failed")
+        completed = tasks_stats[hour].get("completed")
+        logger.info(f"HOUR {hour}, QUEUED {queued}, RUNNING {running}, FAILED {failed}, COMPLETED {completed}")
 
 
-def parse_logs():
-    pass
+def capture_logs(pid: int, output_file: str):
+    try:
+        with open(output_file, 'a') as file:
+            process = subprocess.Popen(['tail', '-f', f'/proc/{pid}/fd/1'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+            while process.poll() is None:
+                output_line = process.stdout.readline().strip()
+                error_line = process.stderr.readline().strip()
+
+                if output_line:
+                    file.write(f"STDOUT: {output_line}\n")
+                if error_line:
+                    file.write(f"STDERR: {error_line}\n")
+    except Exception as e:
+        logger.error(f"Error capturing logs: {e}")
+        raise
+
+
+def parse_logs(path: str):
+    # Check if there were any errors in the logs
+    count = 0
+    with open(path, encoding="utf-8") as file:
+        for line in file:
+            if "ERROR" in line:
+                count += 1
+                logger.info(line)
+            if "Traceback" in line:
+                count += 1
+                logger.info(line)
+
+    if count > 0:
+        logger.error(f"Found {count} errors in the logs")
 
 
 def collect_cpu(pid: int):
     process = psutil.Process(pid)
     return process.cpu_percent()
+
 
 def collect_memory(pid: int):
     process = psutil.Process(pid)
@@ -57,26 +94,39 @@ def collect_memory(pid: int):
 
 
 def run(pid: int):
-    while not are_tasks_done():
-        print("Tasks are not done yet")
+    # Start capturing logs
+    th_capture_logs = threading.Thread(target=capture_logs, args=(pid, "logs.txt"))
+    th_capture_logs.start()
 
-        memory = collect_memory(pid)
-        print(f"Memory usage: {memory:.2f} MB")
+    # Wait for tasks to finish
+    while not are_tasks_done():
+        logger.debug("Tasks are not done yet")
 
         cpu = collect_cpu(pid)
-        print(f"CPU usage: {cpu:.2f} %")
+        memory = collect_memory(pid)
+        logger.info(f"CPU {cpu}, MEM {memory:.2f}")
+
+        # Parse stats
+        parse_stats()
 
         time.sleep(10)
         continue
 
-    print("Tasks are done")
+    logger.debug("Tasks are done")
 
-    parse_logs()
+    # Stop capturing logs
+    th_capture_logs.join(timeout=TIMEOUT_FOR_LOG_CAPTURE)
 
+    # Parse stats
     parse_stats()
+
+    # Parse logs
+    parse_logs("logs.txt")
 
 
 if __name__ == "__main__":
+    TIMEOUT_FOR_LOG_CAPTURE = 5
+
     # Setup command line interface
     parser = argparse.ArgumentParser(description="Check")
 
@@ -107,7 +157,7 @@ if __name__ == "__main__":
 
     logging.basicConfig(
         level=default_loglevel,
-        format="%(asctime)s %(name)-52s %(levelname)-8s %(message)s",
+        format="%(asctime)s %(name)-10s %(levelname)-8s %(message)s",
     )
 
     run(args.pid)
