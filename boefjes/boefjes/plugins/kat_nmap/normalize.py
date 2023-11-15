5945dd3b6602ce6adebe32260c1de603f9dae2cc
Benny <mail2benny@gmail.com>
2023-11-15 14:45:48 +0100

    Initial fix for issue #2037

--- boefjes/boefjes/plugins/kat_nmap/normalize.py
+++ boefjes/boefjes/plugins/kat_nmap/normalize.py
@@ -44,9 +44,9 @@ def get_ip_ports_and_service(host: NmapHost, network: Network, netblock: Referen
             yield ip_port
 
             service_name = service.service
-            if port == 80:
+            if service_name.startswith("http"):
                 service_name = "http"
-            if port == 443:
+            if service_name.startswith("ssl"):
                 service_name = "https"
 
             port_service = Service(name=service_name)
