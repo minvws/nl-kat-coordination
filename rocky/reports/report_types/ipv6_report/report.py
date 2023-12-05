18cff5351a13e15247fc7af3da2fa11f616016f1
Noam Blitz <noamblitz@gmail.com>
2023-12-05 11:33:20 +0100

    add plugins to reports

--- rocky/reports/report_types/ipv6_report/report.py
+++ rocky/reports/report_types/ipv6_report/report.py
@@ -24,7 +24,7 @@ class IPv6Report(Report):
     id = "ipv6-report"
     name = _("IPv6 Report")
     description = _("Check whether hostnames point to ipv6 addresses.")
-    plugins = {"required": [], "optional": []}
+    plugins = {"required": ["dns-records"], "optional": []}
     input_ooi_types = {Hostname, IPAddressV4, IPAddressV6}
     template_path = "ipv6_report/report.html"
 
