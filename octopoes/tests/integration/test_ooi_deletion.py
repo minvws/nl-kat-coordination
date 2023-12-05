5392f66e21ecb766e1f0712f39df3b850e69bb32
Benny <mail2benny@gmail.com>
2023-12-05 16:22:18 +0100

    Formatting an ugly hack is really important

--- octopoes/tests/integration/test_ooi_deletion.py
+++ octopoes/tests/integration/test_ooi_deletion.py
@@ -1,7 +1,7 @@
 import os
+import time
 import uuid
 from datetime import datetime
-import time
 
 import pytest
 
@@ -55,8 +55,8 @@ def test_hostname_nxd_ooi(octopoes_api_connector: OctopoesAPIConnector, valid_ti
     octopoes_api_connector.delete(network.reference)
     octopoes_api_connector.delete(hostname.reference)
 
-    #This sleep is here because otherwise on some systems this test will fail
-    #Delete when issue #2083 is resolved...
+    # This sleep is here because otherwise on some systems this test will fail
+    # Delete when issue #2083 is resolved...
     time.sleep(1)
     assert len(octopoes_api_connector.list_origins(task_id={})) < bits_size
 
