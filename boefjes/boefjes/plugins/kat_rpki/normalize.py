98d9fb65bae3dfe571fc92cb17c520746fe39e78
noamblitz <43830693+noamblitz@users.noreply.github.com>
2023-12-08 09:16:49 +0100

    Adds RPKI report (#2104)

--- boefjes/boefjes/plugins/kat_rpki/normalize.py
+++ boefjes/boefjes/plugins/kat_rpki/normalize.py
@@ -10,8 +10,14 @@ def run(normalizer_meta: NormalizerMeta, raw: Union[bytes, str]) -> Iterable[OOI
     results = json.loads(raw)
     ooi = Reference.from_str(normalizer_meta.raw_data.boefje_meta.input_ooi)
 
-    if not results["valid"] or not results["exists"]:
+    if not results["exists"]:
         ft = KATFindingType(id="KAT-NO-RPKI")
+        f = Finding(finding_type=ft.reference, ooi=ooi)
+        yield ft
+        yield f
+
+    if not results["valid"]:
+        ft = KATFindingType(id="KAT-INVALID-RPKI")
         f = Finding(finding_type=ft.reference, ooi=ooi)
         yield ft
         yield f
