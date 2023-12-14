a822d3f11fbed3f1f0b4c351946ab4a5540f6476
Jeroen Dekkers <jeroen@dekkers.ch>
2023-12-14 11:44:10 +0100

    Rename nmap-tcp back to nmap and make shodan optional

--- rocky/reports/report_types/systems_report/report.py
+++ rocky/reports/report_types/systems_report/report.py
@@ -23,7 +23,7 @@ class SystemReport(Report):
     id = "systems-report"
     name = _("System Report")
     description = _("Combine IP addresses, hostnames and services into systems.")
-    plugins = {"required": ["dns-records", "nmap-udp", "nmap-tcp"], "optional": []}
+    plugins = {"required": ["dns-records", "nmap-udp", "nmap"], "optional": []}
     input_ooi_types = {Hostname, IPAddressV4, IPAddressV6}
     template_path = "systems_report/report.html"
 
