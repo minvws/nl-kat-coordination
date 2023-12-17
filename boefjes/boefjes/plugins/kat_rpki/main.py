07b3fdf621e84c36752161f237211185bd587468
Jan Klopper <janklopper+underdark@gmail.com>
2023-12-17 18:35:50 +0100

    add  caching / update mechanism to rpki boefje
    Untested, dry "as the Sahara" Code.

--- boefjes/boefjes/plugins/kat_rpki/main.py
+++ boefjes/boefjes/plugins/kat_rpki/main.py
@@ -1,21 +1,30 @@
 """Boefje script for validating vrps records based on code from @trideeindhoven"""
+from os import getenv
 import json
-from datetime import datetime
-from typing import List, Tuple, Union
+import requests
 
+from datetime import datetime
+from typing import List, Tuple, Union, Bool, Dict
 from netaddr import IPAddress, IPNetwork
 
 from boefjes.job_models import BoefjeMeta
 
-FINDING_TYPE_PATH = "boefjes/plugins/kat_rpki/rpki.json"
-
+RPKI_PATH = "boefjes/plugins/kat_rpki/rpki.json"
+RPKI_META_PATH = "boefjes/plugins/kat_rpki/rpki-meta.json"
+RPKI_SOURCE_URL = "https://console.rpki-client.org/vrps.json"
+RPKI_CACHE_TIMEOUT = 1800 # in seconds
 
 def run(boefje_meta: BoefjeMeta) -> List[Tuple[set, Union[bytes, str]]]:
     input_ = boefje_meta.arguments["input"]
     ip = input_["address"]
 
-    with open(FINDING_TYPE_PATH) as json_file:
-        rpki_json = json.load(json_file)
+    if not os.path.exists(RPKI_PATH):
+        rpki_json = refresh_rpki()
+    elif not validate_age():
+        rpki_json = refresh_rpki()
+    else:
+        with open(RPKI_PATH) as json_file:
+           rpki_json = json.load(json_file)
 
     exists = False
     valid = False
@@ -24,10 +33,31 @@ def run(boefje_meta: BoefjeMeta) -> List[Tuple[set, Union[bytes, str]]]:
         if IPAddress(ip) in IPNetwork(roa["prefix"]):
             exists = True
             expires = datetime.fromtimestamp(roa["expires"])
-            roas.append({"prefix": roa["prefix"], "expires": expires.strftime("%Y-%m-%dT%H:%M"), "ta": roa["ta"]})
-            if expires > datetime.utcnow():
+            roas.append({"prefix": roa["prefix"],
+                         "expires": expires.strftime("%Y-%m-%dT%H:%M"), "ta": roa["ta"]})
+            if expires > now:
                 valid = True
 
     results = {"vrps_records": roas, "valid": valid, "exists": exists}
 
     return [(set(), json.dumps(results))]
+
+def validate_age() -> Bool:
+    now = datetime.utcnow()
+    maxage = getenv("RPKI_CACHE_TIMEOUT") or RPKI_CACHE_TIMEOUT
+     with open(RPKI_META_PATH) as meta_file:
+        meta = json.load(meta_file)
+    cached_file_timestamp = datetime.strptime(meta["timestamp"], "%Y-%m-%dT%H:%M:%SZ")
+    return (now - cached_file_timestamp).total_seconds()) > maxage
+
+def refresh_rpki() -> Dict:
+    source_url = getenv("RPKI_SOURCE_URL") or RPKI_SOURCE_URL
+    response = requests.get(source_url, allow_redirects=True)
+    response.raise_for_status()
+    with open(RPKI_PATH, "w") as prki_file:
+        prki_file.write(response.content)
+    metadata = {"timestamp":  datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+                "source": source_url}
+    with open(RPKI_META_PATH, "w") as meta_file:
+        meta_file.write(json.dump(metadata))
+    return json.loads(response.content)
