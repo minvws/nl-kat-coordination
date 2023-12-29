cedfe32defdd7c98686bf64cadb4180cd9ee9d35
Ammar <ammar.abdulamir@gmail.com>
2023-12-29 20:02:24 +0100

    Langs and linting

--- rocky/reports/views/base.py
+++ rocky/reports/views/base.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional, Set, Type, TypedDict, Union
+from typing import Any, Dict, List, Optional, Set, Type, Union
 
 from account.mixins import OrganizationView
 from django.forms import Form
