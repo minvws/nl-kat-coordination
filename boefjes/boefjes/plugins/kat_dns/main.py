98624dfd0e855db53eeffab5b0a1363932d03b5f
Jan Klopper <janklopper+underdark@gmail.com>
2024-01-09 16:22:47 +0100

    cleanup dns resolving, and add new types.

--- boefjes/boefjes/plugins/kat_dns/main.py
+++ boefjes/boefjes/plugins/kat_dns/main.py
@@ -21,40 +21,39 @@ def run(boefje_meta: BoefjeMeta) -> List[Tuple[set, Union[bytes, str]]]:
     hostname = boefje_meta.arguments["input"]["name"]
 
     requested_dns_name = dns.name.from_text(hostname)
-    zone_soa_record = get_parent_zone_soa(requested_dns_name)
+    resolver = dns.resolver.Resolver()
+    resolver.nameservers = [str(settings.remote_ns)]
+
+    zone_soa_record = get_parent_zone_soa(resolver, requested_dns_name)
 
     answers = [
         zone_soa_record,
     ]
 
-    dns_record_types = ["A", "AAAA", "CAA", "TXT", "MX", "NS", "CNAME", "DNAME"]
+    dns_record_types = ["A", "AAAA", "CAA", "CERT", "PR", "SRV" "TXT", "MX", "NS", "CNAME", "DNAME"]
     for type_ in dns_record_types:
         try:
-            resolver = dns.resolver.Resolver()
-            resolver.nameservers = [str(settings.remote_ns)]
             answer: Answer = resolver.resolve(hostname, type_)
             answers.append(answer)
-        except dns.resolver.NoAnswer:
+        except (dns.resolver.NoAnswer, dns.resolver.Timeout):
             pass
         except dns.resolver.NXDOMAIN:
             return [(set(), "NXDOMAIN")]
-        except dns.resolver.Timeout:
-            pass
 
     answers_formatted = [f"RESOLVER: {answer.nameserver}\n{answer.response}" for answer in answers]
 
     results = {
         "dns_records": "\n\n".join(answers_formatted),
-        "dmarc_response": get_email_security_records(hostname, "_dmarc"),
-        "dkim_response": get_email_security_records(hostname, "_domainkey"),
+        "dmarc_response": get_email_security_records(resolver, hostname, "_dmarc"),
+        "dkim_response": get_email_security_records(resolver, hostname, "_domainkey"),
     }
     return [(set(), json.dumps(results))]
 
 
-def get_parent_zone_soa(name: Name) -> Answer:
+def get_parent_zone_soa(resolver: dns.resolver.Resolver, name: Name) -> Answer:
     while True:
         try:
-            return dns.resolver.resolve(name, dns.rdatatype.SOA)
+            return resolver.resolve(name, dns.rdatatype.SOA)
         except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer):
             pass
 
@@ -64,10 +63,8 @@ def get_parent_zone_soa(name: Name) -> Answer:
             raise ZoneNotFoundException
 
 
-def get_email_security_records(hostname: str, record_subdomain: str) -> str:
+def get_email_security_records(resolver: dns.resolver.Resolver, hostname: str, record_subdomain: str) -> str:
     try:
-        resolver = dns.resolver.Resolver()
-        resolver.nameservers = [str(settings.remote_ns)]
         answer = resolver.resolve(f"{record_subdomain}.{hostname}", "TXT", raise_on_no_answer=False)
         return answer.response.to_text()
     except dns.resolver.NXDOMAIN:
