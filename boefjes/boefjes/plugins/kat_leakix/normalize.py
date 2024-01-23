64472b5fd60c2ac11ca24e737afb8f2a3b181792
Jan Klopper <janklopper+underdark@gmail.com>
2024-01-23 17:50:08 +0100

    dont mix returns and yields.

--- boefjes/boefjes/plugins/kat_leakix/normalize.py
+++ boefjes/boefjes/plugins/kat_leakix/normalize.py
@@ -35,7 +35,7 @@ def run(normalizer_meta: NormalizerMeta, raw: Union[bytes, str]) -> Iterable_[OO
 
     boefje_meta = normalizer_meta.raw_data.boefje_meta
     pk_ooi = Reference.from_str(boefje_meta.input_ooi)
-    network = Network(name="internet").reference
+    network_reference = Network(name="internet").reference
 
     for event in results:
         # TODO: add event["time"] to results. This is the time the event was first seen. Date of last scan is not
@@ -44,93 +44,91 @@ def run(normalizer_meta: NormalizerMeta, raw: Union[bytes, str]) -> Iterable_[OO
         # TODO: ssh, ssl
 
         # reset loop
-        event_ooi = pk_ooi
+        event_ooi_reference = pk_ooi
 
         # Autonomous System
         as_ooi = None
         if event["network"]["asn"]:
             as_ooi = handle_autonomous_system(event)
+            yield as_ooi
 
         if event["ip"]:
-            event_ooi, ip_port_ooi = handle_ip(event, network, as_ooi)
-
+            for ooi in list(handle_ip(event, network_reference, as_ooi.reference)):
+                yield ooi
+            # we only need the last ooi's reference
+            event_ooi_reference = ooi.reference
+            ip_port_ooi_reference = event_ooi_reference
+                       
         if event["host"]:
-            event_ooi = handle_hostname(event, network)
+            host_ooi = handle_hostname(event, network_reference)
+            yield ooi
+            event_ooi_reference = ooi.reference
 
-        event_ooi, software_ooi = handle_software(event, event_ooi)
-        # Potential TODO: add vendor
+        software_ooi = None
+        for ooi in list(handle_software(event, event_ooi_reference)):
+            yield ooi
+            if type(ooi) is Software:
+                # Leak and CVE Events are bound to the software, not the softwareinstance
+                software_ooi = ooi
 
         # Leak
-        handle_leak(event, event_ooi, software_ooi)
+        yield from handle_leak(event, event_ooi_reference, software_ooi)
 
         # CVES
-        handle_tag(event, software_ooi, ip_port_ooi)
+        yield from handle_tag(event, software_ooi.reference, ip_port_ooi_reference)
 
 
 def handle_autonomous_system(event):
     as_number = event["network"]["asn"]
     as_name = event["network"]["organization_name"]
-    as_ooi = AutonomousSystem(number=as_number, name=as_name) if as_name else AutonomousSystem(number=as_number)
-    yield as_ooi
-    return as_ooi
+    return AutonomousSystem(number=as_number, name=as_name) if as_name else AutonomousSystem(number=as_number)
 
 
-def handle_ip(event, network, as_ooi):
+def handle_ip(event, network_reference, as_ooi_reference):
     # Store IP
     ip = event["ip"]
     ipvx = ipaddress.ip_address(ip)
     netblock_range = event["network"]["network"].split("/")
     if ipvx.version == 4:
-        iptype = IPAddressV4
-        blocktype = IPV4NetBlock
+        ip_type = IPAddressV4
+        block_type = IPV4NetBlock
     else:
-        iptype = IPAddressV6
-        blocktype = IPV6NetBlock
+        ip_type = IPAddressV6
+        block_type = IPV6NetBlock
 
-    ip_ooi = iptype(address=ip, network=network)
+    ip_ooi = ip_type(address=ip, network=network_reference)
     yield ip_ooi
-    event_ooi = ip_ooi.reference
+    
     if as_ooi and len(netblock_range) == 2:
-        yield blocktype(
-            network=network,
-            start_ip=ip_ooi,
+        yield block_type(
+            network=network_reference,
+            start_ip=ip_ooi.reference,
             mask=netblock_range[1],
-            announced_by=as_ooi.reference,
+            announced_by=as_ooi_reference,
         )
 
     # Store port
-    protocol = event["protocol"]
-    if protocol != "udp":
-        protocol = "tcp"
-
     ip_port_ooi = IPPort(
         address=ip_ooi.reference,
-        protocol=Protocol(protocol),
+        protocol=Protocol("tcp" if event["protocol"] != "udp" else "udp"),
         port=int(event["port"]),
         state=PortState("open"),
     )
     yield ip_port_ooi
-    event_ooi = ip_port_ooi.reference
-    return event_ooi, ip_port_ooi
 
 
-def handle_hostname(event, network):
+def handle_hostname(event, network_reference):
     try:
         ipvx = ipaddress.ip_address(event["ip"])
         if ipvx.version == 4:
-            ip_ooi = IPAddressV4(address=event["host"], network=network)
-        else:
-            ip_ooi = IPAddressV6(address=event["host"], network=network)
-        yield ip_ooi
-        return ip_ooi.reference
+            return IPAddressV4(address=event["host"], network=network_reference)
+        return IPAddressV6(address=event["host"], network=network_reference)
     except ValueError:
         # Not an IPAddress, so a hostname
-        hostname_ooi = Hostname(name=event["host"], network=network)
-        yield hostname_ooi
-        return hostname_ooi.reference
+        return Hostname(name=event["host"], network=network_reference)
 
 
-def handle_software(event, event_ooi):
+def handle_software(event, event_ooi_reference):
     software_args = {}
     software_args["name"] = event.get("service", {}).get("software", {}).get("name")
     software_args["version"] = event.get("service", {}).get("software", {}).get("version")
@@ -141,14 +139,11 @@ def handle_software(event, event_ooi):
     if software_args["name"]:
         software_ooi = Software(**{k: v for k, v in software_args if v})
         yield software_ooi
-        software_instance_ooi = SoftwareInstance(ooi=event_ooi, software=software_ooi.reference)
+        software_instance_ooi = SoftwareInstance(ooi=event_ooi_reference, software=software_ooi.reference)
         yield software_instance_ooi
-        event_ooi = software_instance_ooi.reference
-        return event_ooi, software_ooi
-    return event_ooi, None
 
 
-def handle_leak(event, event_ooi, software_ooi):
+def handle_leak(event, event_ooi_reference, software_ooi):
     leak_severity = event.get("leak", {}).get("severity")
     event_source = event.get("event_source")
     leak_stage = event.get("leak", {}).get("dataset", {}).get("stage")
@@ -180,29 +175,29 @@ def handle_leak(event, event_ooi, software_ooi):
         if software_ooi:
             kat_info.append(f'Software = "{software_ooi.name}".')
         else:
-            kat_info.append(f'Plugin = "{event_source}"')
+            kat_info.append(f'Plugin = "{event_source_reference}".')
 
         if leak_infected:
             kat_info.append("Found evidence of external activity.")
         if leak_ransomnote:
             kat_info.append("Found a ransom note.")
         if leak_stage:
-            kat_info.append(f"Stage of the leak is {leak_stage}.")
+            kat_info.append(f"Stage of the leak is: {leak_stage}.")
 
         yield Finding(
             finding_type=finding_type.reference,
-            ooi=event_ooi,
-            description=" ".join(kat_info),
+            ooi=software_ooi.reference if software_ooi or event_source_reference,
+            description=", ".join(kat_info),
         )
 
 
-def handle_tag(event, software_ooi=None, ip_port_ooi=None):
+def handle_tag(event, software_ooi_reference=None, ip_port_ooi_reference=None):
     # Tags (CVE's)
     if isinstance(event.get("tags"), Iterable):
         for tag in event.get("tags", {}):
             if re.match("cve-[0-9]{4}-[0-9]{4,6}", tag):
                 ft = CVEFindingType(id=tag)
-                cve_ooi = software_ooi if software_ooi else ip_port_ooi
+                cve_ooi = software_ooi_reference if software_ooi_reference else ip_port_ooi_reference
                 f = Finding(finding_type=ft.reference, ooi=cve_ooi.reference)
                 yield ft
                 yield f
