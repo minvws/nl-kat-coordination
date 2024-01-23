c6d8f37f26f3c046d5dcf7690f80a0084346dc93
Noam Blitz <noamblitz@gmail.com>
2024-01-23 14:25:29 +0100

    add datetime created_at

--- rocky/reports/views/base.py
+++ rocky/reports/views/base.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Any, Dict, List, Optional, Set, Type, Union
 
 from account.mixins import OrganizationView
@@ -119,6 +120,7 @@ class BaseReportView(OctopoesView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["observed_at"] = self.valid_time
+        context["created_at"] = datetime.now()
         context["selected_oois"] = self.selected_oois
         context["selected_report_types"] = self.selected_report_types
         return context
