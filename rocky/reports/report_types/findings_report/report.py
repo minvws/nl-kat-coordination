faa8468c6623cf506f927d3a96a4ef7262d402f6
Madelon D <maddohmen@hotmail.com>
2024-01-24 16:53:43 +0100

    Update reports

--- rocky/reports/report_types/findings_report/report.py
+++ rocky/reports/report_types/findings_report/report.py
@@ -5,30 +5,34 @@ from typing import Any, Dict
 from django.utils.translation import gettext_lazy as _
 
 from octopoes.models import Reference
-from octopoes.models.ooi.dns.zone import Hostname
+from octopoes.models.ooi.findings import Finding
+from octopoes.models.types import ALL_TYPES
 from reports.report_types.definitions import Report
 
 logger = getLogger(__name__)
+
+TREE_DEPTH = 9
 
 
 class FindingsReport(Report):
     id = "findings-report"
     name = _("Findings Report")
     description = _("Findings Report")
+    plugins = {"required": [], "optional": []}
+    input_ooi_types = ALL_TYPES
     template_path = "findings_report/report.html"
 
     def generate_data(self, input_ooi: str, valid_time: datetime) -> Dict[str, Any]:
         reference = Reference.from_str(input_ooi)
-        if reference.class_type == Hostname:
-            ips = self.octopoes_api_connector.query(
-                "Hostname.<hostname[is ResolvedHostname].address", valid_time, reference
-            )
-        else:
-            ips = [self.octopoes_api_connector.get(reference)]
+        findings = []
 
-        for ip in ips:
-            finding_types = self.octopoes_api_connector.query(
-                "IPAddress.<ooi[is Finding].finding_type", valid_time, ip.reference
-            )
+        tree = self.octopoes_api_connector.get_tree(
+            reference, depth=TREE_DEPTH, types={Finding}, valid_time=valid_time
+        ).store
+        for pk, ooi in tree.items():
+            if ooi.ooi_type == "Finding":
+                findings.append(ooi)
 
-        return {"finding_types": finding_types}
+        return {
+            "findings": findings,
+        }
