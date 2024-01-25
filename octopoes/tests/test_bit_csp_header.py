bd15ce3497719613bbc5c511b1b42fe7ac63d2cb
Donny Peeters <donny@bitestreams.com>
2024-01-25 17:20:47 +0100

    style

--- octopoes/tests/test_bit_csp_header.py
+++ octopoes/tests/test_bit_csp_header.py
@@ -4,7 +4,8 @@ from octopoes.models.ooi.web import HTTPHeader
 
 
 def test_https_hsts(http_resource_https):
-    results = [list(run(HTTPHeader(resource=http_resource_https.reference, key=key, value=value), [], {}))
+    results = [
+        list(run(HTTPHeader(resource=http_resource_https.reference, key=key, value=value), [], {}))
         for key, value in [
             ("Content-Type", "text/html"),
             ("Content-security-poliCY", "text/html"),
@@ -19,16 +20,21 @@ def test_https_hsts(http_resource_https):
     assert results[0] == []
     assert len(results[1]) == 2
     assert results[1][0].id == "KAT-CSP-VULNERABILITIES"
-    assert results[1][1].description == """List of CSP findings:
+    assert (
+        results[1][1].description
+        == """List of CSP findings:
  1. frame-src has not been defined or does not have a fallback.
  2. script-src has not been defined or does not have a fallback.
  3. base-uri has not been defined, default-src does not apply.
  4. frame-ancestors has not been defined.
  5. default-src has not been defined.
  6. CSP setting has no value."""
+    )
 
     assert results[2][0].id == "KAT-CSP-VULNERABILITIES"
-    assert results[2][1].description == """List of CSP findings:
+    assert (
+        results[2][1].description
+        == """List of CSP findings:
  1. Http should not be used in the CSP settings of an HTTP Header.
  2. frame-src has not been defined or does not have a fallback.
  3. script-src has not been defined or does not have a fallback.
@@ -36,18 +42,24 @@ def test_https_hsts(http_resource_https):
  5. frame-ancestors has not been defined.
  6. default-src has not been defined.
  7. CSP setting has no value."""
+    )
 
     assert results[3][0].id == "KAT-CSP-VULNERABILITIES"
-    assert results[3][1].description == """List of CSP findings:
+    assert (
+        results[3][1].description
+        == """List of CSP findings:
  1. frame-src has not been defined or does not have a fallback.
  2. script-src has not been defined or does not have a fallback.
  3. base-uri has not been defined, default-src does not apply.
  4. frame-ancestors has not been defined.
  5. default-src has not been defined.
  6. CSP setting has no value."""
+    )
 
     assert results[4][0].id == "KAT-CSP-VULNERABILITIES"
-    assert results[4][1].description == """List of CSP findings:
+    assert (
+        results[4][1].description
+        == """List of CSP findings:
  1. The wildcard * for the scheme and host part of any URL should never be used in CSP settings.
  2. frame-src has not been defined or does not have a fallback.
  3. script-src has not been defined or does not have a fallback.
@@ -55,9 +67,12 @@ def test_https_hsts(http_resource_https):
  5. frame-ancestors has not been defined.
  6. default-src has not been defined.
  7. CSP setting has no value."""
+    )
 
     assert results[5][0].id == "KAT-CSP-VULNERABILITIES"
-    assert results[5][1].description == """List of CSP findings:
+    assert (
+        results[5][1].description
+        == """List of CSP findings:
  1. frame-src has not been defined or does not have a fallback.
  2. script-src has not been defined or does not have a fallback.
  3. base-uri has not been defined, default-src does not apply.
@@ -67,9 +82,12 @@ def test_https_hsts(http_resource_https):
  7. CSP setting has no value.
  8. Private, local, reserved, multicast, loopback ips should not be allowed in the CSP settings.
  9. CSP setting has no value."""
+    )
 
     assert results[6][0].id == "KAT-CSP-VULNERABILITIES"
-    assert results[6][1].description == """List of CSP findings:
+    assert (
+        results[6][1].description
+        == """List of CSP findings:
  1. unsafe-inline, unsafe-eval and unsafe-hashes should not be used in the CSP settings of an HTTP Header.
  2. frame-src has not been defined or does not have a fallback.
  3. script-src has not been defined or does not have a fallback.
@@ -80,3 +98,4 @@ def test_https_hsts(http_resource_https):
  8. A wildcard source should not be used in the value of any type in the CSP settings.
  9. a blanket protocol source should not be used in the value of any type in the CSP settings.
  10. Private, local, reserved, multicast, loopback ips should not be allowed in the CSP settings."""
+    )
