e5a9eee541e14b84f7eca03d764d0162a8771f00
Madelon D <maddohmen@hotmail.com>
2024-01-25 15:06:16 +0100

    Change query to get

--- rocky/reports/report_types/findings_report/report.py
+++ rocky/reports/report_types/findings_report/report.py
@@ -5,6 +5,7 @@ from typing import Any, Dict
 from django.utils.translation import gettext_lazy as _
 
 from octopoes.models import Reference
+from octopoes.models.exception import ObjectNotFoundException
 from octopoes.models.ooi.findings import Finding
 from octopoes.models.types import ALL_TYPES
 from reports.report_types.definitions import Report
@@ -17,7 +18,7 @@ TREE_DEPTH = 9
 class FindingsReport(Report):
     id = "findings-report"
     name = _("Findings Report")
-    description = _("Findings Report")
+    description = _("Shows all the finding types and their occurrences.")
     plugins = {"required": [], "optional": []}
     input_ooi_types = ALL_TYPES
     template_path = "findings_report/report.html"
@@ -30,21 +31,20 @@ class FindingsReport(Report):
         tree = self.octopoes_api_connector.get_tree(
             reference, depth=TREE_DEPTH, types={Finding}, valid_time=valid_time
         ).store
+
         for pk, ooi in tree.items():
             if ooi.ooi_type == "Finding":
                 findings.append(ooi)
 
         for finding in findings:
-            finding_types = self.octopoes_api_connector.query(
-                "Finding.finding_type", valid_time, Reference.from_str(finding)
-            )
+            try:
+                finding_type = self.octopoes_api_connector.get(Reference.from_str(finding.finding_type), valid_time)
 
-            if finding_types:
-                finding_type = finding_types[0]
-
-                if finding_type.id in results:
-                    results[finding_type.id]["occurrences"].append(finding)
+                if finding_type in results:
+                    results[finding_type]["occurrences"].append(finding)
                 else:
-                    results[finding_type.id] = {"finding_type": finding_type, "occurrences": [finding]}
+                    results[finding_type] = {"finding_type": finding_type, "occurrences": [finding]}
+            except ObjectNotFoundException:
+                logger.error("No Finding Type found for Finding '%s' on date %s.", finding, str(valid_time))
 
         return results
