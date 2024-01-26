6088c36bb40d91bbe4af7fd20a87dfcd86f7158c
Jan Klopper <janklopper+underdark@gmail.com>
2024-01-26 18:09:41 +0100

    fix checkboxToggler.js
    previous version would not reset the form when the max was reached and we de decreased the amount of checked inputs, or when another checkbox than the first was selected if the minimum wasn't reached.

--- rocky/assets/js/checkboxToggler.js
+++ rocky/assets/js/checkboxToggler.js
@@ -19,44 +19,51 @@ function toggleCheckboxes(name, value) {
 const checkbox_required_anchors = document.querySelectorAll('.checkboxes_required');
 for (var i = 0; i < checkbox_required_anchors.length; i++){
   let anchor = checkbox_required_anchors[i];
+  var form = false;
+  var collection = [];
   if (anchor.tagName == 'INPUT' && anchor.type == 'checkbox'){
     // we are looking at a checkbox itself, assume we want all checkboxes with the same name in the same form
-    var form = anchor.form;
-    var collection = form.getElementsByName(anchor.name);
+    if(anchor.name){
+      form = anchor.form;
+      collection = form.getElementsByName(anchor.name);
+    }
   } else if (anchor.tagName == 'FORM'){
-    var form = anchor;
-    var collection = anchor.querySelectorAll('input[type=checkbox]');
+    form = anchor;
+    collection = anchor.querySelectorAll('input[type=checkbox]');
   } else {
     // we are looking at a parent of a group of checkboxes. lets collect all underlying checkboxes.
-    var collection = anchor.querySelectorAll('input[type=checkbox]');
-    var form = collection[0].form;
+    collection = anchor.querySelectorAll('input[type=checkbox]');
+    form = collection[0].form;
   }
-  form.addEventListener('submit', checkbox_required_validity.bind(null, form, anchor));
-};
+  if (form){
+    form.addEventListener('submit', checkbox_required_validity.bind(null, form, anchor));
+  }
+}
 
 function checkbox_required_validity(form, anchor, event) {
   //  validate the current list of checkboxes against the current min/max required settings only at submit time.
   var selected_count = 0;
-  var error_element = null;
+  var elements = [];
   if (anchor.tagName == 'INPUT' && anchor.type == 'checkbox'){
     // we are looking at a checkbox itself, assume we want all checkboxes with the same name in the same form
-    var name = anchor.name;
-    selected_count = anchor.form.querySelectorAll('input[type=checkbox][name='+name+']:checked').length;
-    error_element = anchor.form.querySelector('input[type=checkbox][name='+name+']');
+    selected_count = anchor.form.querySelectorAll('input[type=checkbox][name='+anchor.name+']:checked').length;
+    elements = anchor.form.querySelectorAll('input[type=checkbox][name='+anchor.name+']');
   } else if (anchor.tagName == 'FORM'){
     selected_count = anchor.querySelectorAll('input[type=checkbox]:checked').length;
-    error_element = anchor.querySelector('input[type=checkbox]');
+    elements = anchor.querySelectorAll('input[type=checkbox]');
   } else {
     // we are looking at a parent of a group of checkboxes. lets collect all underlying checkboxes.
     selected_count = anchor.querySelectorAll('input[type=checkbox]:checked').length;
-    error_element = anchor.form.querySelector('input[type=checkbox]');
+    elements = anchor.form.querySelectorAll('input[type=checkbox]');
   }
-
-  var minselected = 1;
+  
+  var error_element = elements[0];
+  var minselected = 1; // we expect at least one, unless otherwise specified.
   var validity = true;
   error_element.setCustomValidity('');
   if ((("min" in anchor.dataset) && anchor.dataset.min > selected_count) || minselected > selected_count) {
-    error_element.setCustomValidity('Not enough checkboxes selected, select at least: '+(("min" in anchor.dataset) && anchor.dataset.min || minselected));
+    minselected = (("min" in anchor.dataset) && anchor.dataset.min || minselected)
+    error_element.setCustomValidity('Not enough checkboxes selected, select at least: '+minselected);
     validity = false;
     event.preventDefault();
   } else if (("max" in anchor.dataset) && anchor.dataset.max < selected_count) {
@@ -64,11 +71,14 @@ function checkbox_required_validity(form, anchor, event) {
     validity = false;
     event.preventDefault();
   }
-  error_element.addEventListener('change', function(event){
-    // we need to remove the custom error on change, because otherwise the submit code won't run.
-    if(event.target.checked){
+  
+  // bind a change event to *all* checkboxes that might increase or decrease our selected_count; 
+  // Increase resets the usecase of a minimal selection, decrease is needed if we have reached the max.
+  elements.forEach(function (element){
+    element.addEventListener('change', function(event){
+      // we need to remove the custom error on change, because otherwise the submit won't allow us to revalidate as the form immediately raises an invalid state.
       event.target.setCustomValidity('');
-    }
+    });
   });
   error_element.reportValidity();
   return validity;
