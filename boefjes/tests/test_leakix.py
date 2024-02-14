f7bb99bfab89bdf2f1ccaaf077a306a1194e9c6b
Jan Klopper <janklopper+underdark@gmail.com>
2024-02-14 14:35:59 +0100

    Create test_leakix.py

---
+++ boefjes/tests/test_leakix.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+from pydantic import parse_obj_as
+
+from boefjes.job_handler import serialize_ooi
+from boefjes.plugins.kat_leakix.normalize import run
+from octopoes.models.types import OOIType
+from tests.loading import get_boefje_meta, get_dummy_data, get_normalizer_meta
+
+
+class LeakIxNormalizerTest(TestCase):
+    def test_output(self):
+        input_ooi = parse_obj_as(
+            OOIType,
+            {
+                "object_type": "HostnameHTTPURL",
+                "network": "Network|internet",
+                "scheme": "https",
+                "port": 443,
+                "path": "/",
+                "netloc": "Hostname|internet|example.com",
+            },
+        )
+        boefje_meta = get_boefje_meta(input_ooi=input_ooi.reference)
+        boefje_meta.arguments["input"] = serialize_ooi(input_ooi)
+
+        output = [x for x in run(get_normalizer_meta(boefje_meta), get_dummy_data("raw/leakix-example.com.json"))]
+
+        self.assertEqual(170, len(output))
+        self.assertEqual(get_dummy_data("raw/leakix-example.com-output.json").decode(), str(output))
