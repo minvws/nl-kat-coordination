ed28aff6d072ee0526e9f9124fa2d61ec60567ea
Noam Blitz <noamblitz@gmail.com>
2024-02-14 13:43:27 +0100

    strip double quotes from max-age

--- octopoes/bits/check_hsts_header/check_hsts_header.py
+++ octopoes/bits/check_hsts_header/check_hsts_header.py
@@ -22,7 +22,7 @@ def run(input_ooi: HTTPHeader, additional_oois: List, config: Dict[str, str]) ->
     if "max-age" not in header.value.lower():
         findings.append("The cache validity period of the HSTS should be defined and should be at least 1 year.")
 
-    if "max-age" in header.value.lower() and int(header.value.split("=")[1].split(";")[0]) < max_age:
+    if "max-age" in header.value.lower() and int(header.value.split("=")[1].strip('"').split(";")[0]) < max_age:
         findings.append(f"The cache validity period of the HSTS should be at least be {max_age} seconds.")
 
     if findings:
