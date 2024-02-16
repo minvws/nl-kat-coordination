a26e356c31abe2c183632a7d4411e3fef419d173
noamblitz <43830693+noamblitz@users.noreply.github.com>
2024-02-16 16:17:02 +0100

    Open ports report unit tests (#2514)
    Co-authored-by: Jeroen Dekkers <jeroen@dekkers.ch>

--- rocky/reports/report_types/open_ports_report/report.py
+++ rocky/reports/report_types/open_ports_report/report.py
@@ -57,7 +57,6 @@ class OpenPortsReport(Report):
                 )
                 port_numbers[port.port] = found_by_openkat
 
-                self.octopoes_api_connector.query("IPPort.<ip_port [is IPService].service", valid_time, port.reference)
                 services[port.port] = [
                     service.name
                     for service in self.octopoes_api_connector.query(
