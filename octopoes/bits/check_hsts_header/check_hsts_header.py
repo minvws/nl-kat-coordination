d35b8f7b21bde46da7e4b5b204a2dcbaf86bf46f
noamblitz <43830693+noamblitz@users.noreply.github.com>
2024-02-19 08:58:49 +0100

    Update octopoes/bits/check_hsts_header/check_hsts_header.py
    Co-authored-by: Jan Klopper <janklopper+underdark@gmail.com>

--- octopoes/bits/check_hsts_header/check_hsts_header.py
+++ octopoes/bits/check_hsts_header/check_hsts_header.py
@@ -20,7 +20,7 @@ def run(input_ooi: HTTPHeader, additional_oois: List, config: Dict[str, str]) ->
     if "includesubdomains" not in headervalue:
         findings.append("The HSTS should include subdomains.")
 
-    if "max-age" not in header.value.lower():
+    if "max-age" not in headervalue:
         findings.append("The cache validity period of the HSTS should be defined and should be at least 1 year.")
 
     if "max-age" in header.value.lower() and int(header.value.split("=")[1].strip('"').split(";")[0]) < max_age:
