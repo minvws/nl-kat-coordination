85fed61f1edbbefb41185b7d998f336555bb1c6c
Noam Blitz <noamblitz@gmail.com>
2024-02-19 09:09:08 +0100

    catch non int max age

--- octopoes/bits/check_hsts_header/check_hsts_header.py
+++ octopoes/bits/check_hsts_header/check_hsts_header.py
@@ -23,8 +23,11 @@ def run(input_ooi: HTTPHeader, additional_oois: List, config: Dict[str, str]) ->
     if "max-age" not in headervalue:
         findings.append("The cache validity period of the HSTS should be defined and should be at least 1 year.")
 
-    if "max-age" in headervalue and int(headervalue.split("=")[1].strip('"').split(";")[0]) < max_age:
-        findings.append(f"The cache validity period of the HSTS should be at least be {max_age} seconds.")
+    try:
+        if "max-age" in headervalue and int(headervalue.split("=")[1].strip('"').split(";")[0]) < max_age:
+            findings.append(f"The cache validity period of the HSTS should be at least be {max_age} seconds.")
+    except ValueError:
+        findings.append("The max-age value should be an integer.")
 
     if findings:
         description: str = "List of HSTS findings:\n"
