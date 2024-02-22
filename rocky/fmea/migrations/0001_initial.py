d07104bdb862d70716759e43370159c63a049963
Ammar <ammar.abdulamir@gmail.com>
2024-02-22 10:00:09 +0100

    Fixed type hint error and removed fmea from pre-commit config

--- rocky/fmea/migrations/0001_initial.py
+++ rocky/fmea/migrations/0001_initial.py
@@ -7,8 +7,6 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
     initial = True
 
-    dependencies = []
-
     operations = [
         migrations.CreateModel(
             name="FailureMode",
