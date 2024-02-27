7ef7e164a4426a7831116a88b1e802bba27b1beb
Benny <mail2benny@gmail.com>
2024-02-27 09:40:20 +0100

    Uppercase constansts in unicode tests

--- octopoes/tests/integration/test_unicode.py
+++ octopoes/tests/integration/test_unicode.py
@@ -15,12 +15,12 @@ from octopoes.models.origin import OriginType
 if os.environ.get("CI") != "1":
     pytest.skip("Needs XTDB multinode container.", allow_module_level=True)
 
-use_unicode = True
-names = ["🐱", "★.com", "🐈"] if use_unicode else ["cat", "xn--p3h.com", "boefje"]
+USE_UNICODE = True
+NAMES = ["🐱", "★.com", "🐈"] if USE_UNICODE else ["cat", "xn--p3h.com", "boefje"]
 
 
 def test_unicode_network(octopoes_api_connector: OctopoesAPIConnector, valid_time: datetime):
-    network = Network(name=names[0])
+    network = Network(name=NAMES[0])
     octopoes_api_connector.save_declaration(
         Declaration(
             ooi=network,
@@ -32,12 +32,12 @@ def test_unicode_network(octopoes_api_connector: OctopoesAPIConnector, valid_tim
 
     assert octopoes_api_connector.list_objects(types={Network}, valid_time=valid_time).count == 1
     network_object = octopoes_api_connector.list_objects(types={Network}, valid_time=valid_time).items[0]
-    assert network_object.name == names[0]
+    assert network_object.name == NAMES[0]
     assert network_object.reference == network.reference
 
 
 def test_unicode_hostname(octopoes_api_connector: OctopoesAPIConnector, valid_time: datetime):
-    network = Network(name=names[0])
+    network = Network(name=NAMES[0])
     octopoes_api_connector.save_declaration(
         Declaration(
             ooi=network,
@@ -45,12 +45,12 @@ def test_unicode_hostname(octopoes_api_connector: OctopoesAPIConnector, valid_ti
         )
     )
 
-    hostname = Hostname(network=network.reference, name=names[1])
+    hostname = Hostname(network=network.reference, name=NAMES[1])
     task_id = uuid.uuid4()
 
     octopoes_api_connector.save_observation(
         Observation(
-            method=names[2],
+            method=NAMES[2],
             source=network.reference,
             task_id=task_id,
             valid_time=valid_time,
@@ -66,16 +66,16 @@ def test_unicode_hostname(octopoes_api_connector: OctopoesAPIConnector, valid_ti
     assert octopoes_api_connector.list_objects(types={Network, Hostname}, valid_time=valid_time).count == 2
 
     network_object = octopoes_api_connector.list_objects(types={Network}, valid_time=valid_time).items[0]
-    assert network_object.name == names[0]
+    assert network_object.name == NAMES[0]
     assert network_object.reference == network.reference
 
     hostname_object = octopoes_api_connector.list_objects(types={Hostname}, valid_time=valid_time).items[0]
-    assert hostname_object.name == names[1].encode("idna").decode()
+    assert hostname_object.name == NAMES[1].encode("idna").decode()
     assert hostname_object.reference == hostname.reference
 
     origins = octopoes_api_connector.list_origins(task_id=task_id, valid_time=valid_time)
     assert origins[0].dict() == {
-        "method": names[2],
+        "method": NAMES[2],
         "origin_type": OriginType.OBSERVATION,
         "source": network.reference,
         "result": [hostname.reference],
