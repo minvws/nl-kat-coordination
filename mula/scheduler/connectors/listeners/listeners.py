c07adb71a725df7feae6992b8ad495d06be3f160
JP Bruins Slot <jpbruinsslot@gmail.com>
2024-03-04 13:28:23 +0100

    Add metrics collection for scheduler using prometheus (#2468)
    Co-authored-by: Jan Klopper <janklopper+underdark@gmail.com>
    Co-authored-by: Ammar <ammar.abdulamir@gmail.com>
    Co-authored-by: Jeroen Dekkers <jeroen@dekkers.ch>

--- mula/scheduler/connectors/listeners/listeners.py
+++ mula/scheduler/connectors/listeners/listeners.py
@@ -91,7 +91,9 @@ class RabbitMQ(Listener):
         self.prefetch_count: int = prefetch_count
         self.func: Callable = func
 
-        self.executor: futures.ThreadPoolExecutor = futures.ThreadPoolExecutor(max_workers=10)
+        self.executor: futures.ThreadPoolExecutor = futures.ThreadPoolExecutor(
+            max_workers=10, thread_name_prefix=f"Listener-TPE-{self.__class__.__name__}"
+        )
         self.connection: pika.BlockingConnection | None = None
         self.channel: pika.BlockingConnection.channel | None = None
         self.connect(self.queue, self.durable, self.prefetch_count)
