26d9351dc2da7d1e561f11e39c03a57f0b3d5c96
Madelon D <maddohmen@hotmail.com>
2024-03-05 16:49:41 +0100

    Valid time fix in dns tree request

--- rocky/reports/report_types/dns_report/report.py
+++ rocky/reports/report_types/dns_report/report.py
@@ -24,7 +24,9 @@ class DNSReport(Report):
 
     def generate_data(self, input_ooi: str, valid_time: datetime) -> dict[str, Any]:
         ref = Reference.from_str(input_ooi)
-        tree = self.octopoes_api_connector.get_tree(ref, valid_time, depth=3, types={DNSRecord, Finding}).store
+        tree = self.octopoes_api_connector.get_tree(
+            ref, valid_time=valid_time, depth=3, types={DNSRecord, Finding}
+        ).store
 
         findings = []
         finding_types: dict[str, dict] = {}
