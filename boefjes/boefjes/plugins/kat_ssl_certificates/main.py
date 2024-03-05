3822f2fb56ad30c0a466f6f3d1a06d9b61dba39d
Tobias S <123560075+tobiasBDO@users.noreply.github.com>
2024-03-05 15:16:38 +0100

    fix: openssl boefje stuck on port 80

--- boefjes/boefjes/plugins/kat_ssl_certificates/main.py
+++ boefjes/boefjes/plugins/kat_ssl_certificates/main.py
@@ -9,8 +9,12 @@ def run(boefje_meta: BoefjeMeta) -> list[tuple[set, bytes | str]]:
     client = docker.from_env()
     input_ = boefje_meta.arguments["input"]
     hostname = input_["hostname"]["name"]
+    scheme = input_["ip_service"]["service"]["name"]
     ip_address = input_["ip_service"]["ip_port"]["address"]["address"]
     port = input_["ip_service"]["ip_port"]["port"]
+
+    if scheme != "https":
+        return [(set(), "")]
 
     try:
         output = client.containers.run(
