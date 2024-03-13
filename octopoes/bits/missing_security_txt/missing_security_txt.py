6101e3209d272e55496d1f7673609cac2fa34f66
Jan Klopper <janklopper+underdark@gmail.com>
2024-03-13 15:51:16 +0100

    Update missing_security_txt.py

--- octopoes/bits/missing_security_txt/missing_security_txt.py
+++ octopoes/bits/missing_security_txt/missing_security_txt.py
@@ -2,8 +2,7 @@ from collections.abc import Iterator
 
 from octopoes.models import OOI
 from octopoes.models.ooi.findings import Finding, KATFindingType
-from octopoes.models.ooi.web import Website, SecurityTXT
-
+from octopoes.models.ooi.web import SecurityTXT, Website
 
 def run(input_ooi: Website, additional_oois: list[SecurityTXT], config: dict[str, str]) -> Iterator[OOI]:
     if not additional_oois:
