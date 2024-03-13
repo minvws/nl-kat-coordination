65db4955974ac64c73105a04445a23572d1da83c
Jan Klopper <janklopper+underdark@gmail.com>
2024-03-13 15:30:42 +0100

    Create missing_security_txt.py

---
+++ octopoes/bits/missing_security_txt/missing_security_txt.py
@@ -0,0 +1,16 @@
+from collections.abc import Iterator
+
+from octopoes.models import OOI
+from octopoes.models.ooi.web import Website, SecurityTXT
+from octopoes.models.ooi.findings import Finding, KATFindingType
+
+
+def run(input_ooi: Website, additional_oois: list[SecurityTXT], config: dict[str, str]) -> Iterator[OOI]:
+    if not additional_oois:
+        ft = KATFindingType(id="KAT-NO-SECURITY-TXT")
+        yield ft
+        yield Finding(
+            ooi=input_ooi.reference,
+            finding_type=ft.reference,
+            description="This website does not have a Security.txt file",
+        )
