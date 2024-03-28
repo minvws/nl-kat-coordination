9efc251db6cd65359b7bb24febf57c6282479c66
Benny <mail2benny@gmail.com>
2024-03-28 12:19:15 +0100

    Some introspection

--- octopoes/tools/xtdb-cli.py
+++ octopoes/tools/xtdb-cli.py
@@ -14,172 +14,119 @@ class XTDB:
         self.port = port
         self.node = node
 
-    def root(self, target: str = ""):
+    def _root(self, target: str = ""):
         return f"http://{self.host}:{self.port}/_xtdb/{self.node}/{target}"
 
     def status(self):
         headers = {"Accept": "application/json"}
-        req = httpx.get(self.root("status"), headers=headers)
+        req = httpx.get(self._root("status"), headers=headers)
         return req.text
 
     def query(self, query: str = "{:query {:find [ ?var ] :where [[?var :xt/id ]]}}"):
         headers = {"Accept": "application/json", "Content-Type": "application/edn"}
-        req = httpx.post(self.root("query"), headers=headers, data=query)
+        req = httpx.post(self._root("query"), headers=headers, data=query)
         return req.text
 
     def entity(self, key: str):
         headers = {"Accept": "application/json"}
-        req = httpx.get(self.root(f"entity?eid={key}"), headers=headers)
+        req = httpx.get(self._root(f"entity?eid={key}"), headers=headers)
         return req.text
 
     def history(self, key: str):
         headers = {"Accept": "application/json"}
-        req = httpx.get(self.root(f"entity?eid={key}&history=true&sortOrder=asc"), headers=headers)
+        req = httpx.get(self._root(f"entity?eid={key}&history=true&sortOrder=asc"), headers=headers)
         return req.text
 
     def entity_tx(self, key: str):
         headers = {"Accept": "application/json"}
-        req = httpx.get(self.root(f"entity-tx?eid={key}"), headers=headers)
+        req = httpx.get(self._root(f"entity-tx?eid={key}"), headers=headers)
         return req.text
 
     def attribute_stats(self):
         headers = {"Accept": "application/json"}
-        req = httpx.get(self.root("attribute-stats"), headers=headers)
+        req = httpx.get(self._root("attribute-stats"), headers=headers)
         return req.text
 
     def sync(self, timeout: int = 500):
         headers = {"Accept": "application/json"}
-        req = httpx.get(self.root(f"sync?timeout={timeout}"), headers=headers)
+        req = httpx.get(self._root(f"sync?timeout={timeout}"), headers=headers)
         return req.text
 
     def await_tx(self, txid: int):
         headers = {"Accept": "application/json"}
-        req = httpx.get(self.root(f"await-tx?txId={txid}"), headers=headers)
+        req = httpx.get(self._root(f"await-tx?txId={txid}"), headers=headers)
         return req.text
 
     def await_tx_time(self, tm: str = datetime.datetime.now().isoformat()):
         headers = {"Accept": "application/json"}
-        req = httpx.get(self.root(f"await-tx-time?tx-time={tm}"), headers=headers)
+        req = httpx.get(self._root(f"await-tx-time?tx-time={tm}"), headers=headers)
         return req.text
 
     def tx_log(self):
         headers = {"Accept": "application/json"}
-        req = httpx.get(self.root("tx-log"), headers=headers)
+        req = httpx.get(self._root("tx-log"), headers=headers)
         return req.text
 
     def submit_tx(self, txs):
         headers = {"Accept": "application/json", "Content-Type": "application/json"}
         data = '{{"tx-ops": {}}}'.format(" ".join(txs))
-        req = httpx.post(self.root("submit-tx"), headers=headers, data=data)
+        req = httpx.post(self._root("submit-tx"), headers=headers, data=data)
         return req.text
 
     def tx_committed(self, txid: int):
         headers = {"Accept": "application/json"}
-        req = httpx.get(self.root(f"tx_commited?txId={txid}"), headers=headers)
+        req = httpx.get(self._root(f"tx_commited?txId={txid}"), headers=headers)
         return req.text
 
     def latest_completed_tx(self):
         headers = {"Accept": "application/json"}
-        req = httpx.get(self.root("latest-completed-tx"), headers=headers)
+        req = httpx.get(self._root("latest-completed-tx"), headers=headers)
         return req.text
 
     def latest_submitted_tx(self):
         headers = {"Accept": "application/json"}
-        req = httpx.get(self.root("latest-submitted-tx"), headers=headers)
+        req = httpx.get(self._root("latest-submitted-tx"), headers=headers)
         return req.text
 
     def active_queries(self):
         headers = {"Accept": "application/json"}
-        req = httpx.get(self.root("active-queries"), headers=headers)
+        req = httpx.get(self._root("active-queries"), headers=headers)
         return req.text
 
     def recent_queries(self):
         headers = {"Accept": "application/json"}
-        req = httpx.get(self.root("recent-queries"), headers=headers)
+        req = httpx.get(self._root("recent-queries"), headers=headers)
         return req.text
 
     def slowest_queries(self):
         headers = {"Accept": "application/json"}
-        req = httpx.get(self.root("recent-queries"), headers=headers)
+        req = httpx.get(self._root("recent-queries"), headers=headers)
         return req.text
 
 
 def dispatch(xtdb, instruction):
     match instruction.pop(0):
-        case "status":
-            return xtdb.status()
-        case "query":
-            if instruction:
-                return xtdb.query(instruction[0])
-            else:
-                return xtdb.query()
         case "list-keys":
             return xtdb.query()
         case "list-values":
             return xtdb.query("{:query {:find [(pull ?var [*])] :where [[?var :xt/id]]}}")
-        case "entity":
-            if instruction:
-                return xtdb.entity(instruction[0])
-        case "history":
-            if instruction:
-                return xtdb.entity(instruction[0])
-        case "entity-tx":
-            if instruction:
-                return xtdb.entity(instruction[0])
-        case "attribute-stats":
-            return xtdb.attribute_stats()
-        case "sync":
-            if instruction:
-                return xtdb.sync(instruction[0])
-            else:
-                return xtdb.sync()
-        case "await-tx":
-            if instruction:
-                return xtdb.await_tx(instruction[0])
-        case "await-tx-time":
-            if instruction:
-                return xtdb.await_tx_time(instruction[0])
-        case "tx-log":
-            return xtdb.tx_log()
         case "submit-tx":
             if instruction:
                 return xtdb.submit_tx(instruction)
-        case "tx-committed":
-            if instruction:
-                return xtdb.tx_committed(instruction.pop(0))
-        case "latest-completed-tx":
-            return xtdb.latest_completed_tx()
-        case "latest-submitted-tx":
-            return xtdb.latest_submitted_tx()
-        case "active-queries":
-            return xtdb.active_queries()
-        case "recent-queries":
-            return xtdb.recent_queries()
-        case "slowest-queries":
-            return xtdb.recent_queries()
+        case x:
+            call = getattr(xtdb, x.replace("-", "_"))
+            match call.__code__.co_argcount - 1:
+                case 1:
+                    return call(instruction[0])
+                case 0:
+                    return call()
 
 
 KEYWORDS = [
-    "status",
-    "query",
-    "list-keys",
-    "list-values",
-    "entity",
-    "history",
-    "entity-tx",
-    "attribute-stats",
-    "sync",
-    "await-tx",
-    "await-tx-time",
-    "tx-log",
-    "submit-tx",
-    "tx-committed",
-    "latest-completed-tx",
-    "latest-submitted-tx",
-    "active-queries",
-    "recent-queries",
-    "slowest-queries",
-]
+    keyword.replace("_", "-")
+    for keyword in dir(XTDB)
+    if callable(getattr(XTDB, keyword)) and not keyword.startswith("_")
+] + ["list-keys", "list-values"]
 
 EPILOG = """
 As instructions the following keywords with arguments are supported:
@@ -202,6 +149,9 @@ As instructions the following keywords with arguments are supported:
   active-queries
   recent-queries
   slowest-queries
+
+If no keyword is given in the initial instruction either use a dash "-" to read stdin otherwise all instructions are treated as filenames
+
 See https://v1-docs.xtdb.com/clients/http/ for more information.
 
 OpenKAT https://openkat.nl/.
