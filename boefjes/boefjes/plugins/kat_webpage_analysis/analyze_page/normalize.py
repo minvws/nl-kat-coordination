3fb2d9f1aa213a514782282408021e5803a6125e
Ammar <ammar.abdulamir@gmail.com>
2024-03-29 12:42:53 +0100

    Fixed normalizer

--- boefjes/boefjes/plugins/kat_webpage_analysis/analyze_page/normalize.py
+++ boefjes/boefjes/plugins/kat_webpage_analysis/analyze_page/normalize.py
@@ -6,10 +6,17 @@ from Wappalyzer import Wappalyzer, WebPage
 
 from boefjes.job_models import NormalizerMeta
 from octopoes.models import OOI, Reference
+from octopoes.models.ooi.dns.zone import Hostname
+from octopoes.models.ooi.network import Network
 from octopoes.models.ooi.software import Software, SoftwareInstance
 
 
 def run(normalizer_meta: NormalizerMeta, raw: bytes | str) -> Iterable[OOI]:
+    pk = normalizer_meta.raw_data.boefje_meta.input_ooi
+    tokenized_hostname = Reference.from_str(pk).tokenized["website"]["hostname"]
+    hostname = Hostname(
+        network=Network(name=tokenized_hostname["network"]["name"]).reference, name=tokenized_hostname["name"]
+    )
     raw_respsone, body = raw.split(b"\n\n", 1)
     response_object = json.loads(raw_respsone)
     url = response_object["response"]["url"]
@@ -24,5 +31,5 @@ def run(normalizer_meta: NormalizerMeta, raw: bytes | str) -> Iterable[OOI]:
 
     for name, data in results.items():
         software = Software(name=name, version=data["versions"].pop(0))
-        software_instance = SoftwareInstance(ooi=Reference.from_str(url), software=software.reference)
+        software_instance = SoftwareInstance(ooi=hostname.reference, software=software.reference)
         yield from [software, software_instance]
