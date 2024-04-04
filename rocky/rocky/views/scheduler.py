f7b865f32a48959c96a58c71d3d4fcac4eb57c4f
Rieven <rsnmartis@gmail.com>
2024-04-04 11:56:44 +0200

    Fix more try and except

--- rocky/rocky/views/scheduler.py
+++ rocky/rocky/views/scheduler.py
@@ -78,18 +78,24 @@ class SchedulerView(OctopoesView):
             )[0].result
         except IndexError:
             return []
+        except SchedulerError as error:
+            messages.error(self.request, error.message)
+            return []
 
     def get_json_task_details(self) -> JsonResponse | None:
-        task = self.get_task_details(self.kwargs["task_id"])
-        if task:
-            return JsonResponse(
-                {
-                    "oois": self.get_output_oois(task),
-                    "valid_time": task.p_item.data.raw_data.boefje_meta.ended_at.strftime("%Y-%m-%dT%H:%M:%S"),
-                },
-                safe=False,
-            )
-        return task
+        try:
+            task = self.get_task_details(self.kwargs["task_id"])
+            if task:
+                return JsonResponse(
+                    {
+                        "oois": self.get_output_oois(task),
+                        "valid_time": task.p_item.data.raw_data.boefje_meta.ended_at.strftime("%Y-%m-%dT%H:%M:%S"),
+                    },
+                    safe=False,
+                )
+            return task
+        except SchedulerError as error:
+            return messages.error(self.request, error.message)
 
     def schedule_task(self, p_item: PrioritizedItem) -> None:
         try:
@@ -122,43 +128,56 @@ class SchedulerView(OctopoesView):
     # task info from the scheduler. Task data should be available from the context
     # from which the task is created.
     def reschedule_task(self, task_id: str) -> None:
-        task = self.scheduler_client.get_task_details(task_id)
-        new_p_item = PrioritizedItem(
-            data=task.p_item.data,
-            priority=1,
-        )
+        try:
+            task = self.scheduler_client.get_task_details(task_id)
 
-        self.schedule_task(new_p_item)
+            new_p_item = PrioritizedItem(
+                data=task.p_item.data,
+                priority=1,
+            )
+
+            self.schedule_task(new_p_item)
+        except SchedulerError as error:
+            messages.error(self.request, error.message)
 
     def run_normalizer(self, katalogus_normalizer: Normalizer, raw_data: RawData) -> None:
-        normalizer_task = NormalizerTask(
-            normalizer=SchedulerNormalizer.model_validate(katalogus_normalizer.model_dump()),
-            raw_data=raw_data,
-        )
+        try:
+            normalizer_task = NormalizerTask(
+                normalizer=SchedulerNormalizer.model_validate(katalogus_normalizer.model_dump()),
+                raw_data=raw_data,
+            )
 
-        p_item = PrioritizedItem(priority=1, data=normalizer_task)
+            p_item = PrioritizedItem(priority=1, data=normalizer_task)
 
-        self.schedule_task(p_item)
+            self.schedule_task(p_item)
+        except SchedulerError as error:
+            messages.error(self.request, error.message)
 
     def run_boefje(self, katalogus_boefje: Boefje, ooi: OOI | None) -> None:
-        boefje_task = BoefjeTask(
-            boefje=SchedulerBoefje.model_validate(katalogus_boefje.model_dump()),
-            input_ooi=ooi.reference if ooi else None,
-            organization=self.organization.code,
-        )
+        try:
+            boefje_task = BoefjeTask(
+                boefje=SchedulerBoefje.model_validate(katalogus_boefje.model_dump()),
+                input_ooi=ooi.reference if ooi else None,
+                organization=self.organization.code,
+            )
 
-        p_item = PrioritizedItem(priority=1, data=boefje_task)
-        self.schedule_task(p_item)
+            p_item = PrioritizedItem(priority=1, data=boefje_task)
+            self.schedule_task(p_item)
+        except SchedulerError as error:
+            messages.error(self.request, error.message)
 
     def run_boefje_for_oois(
         self,
         boefje: Boefje,
         oois: list[OOI],
     ) -> None:
-        if not oois and not boefje.consumes:
-            self.run_boefje(boefje, None)
+        try:
+            if not oois and not boefje.consumes:
+                self.run_boefje(boefje, None)
 
-        for ooi in oois:
-            if ooi.scan_profile and ooi.scan_profile.level < boefje.scan_level:
-                self.can_raise_clearance_level(ooi, boefje.scan_level)
-            self.run_boefje(boefje, ooi)
+            for ooi in oois:
+                if ooi.scan_profile and ooi.scan_profile.level < boefje.scan_level:
+                    self.can_raise_clearance_level(ooi, boefje.scan_level)
+                self.run_boefje(boefje, ooi)
+        except SchedulerError as error:
+            messages.error(self.request, error.message)
