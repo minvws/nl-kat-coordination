cd96b3d1365eb78e41c5b4d0f97778db02151d62
stephanie0x00 <9821756+stephanie0x00@users.noreply.github.com>
2024-04-04 10:19:08 +0200

    Create backup-volume.sh
    Adding backup script from TobiasBDO with the following adjustments: 
    
    - Help file for backup-volume.sh wasn't working due to syntax error. 
    - globbing and word splitting fixes (shellcheck SC2086)
    - Changed “prefix” to "backup_path"
    - Changed help for --mount, which was not an accepted parameter
    - set -eu on top (exits on failure of command, undefined variables now give errors instead of performing unexpected actions)

---
+++ scripts/backup-volume.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -eu
+# creates a backup of the docker volume
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -p|--path)
+      backup_path="$2"
+      ;;
+    -h|-help|--help)
+      printf -- "--path  path where the backups are stored\n"
+      exit 1
+      ;;
+    *)
+      printf "*******************************\n"
+      printf "* Error: Invalid argument: $1 *\n"
+      printf "*******************************\n"
+      exit 1
+  esac
+  shift
+  shift
+done
+
+for volume in $(docker volume ls --filter name=nl-kat* --quiet);
+do
+  uuid="$(cat /proc/sys/kernel/random/uuid)"
+  if [ ! -d "$backup_path/$volume" ]; then
+    mkdir -p "$backup_path/$volume"
+  fi
+
+  IMAGE=alpine:latest
+  docker run \
+  --mount "type=volume,src=${volume},dst=/data" \
+  --name "$uuid" \
+  "$IMAGE"
+  
+  timestamp="$(date +%Y-%m-%d_%H%M%S)"
+  docker cp -a "$uuid:/data" "/tmp/$uuid"
+  tar -C "/tmp/$uuid" -czf "$backup_path/$volume/${timestamp}_${volume}.tar.gz" .
+  rm -rf "/tmp/$uuid"
+  docker rm "$uuid"
+done
