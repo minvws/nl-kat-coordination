5a0734db028b93e2972071df1bc7dd899ce0d7fb
stephanie0x00 <9821756+stephanie0x00@users.noreply.github.com>
2024-04-04 10:21:16 +0200

    Create create-volume-from-backup.sh
    Adding backup scripts created by TobiasBDO with the following adjustments: 
    - printf requires --  for help
    - globbing and word splitting fixes (shellcheck SC2086)
    - set -eu on top (exits on failure of command, undefined variables now give errors instead of performing unexpected actions)
    - fix format string error (variable in printf, instead of %s)
    - Changed help for --snapshot, which was not mentioned in the help file but accepted by the code.
    - Changed “prefix” to "backup_path"

---
+++ scripts/backup/create-volume-from-backup.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# creates a docker volume from a backup
+set -eu
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -v|-volume|--volume)
+      volume="$2"
+      ;;
+    -p|-path|--path)
+      backup_path="$2"
+      ;;
+    -n|-volume-name|--volume-name)
+      volume_name="$2"
+      ;;
+    -s|-snapshot|--snapshot)
+      snapshot="$2"
+      ;;
+    -h|-help|--help)
+      printf -- "-v | --volume: create the backup name of the volume\n"
+      printf -- "-n | --volume-name (optional): create the backup as this volume name\n"
+      printf -- "-p | --path: the storage path of the backup location\n"
+      printf -- "-s | --snapshot: the snapshot to restore\n"
+      exit 1
+      ;;
+    *)
+      printf "***************************\n"
+      printf "* Error: Invalid argument.*\n"
+      printf "***************************\n"
+      exit 1
+  esac
+  shift
+  shift
+done
+
+if [ -z "${volume_name:-}" ]; then
+  volume_name="$volume"
+fi
+
+volume_exists="$(docker volume ls | grep -q "$volume_name")"
+if [ "$volume_exists" ]; then
+  printf "***********************************\n"
+  printf "Error: volume %s exists. \n" "$volume_name"
+  printf "Please delete before proceeding    \n"
+  printf "***********************************\n"
+  exit 1
+fi
+
+if [ -z "${snapshot:-}" ]; then
+#  snapshot="$(ls -At "${backup_path}/${volume}/" | tail -n 1)"
+  snapshot="$(ls -At "${backup_path}/${volume}/" | head -n 1)"
+fi
+
+if [ -z "${snapshot:-}" ]; then
+  printf "**********************************\n"
+  printf "* Error: Unable to find snapshot.*\n"
+  printf "**********************************\n"
+  exit 1
+else
+  echo "creating from snapshot: ${snapshot}"
+fi
+
+uuid="$(cat /proc/sys/kernel/random/uuid)"
+cwd="$(pwd)"
+
+IMAGE=alpine:latest
+docker run \
+--mount "type=volume,src=${volume_name},dst=/data" \
+--name "$uuid" \
+"$IMAGE"
+
+mkdir "/tmp/$uuid"
+tar -xf "$backup_path/$volume/$snapshot" -C "/tmp/$uuid"
+cd "/tmp/$uuid"
+
+docker cp -a . "$uuid:/data"
+docker rm "$uuid"
+cd "$cwd"
