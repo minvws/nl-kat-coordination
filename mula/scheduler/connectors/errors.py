787230ec875f90d218dfa6c900db895a99926ac5
JP Bruins Slot <jpbruinsslot@gmail.com>
2024-04-16 09:48:44 +0200

    Updates from code review

--- mula/scheduler/connectors/errors.py
+++ mula/scheduler/connectors/errors.py
@@ -8,15 +8,23 @@ class ExternalServiceError(Exception):
     pass
 
 
+class ExternalServiceConnectionError(ExternalServiceError):
+    pass
+
+
+class ExternalServiceValidationError(ExternalServiceError):
+    pass
+
+
 def exception_handler(func):
     @functools.wraps(func)
     def inner_function(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except httpx.ConnectError as exc:
-            raise ExternalServiceError("External service is not available.") from exc
-        except pydantic.ValidationError:
-            ExternalServiceError("Validation error occurred.")
+            raise ExternalServiceConnectionError("External service is not available.") from exc
+        except pydantic.ValidationError as exc:
+            raise ExternalServiceValidationError("Validation error occurred.") from exc
         except Exception as exc:
             raise ExternalServiceError("External service returned an error.") from exc
 
