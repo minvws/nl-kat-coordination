f4e108ee0b75701087a6a5cf44eb5ae5a7fb6fa4
Madelon D <maddohmen@hotmail.com>
2024-04-19 10:36:33 +0200

    Fix pre-commit mypy error

--- rocky/reports/views/view_helpers.py
+++ rocky/reports/views/view_helpers.py
@@ -1,64 +1,84 @@
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
+from tools.models import Organization
 from tools.view_helpers import StepsMixin
 
 from reports.views.base import get_selection
 
 
 class KatGenerateReportStepsMixin(StepsMixin):
+    organization: Organization
+
     def build_steps(self):
-        kwargs = self.get_kwargs()
         selection = get_selection(self.request)
         steps = [
             {
                 "text": _("1: Select objects"),
-                "url": reverse_lazy("generate_report_select_oois", kwargs=kwargs) + selection,
+                "url": reverse_lazy("generate_report_select_oois", kwargs={"organization_code": self.organization.code})
+                + selection,
             },
             {
                 "text": _("2: Choose report types"),
-                "url": reverse_lazy("generate_report_select_report_types", kwargs=kwargs) + selection,
+                "url": reverse_lazy(
+                    "generate_report_select_report_types", kwargs={"organization_code": self.organization.code}
+                )
+                + selection,
             },
             {
                 "text": _("3: Configuration"),
-                "url": reverse_lazy("generate_report_setup_scan", kwargs=kwargs) + selection,
+                "url": reverse_lazy("generate_report_setup_scan", kwargs={"organization_code": self.organization.code})
+                + selection,
             },
         ]
         return steps
 
 
 class KatAggregateReportStepsMixin(StepsMixin):
+    organization: Organization
+
     def build_steps(self):
-        kwargs = self.get_kwargs()
         selection = get_selection(self.request)
         steps = [
             {
                 "text": _("1: Select objects"),
-                "url": reverse_lazy("aggregate_report_select_oois", kwargs=kwargs) + selection,
+                "url": reverse_lazy(
+                    "aggregate_report_select_oois", kwargs={"organization_code": self.organization.code}
+                )
+                + selection,
             },
             {
                 "text": _("2: Choose report types"),
-                "url": reverse_lazy("aggregate_report_select_report_types", kwargs=kwargs) + selection,
+                "url": reverse_lazy(
+                    "aggregate_report_select_report_types", kwargs={"organization_code": self.organization.code}
+                )
+                + selection,
             },
             {
                 "text": _("3: Configuration"),
-                "url": reverse_lazy("aggregate_report_setup_scan", kwargs=kwargs) + selection,
+                "url": reverse_lazy("aggregate_report_setup_scan", kwargs={"organization_code": self.organization.code})
+                + selection,
             },
         ]
         return steps
 
 
 class KatMultiReportStepsMixin(StepsMixin):
-    def build_steps(self):
-        kwargs = self.get_kwargs()
+    organization: Organization
+
+    def build_steps(self, **kwargs):
         selection = get_selection(self.request)
         steps = [
             {
                 "text": _("1: Select objects"),
-                "url": reverse_lazy("multi_report_select_oois", kwargs=kwargs) + selection,
+                "url": reverse_lazy("multi_report_select_oois", kwargs={"organization_code": self.organization.code})
+                + selection,
             },
             {
                 "text": _("2: Choose report types"),
-                "url": reverse_lazy("multi_report_select_report_types", kwargs=kwargs) + selection,
+                "url": reverse_lazy(
+                    "multi_report_select_report_types", kwargs={"organization_code": self.organization.code}
+                )
+                + selection,
             },
         ]
         return steps
