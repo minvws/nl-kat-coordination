da801c0075a910da491c091e45d7b76070e806fd
ammar92 <ammar.abdulamir@gmail.com>
2024-04-26 13:01:18 +0200

    Fixed image reference in `pdio_subfinder` plugin (#2860)
    Co-authored-by: Jan Klopper <janklopper+underdark@gmail.com>

--- boefjes/boefjes/plugins/pdio_subfinder/normalize.py
+++ boefjes/boefjes/plugins/pdio_subfinder/normalize.py
@@ -10,6 +10,6 @@ def run(normalizer_meta: NormalizerMeta, raw: bytes | str) -> Iterable[OOI]:
     hostname_ooi_reference = Reference.from_str(normalizer_meta.raw_data.boefje_meta.input_ooi)
     network_reference = Network(name=hostname_ooi_reference.tokenized.network.name).reference
 
-    for hostname in raw.splitlines():
+    for hostname in raw.decode().splitlines():
         hostname_ooi = Hostname(name=hostname, network=network_reference)
         yield hostname_ooi
