95e0868a67f6fda6d5666a2c216407695e1d904d
Soufyan Abdellati <soufyan@soufyan.cynalytics.nl>
2024-05-02 11:01:28 +0000

    Now the correct environment variable gets read which makes the boefje functional!

--- boefjes/boefjes/plugins/kat_hello_katty/main.py
+++ boefjes/boefjes/plugins/kat_hello_katty/main.py
@@ -2,12 +2,10 @@ import json
 from os import getenv
 
 def run(boefje_meta: dict) -> list[tuple[set, bytes | str]]:
-    """Function that gets ran to give raw data for the normalizers that read from """
-    print(str(boefje_meta))
-    
+    """Function that gets ran to give raw data for the normalizers that read from """   
     address = boefje_meta["arguments"]["input"]["address"]
     MESSAGE = getenv("MESSAGE", "ERROR")
-    NUMBER = getenv("NUMBER", "")
+    NUMBER = getenv("NUMBER", "1")
 
     # Check if NUMBER has been given, if it has not. Keep it at 0
     amount_of_emoji = 0
@@ -18,7 +16,7 @@ def run(boefje_meta: dict) -> list[tuple[set, bytes | str]]:
             pass
 
     emojis = "😺" * amount_of_emoji
-    greeting = f"{MESSAGE} {emojis}"
+    greeting = f"{MESSAGE} {emojis}!!!"
     
     raw = json.dumps({
         "address": address,
