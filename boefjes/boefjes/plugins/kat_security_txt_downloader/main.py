eb8dac21e7f30d06054987070f7f8ae894a4c6ab
Jan Klopper <janklopper+underdark@gmail.com>
2024-05-03 09:44:31 +0200

    refactor boefje, single output

--- boefjes/boefjes/plugins/kat_security_txt_downloader/main.py
+++ boefjes/boefjes/plugins/kat_security_txt_downloader/main.py
@@ -10,6 +10,7 @@ from boefjes.job_models import BoefjeMeta
 
 DEFAULT_TIMEOUT = 30
 
+
 def run(boefje_meta: BoefjeMeta) -> list[tuple[set, bytes | str]]:
     input_ = boefje_meta.arguments["input"]
     netloc = input_["hostname"]["name"]
@@ -17,7 +18,7 @@ def run(boefje_meta: BoefjeMeta) -> list[tuple[set, bytes | str]]:
     ip = input_["ip_service"]["ip_port"]["address"]["address"]
 
     useragent = getenv("USERAGENT", default="OpenKAT")
-    
+
     try:
         timeout = int(getenv("TIMEOUT", default=DEFAULT_TIMEOUT))
     except ValueError:
@@ -28,36 +29,35 @@ def run(boefje_meta: BoefjeMeta) -> list[tuple[set, bytes | str]]:
     results = {}
 
     for path in [".well-known/security.txt", "security.txt"]:
-        uri = f"{scheme}://{netloc}/{path}"
+        request_url = f"{scheme}://{netloc}/{path}"
 
         if scheme == "https":
-            session.mount(uri, ForcedIPHTTPSAdapter(dest_ip=ip))
+            session.mount(request_url, ForcedIPHTTPSAdapter(dest_ip=ip))
         else:
             addr = ipaddress.ip_address(ip)
             iploc = f"[{ip}]" if addr.version == 6 else ip
-            uri = f"{scheme}://{iploc}/{path}"
+            request_url = f"{scheme}://{iploc}/{path}"
 
         response = do_request(netloc, session, uri, useragent, timeout)
 
         # if the response is 200, return the content
         if response.status_code == 200:
-            results[path] = {"content": response.content.decode(), "url": response.url, "ip": ip, "status": 200}
+            status = 200
         # if the response is 301, we need to follow the location header to the correct security txt,
         # we can not force the ip anymore because we dont know it yet.
         # TODO return a redirected URL and have OpenKAT figure out if we want to follow this.
         elif response.status_code in [301, 302, 307, 308]:
-            uri = response.headers["Location"]
-            response = requests.get(uri, stream=True, timeout=timeout, verify=False)  # noqa: S501
-            ip = response.raw._connection.sock.getpeername()[0]
-            results[path] = {
-                "content": response.content.decode(),
-                "url": response.url,
-                "original_url": uri,
-                "ip": str(ip),
-                "status": response.status_code,
-            }
-        else:
-            results[path] = {"content": response.content.decode(), "url": None, "ip": None, "status": response.status_code}
+            request_url = response.headers["Location"]
+            response = requests.get(request_url, stream=True, timeout=timeout, verify=False)  # noqa: S501
+            ip = str(response.raw._connection.sock.getpeername()[0])
+
+        results[path] = {
+            "content": response.content.decode(),
+            "url": response.url,
+            "request_url": request_url,
+            "ip": ip,
+            "status": response.status_code
+        }
     return [(set(), json.dumps(results))]
 
 
