919c699ee6a4404118a42cf27278c2c015d3bbb2
TwistMeister <roelof.korporaal@gmail.com>
2024-05-03 16:32:51 +0200

    Improve JS logic for "show more" button

--- rocky/assets/js/pluginToggler.js
+++ rocky/assets/js/pluginToggler.js
@@ -1,13 +1,60 @@
 function togglePlugins(containerClass) {
   const container = document.querySelector(`.${containerClass}`);
   const button = container.querySelector('#more-suggested-plugins');
+  const checkbox = document.getElementById("checkbox-for-enabled-optional-plugins");
 
+
+  // Toggle "overflow" when amount of plugins exceed 4 (We only show 1 row containing 4 items, initially)
   function updateVisibility() {
-    container.classList.toggle("hide-overflow")
+    container.classList.toggle("hide-overflow");
+    setButtonAmountText();
   }
 
-  updateVisibility();
+  function setButtonAmountText() {
+    const elementsHtmlCollection = container.getElementsByClassName('plugin');
+    const itemsArray = Array.prototype.slice.call(elementsHtmlCollection);
+
+    let disabledCounter = 0;
+
+    // Determine amount of not yet enabled plugins
+    itemsArray.forEach((pluginElement) => {
+      if(pluginElement.classList.contains('plugin-is-disabled')) {
+        disabledCounter++;
+      }
+    });
+
+    let baseText = ""
+
+    // Determine if the button text should be "Show" or "Hide"
+    if(container.classList.contains('hide-overflow')) {
+      baseText = button.dataset.showText
+    } else {
+      baseText = button.dataset.hideText
+    }
+
+
+    // Determine amount of "to be shown" plugins. Hide button if amount !> 0
+    if(checkbox.checked) {
+      if((itemsArray.length - 4 > 0)) {
+        button.classList.remove("hidden")
+        button.innerHTML = `${baseText} (${itemsArray.length - 4})`
+      } else {
+        button.classList.add("hidden")
+      }
+    }
+    else {
+      if((disabledCounter - 4) > 0) {
+        button.classList.remove("hidden")
+        button.innerHTML = `${baseText} (${disabledCounter - 4})`
+      } else {
+        button.classList.add("hidden")
+      }
+    }
+  }
+
+  setButtonAmountText();
   button.addEventListener('click', updateVisibility)
+  checkbox.addEventListener('change', setButtonAmountText)
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
