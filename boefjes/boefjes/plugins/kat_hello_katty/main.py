03720361e432a5f3e9263f9d3e0caf6153597ff8
Soufyan Abdellati <soufyan@soufyan.cynalytics.nl>
2024-05-08 09:17:31 +0000

    Removed space from greeting, and made the default amount of emojis to 1.

--- boefjes/boefjes/plugins/kat_hello_katty/main.py
+++ boefjes/boefjes/plugins/kat_hello_katty/main.py
@@ -5,18 +5,18 @@ def run(boefje_meta: dict) -> list[tuple[set, bytes | str]]:
     """Function that gets ran to give raw data for the normalizers that read from """   
     address = boefje_meta["arguments"]["input"]["address"]
     MESSAGE = getenv("MESSAGE", "ERROR")
-    NUMBER = getenv("NUMBER", "1")
+    NUMBER = getenv("NUMBER", "0")
 
     # Check if NUMBER has been given, if it has not. Keep it at 0
-    amount_of_emoji = 0
+    amount_of_cats = 0
     if NUMBER != "":
         try:
-            amount_of_emoji = int(NUMBER)
+            amount_of_cats = int(NUMBER)
         except:
             pass
 
-    emojis = "😺" * amount_of_emoji
-    greeting = f"{MESSAGE} {emojis}!!!"
+    cats = "😺" * amount_of_cats
+    greeting = f"{MESSAGE}{cats}!!!"
     
     raw = json.dumps({
         "address": address,
