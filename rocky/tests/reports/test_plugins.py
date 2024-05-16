c2aa6712036db4fa2f8353e2d32082630f33aca5
Rieven <rsnmartis@gmail.com>
2024-05-16 11:22:43 +0200

    fix test

--- rocky/tests/reports/test_plugins.py
+++ rocky/tests/reports/test_plugins.py
@@ -11,24 +11,25 @@ from tests.conftest import setup_request
 def test_generate_report_setup_scan_wrong_plugin_id(
     rf,
     client_member,
-    mock_katalogus_client,
     valid_time,
     mocker,
     mock_organization_view_octopoes,
     listed_hostnames,
 ):
-    report = mocker.patch("reports.views.base")
+    base = mocker.patch("reports.views.base")
+    katalogus_client = mocker.patch("reports.views.base.get_katalogus")()
+
     ipv6_report_wrong_plugin_id = IPv6Report.plugins = {
         "required": ["dns-reco"],
         "optional": [],
     }
-    report.get_report_by_id.return_value = ipv6_report_wrong_plugin_id
+    base.get_report_by_id.return_value = ipv6_report_wrong_plugin_id
 
     mock_organization_view_octopoes().list_objects.return_value = Paginated[OOIType](
         count=len(listed_hostnames), items=listed_hostnames
     )
 
-    mock_katalogus_client.get_plugins.side_effect = KATalogusHTTPStatusError
+    katalogus_client.get_plugins.side_effect = KATalogusHTTPStatusError
 
     kwargs = {"organization_code": client_member.organization.code}
     url = reverse("generate_report_setup_scan", kwargs=kwargs)
@@ -48,4 +49,4 @@ def test_generate_report_setup_scan_wrong_plugin_id(
     response = SetupScanGenerateReportView.as_view()(request, organization_code=client_member.organization.code)
 
     assert response.status_code == 302
-    assert list(request._messages)[0].message == "404: A HTTP error occurred. Check logs for more info."
+    assert list(request._messages)[0].message == "A HTTP error occurred. Check logs for more info."
