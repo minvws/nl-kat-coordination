67e7c3a3b423c4236fab7978326eb8fd7b454ee7
Soufyan Abdellati <abdellatis149@gmail.com>
2024-05-22 09:19:18 +0000

    Removed unused comment

--- rocky/tests/reports/test_greeting_report.py
+++ rocky/tests/reports/test_greeting_report.py
@@ -3,7 +3,6 @@ from reports.report_types.greetings_report.report import GreetingsReport
 from octopoes.models.tree import ReferenceTree
 
 
-# Greeting|Hellloooo😺😺!!!|internet|13.107.236.208
 def test_greetings_report_data_filled(mock_octopoes_api_connector, valid_time, hostname, tree_data_no_findings):
     mock_octopoes_api_connector.tree = {
         hostname.reference: ReferenceTree.model_validate(tree_data_no_findings),
