2ef542a00cbe7f21ea3c9dcd8cc5dfd493f17108
TwistMeister <roelof.korporaal@gmail.com>
2024-06-06 16:25:48 +0200

    Update modal.py

--- rocky/components/modal/modal.py
+++ rocky/components/modal/modal.py
@@ -3,9 +3,6 @@ from django_components import component
 
 @component.register("modal")
 class Modal(component.Component):
-    def __init__(self):
-        super().__init__()
-
     template_name = "modal/template.html"
 
     def get_context_data(self, **kwargs):
