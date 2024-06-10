c910b31cc7edde3e08631071771fc1f3c7ac45e1
Madelon D <maddohmen@hotmail.com>
2024-06-10 16:30:25 +0200

    Small fixes and typos

--- rocky/reports/report_types/concatenated_report/report.py
+++ rocky/reports/report_types/concatenated_report/report.py
@@ -4,7 +4,12 @@ from reports.report_types.definitions import Report
 
 
 class ConcatenatedReport(Report):
-    id = "concatinated-report"
+    """
+    Since the database only takes one report type for each report, we introduced the ConcatenatedReport class.
+    This class is only used inside the code, to represent multiple reports placed below each other.
+    """
+
+    id = "concatenated-report"
     name = _("Concatenated Report")
     description = "A Concatenated Report shows multiple reports placed below each other in a single report."
     label_style = "5-light"
