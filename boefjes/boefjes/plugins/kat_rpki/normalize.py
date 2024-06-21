6dcde320a99d1d00bf7605bce1d063bd58a86d3c
Noam Blitz <noamblitz@gmail.com>
2024-06-21 12:56:26 +0200

    no findings on private ips

--- boefjes/boefjes/plugins/kat_rpki/normalize.py
+++ boefjes/boefjes/plugins/kat_rpki/normalize.py
@@ -1,5 +1,6 @@
 import json
 from collections.abc import Iterable
+from ipaddress import IPv4Address
 
 from boefjes.job_models import NormalizerOutput
 from octopoes.models import Reference
@@ -10,14 +11,18 @@ def run(input_ooi: dict, raw: bytes) -> Iterable[NormalizerOutput]:
     results = json.loads(raw)
     ooi = Reference.from_str(input_ooi["primary_key"])
 
-    if not results["exists"]:
-        ft = KATFindingType(id="KAT-NO-RPKI")
-        f = Finding(finding_type=ft.reference, ooi=ooi)
-        yield ft
-        yield f
+    address = IPv4Address(ooi.tokenized.address)
 
-    if results["invalid_bgp_entries"]:
-        ft = KATFindingType(id="KAT-INVALID-RPKI")
-        f = Finding(finding_type=ft.reference, ooi=ooi)
-        yield ft
-        yield f
+    # if the address is private, we do not need a ROA
+    if not address.is_private:
+        if not results["exists"]:
+            ft = KATFindingType(id="KAT-NO-RPKI")
+            f = Finding(finding_type=ft.reference, ooi=ooi)
+            yield ft
+            yield f
+
+        if results["invalid_bgp_entries"]:
+            ft = KATFindingType(id="KAT-INVALID-RPKI")
+            f = Finding(finding_type=ft.reference, ooi=ooi)
+            yield ft
+            yield f
