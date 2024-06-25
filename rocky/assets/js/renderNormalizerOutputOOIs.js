be1138abe485cc30e35e4cc6db33372ae21d2e4a
Jan Klopper <janklopper+underdark@gmail.com>
2024-06-25 13:19:09 +0200

    Update renderNormalizerOutputOOIs.js
    remove unneeded code

--- rocky/assets/js/renderNormalizerOutputOOIs.js
+++ rocky/assets/js/renderNormalizerOutputOOIs.js
@@ -111,23 +111,3 @@ function escapeHTMLEntities(input) {
   output = output.replace(/</g, "&lt;");
   return output.replace(/>/g, "&gt;");
 }
-
-function padTo2Digits(num) {
-  return num.toString().padStart(2, '0');
-}
-
-function formatDate(date) {
-  return (
-    [
-      date.getFullYear(),
-      padTo2Digits(date.getMonth() + 1),
-      padTo2Digits(date.getDate()),
-    ].join('-') +
-    'T' +
-    [
-      padTo2Digits(date.getHours()),
-      padTo2Digits(date.getMinutes()),
-      padTo2Digits(date.getSeconds()),
-    ].join(':')
-  );
-}
