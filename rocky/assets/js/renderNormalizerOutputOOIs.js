19ed1c489671e49111485d12261527a6c7654e3b
Jan Klopper <janklopper+underdark@gmail.com>
2024-07-02 11:56:10 +0200

    Update renderNormalizerOutputOOIs.js

--- rocky/assets/js/renderNormalizerOutputOOIs.js
+++ rocky/assets/js/renderNormalizerOutputOOIs.js
@@ -3,7 +3,7 @@ import { language, organization_code } from "./utils.js";
 const buttons = document.querySelectorAll(
   ".expando-button.normalizer-list-table-row",
 );
-const asyncoffset = 15; // time (in seconds) to allow for the database to actually save the OOIs
+const asyncoffset = 5; // time (in seconds) to allow for the database to actually save the OOIs
 
 buttons.forEach((button) => {
   const raw_task_id = button.closest("tr").getAttribute("data-task-id");
@@ -83,7 +83,7 @@ buttons.forEach((button) => {
           let object_list = "";
           // set the observed at time a fews seconds into the future, as the job finish time is not the same as the ooi-creation time. Due to async reasons the object might be a bit slow.
           data["timestamp"] = Date.parse(data["valid_time"]+"Z");
-          data["valid_time_async"] = new Date(data["timestamp"] + asyncoffset)
+          data["valid_time_async"] = new Date(data["timestamp"] + (asyncoffset * 1000))
             .toISOString()
             .substring(0, 19); // strip milliseconds
           // Build HTML snippet for every yielded object.
