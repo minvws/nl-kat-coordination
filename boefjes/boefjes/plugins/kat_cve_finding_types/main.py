2263e416793f5200945033d3f2822bb316d3f805
Jan Klopper <janklopper+underdark@gmail.com>
2024-07-05 11:36:15 +0200

    Always uppercase for CVE-api urls

--- boefjes/boefjes/plugins/kat_cve_finding_types/main.py
+++ boefjes/boefjes/plugins/kat_cve_finding_types/main.py
@@ -6,7 +6,7 @@ from boefjes.job_models import BoefjeMeta
 
 
 def run(boefje_meta: BoefjeMeta) -> list[tuple[set, bytes | str]]:
-    cve_id = boefje_meta.arguments["input"]["id"]
+    cve_id = boefje_meta.arguments["input"]["id"].upper()
     cveapi_url = getenv("CVEAPI_URL", "https://cve.openkat.dev/v1")
     response = requests.get(f"{cveapi_url}/{cve_id}.json", timeout=30)
 
