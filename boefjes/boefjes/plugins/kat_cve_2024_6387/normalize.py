a51911e4b3d811a866b91e27ba4b90d78520692c
noamblitz <43830693+noamblitz@users.noreply.github.com>
2024-07-05 15:09:54 +0200

    Update boefjes/boefjes/plugins/kat_cve_2024_6387/normalize.py
    Co-authored-by: Jan Klopper <janklopper+underdark@gmail.com>

--- boefjes/boefjes/plugins/kat_cve_2024_6387/normalize.py
+++ boefjes/boefjes/plugins/kat_cve_2024_6387/normalize.py
@@ -9,9 +9,11 @@ from octopoes.models.ooi.findings import CVEFindingType, Finding
 def run(input_ooi: dict, raw: bytes) -> Iterable[NormalizerOutput]:
     ooi = Reference.from_str(input_ooi["primary_key"])
 
-    results = json.loads(raw)
-
-    if results["status"] == "vulnerable":
+    if "SSH-2.0-OpenSSH" in raw:
+        if (
+            any(version in raw for version in SSHChecker.vulnerable_versions)
+            and raw not in SSHChecker.excluded_versions
+        ):
         finding_type = CVEFindingType(id="CVE-2024-6387")
         finding = Finding(
             finding_type=finding_type.reference,
