3a7cd31a58ee3f20faf453f8a9b341e47d6f20ba
Jeroen Dekkers <jeroen@dekkers.ch>
2024-07-10 16:06:59 +0200

    Make sure new security updates won't be reported as vulnerable

--- boefjes/boefjes/plugins/kat_cve_2024_6387/normalize.py
+++ boefjes/boefjes/plugins/kat_cve_2024_6387/normalize.py
@@ -9,6 +9,7 @@ from collections.abc import Iterable
 from boefjes.job_models import NormalizerOutput
 from octopoes.models import Reference
 from octopoes.models.ooi.findings import CVEFindingType, Finding
+from packaging.version import Version
 
 VULNERABLE_VERSIONS = [
     "SSH-2.0-OpenSSH_8.5",
@@ -26,14 +27,29 @@ VULNERABLE_VERSIONS = [
     "SSH-2.0-OpenSSH_9.7",
 ]
 
-EXCLUDED_VERSIONS = [
-    "SSH-2.0-OpenSSH_8.9p1 Ubuntu-3ubuntu0.10",
-    "SSH-2.0-OpenSSH_9.3p1 Ubuntu-3ubuntu3.6",
-    "SSH-2.0-OpenSSH_9.6p1 Ubuntu-3ubuntu13.3",
-    "SSH-2.0-OpenSSH_9.3p1 Ubuntu-1ubuntu3.6",
-    "SSH-2.0-OpenSSH_9.2p1 Debian-2+deb12u3",
-    "SSH-2.0-OpenSSH_8.4p1 Debian-5+deb11u3",
-]
+
+def is_vulnerable(banner: str) -> bool:
+    if not any(version in banner for version in VULNERABLE_VERSIONS):
+        return False
+
+    if banner.startswith("SSH-2.0-OpenSSH_9.2p1 Debian-2+deb12u"):
+        _, security_update = banner.split("deb12u")
+        if Version(security_update) >= Version("3"):
+            return False
+    elif banner.startswith("SSH-2.0-OpenSSH_9.6p1 Ubuntu-3ubuntu"):
+        _, security_update = banner.split("3ubuntu")
+        if Version(security_update) >= Version("13.3"):
+            return False
+    elif banner.startswith("SSH-2.0-OpenSSH_9.3p1 Ubuntu-1ubuntu"):
+        _, security_update = banner.split("1ubuntu")
+        if Version(security_update) >= Version("3.6"):
+            return False
+    elif banner.startswith("SSH-2.0-OpenSSH_8.9p1 Ubuntu-3ubuntu"):
+        _, security_update = banner.split("3ubuntu")
+        if Version(security_update) >= Version("0.10"):
+            return False
+
+    return True
 
 
 def run(input_ooi: dict, raw: bytes) -> Iterable[NormalizerOutput]:
@@ -41,11 +57,7 @@ def run(input_ooi: dict, raw: bytes) -> Iterable[NormalizerOutput]:
 
     banner = raw.decode()
 
-    if (
-        "SSH-2.0-OpenSSH" in banner
-        and any(version in banner for version in VULNERABLE_VERSIONS)
-        and banner not in EXCLUDED_VERSIONS
-    ):
+    if banner.startswith("SSH-2.0-OpenSSH") and is_vulnerable(banner):
         finding_type = CVEFindingType(id="CVE-2024-6387")
         finding = Finding(
             finding_type=finding_type.reference,
