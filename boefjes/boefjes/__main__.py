398acc914de0ac047bf8810e4674679f171f4ad4
Ammar <ammar.abdulamir@gmail.com>
2024-07-10 09:13:13 +0200

    Added colors logging for some services

--- boefjes/boefjes/__main__.py
+++ boefjes/boefjes/__main__.py
@@ -20,7 +20,7 @@ structlog.configure(
         structlog.stdlib.PositionalArgumentsFormatter(),
         structlog.processors.TimeStamper("iso", utc=False),
         (
-            structlog.dev.ConsoleRenderer(pad_level=False)
+            structlog.dev.ConsoleRenderer(colors=True, pad_level=False)
             if settings.logging_format == "text"
             else structlog.processors.JSONRenderer()
         ),
