c0b62d8768827f2ee6f9ebe97e161795dff081a8
Lex van Roon <lexvanroon@z-cert.nl>
2024-07-11 14:15:44 +0200

    Limit the number of celery workers that Octopoes can start

--- octopoes/entrypoint.sh
+++ octopoes/entrypoint.sh
@@ -7,9 +7,11 @@ shopt -s nocasematch
 if [ "$1" = "web" ]; then
     exec uvicorn octopoes.api.api:app --host 0.0.0.0 --port 80
 elif [ "$1" = "worker-beat" ]; then
-    exec celery -A octopoes.tasks.tasks worker -B -s /tmp/celerybeat-schedule --loglevel=INFO
+    exec celery -A octopoes.tasks.tasks worker --concurrency="${OCTOPOES_NUM_WORKERS}" \
+      -B -s /tmp/celerybeat-schedule --loglevel=INFO
 elif [ "$1" = "worker" ]; then
-    exec celery -A octopoes.tasks.tasks worker --loglevel=INFO
+    exec celery -A octopoes.tasks.tasks worker --concurrency="${OCTOPOES_NUM_WORKERS}" \
+      --loglevel=INFO
 fi
 
 exec "$@"
