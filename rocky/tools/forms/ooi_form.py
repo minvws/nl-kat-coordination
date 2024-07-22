fbe8eb1c861ff1444fb06dd2a7f583b34118b94a
noamblitz <noamblitz@gmail.com>
2024-07-22 11:45:11 +0200

    add search to frontend

--- rocky/tools/forms/ooi_form.py
+++ rocky/tools/forms/ooi_form.py
@@ -197,3 +197,7 @@ class OOITypeMultiCheckboxForm(BaseRockyForm):
         choices=OOI_TYPE_CHOICES,
         widget=forms.CheckboxSelectMultiple,
     )
+
+
+class OOISearchForm(BaseRockyForm):
+    search = forms.CharField(label=_("Search"), required=False, max_length=256)
