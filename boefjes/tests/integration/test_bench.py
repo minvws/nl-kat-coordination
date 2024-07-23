c3d63bfd0998cbf9e16905f18c8b2e03c9d9a429
Donny Peeters <donny@bitestreams.com>
2024-07-23 12:27:17 +0200

    Create octopoes dump without source_method for the migration test using the `io` endpoints.

--- boefjes/tests/integration/test_bench.py
+++ boefjes/tests/integration/test_bench.py
@@ -1,21 +1,10 @@
 import pytest
 from octopoes.connector.octopoes import OctopoesAPIConnector
 
-from tests.conftest import seed_system
+from tests.loading import get_dummy_data
 
 
 @pytest.mark.slow
 def test_migration(octopoes_api_connector: OctopoesAPIConnector, valid_time):
-    seed_system(octopoes_api_connector, valid_time)
-
-    hostname_range = range(0, 20)
-
-    for x in hostname_range:
-        seed_system(
-            octopoes_api_connector,
-            valid_time,
-            test_hostname=f"{x}.com",
-            test_ip=f"192.0.{x % 7}.{x % 13}",
-            test_ipv6=f"{x % 7}e4d:64a2:cb49:bd48:a1ba:def3:d15d:{x % 5}230",
-        )
+    octopoes_api_connector.import_add(get_dummy_data("old_octopoes_dump.json"))
 
