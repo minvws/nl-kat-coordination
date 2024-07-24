5cc6bf121f6f4745a5bf615497189c551d3720df
originalsouth <mail2benny@gmail.com>
2024-07-24 15:07:22 +0200

    Update octopoes/octopoes/models/__init__.py
    Co-authored-by: Jan Klopper <janklopper+underdark@gmail.com>

--- octopoes/octopoes/models/__init__.py
+++ octopoes/octopoes/models/__init__.py
@@ -77,7 +77,7 @@ class ScanProfileBase(BaseModel):
     scan_profile_type: str
     reference: Reference
     level: ScanLevel
-    user: str | None = None
+    user: int | None = None
 
     def __eq__(self, other):
         if isinstance(other, ScanProfileBase) and self.__class__ == other.__class__:
