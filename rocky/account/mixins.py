f04fe595ae0abffd5712b69443ec45abd11d43ee
originalsouth <mail2benny@gmail.com>
2024-07-24 15:07:37 +0200

    Update rocky/account/mixins.py
    Co-authored-by: Jan Klopper <janklopper+underdark@gmail.com>

--- rocky/account/mixins.py
+++ rocky/account/mixins.py
@@ -136,7 +136,7 @@ class OrganizationView(View):
     def raise_clearance_level(self, ooi_reference: Reference, level: int) -> bool:
         self.verify_raise_clearance_level(level)
         self.octopoes_api_connector.save_scan_profile(
-            DeclaredScanProfile(reference=ooi_reference, level=ScanLevel(level), user=str(self.request.user)),
+            DeclaredScanProfile(reference=ooi_reference, level=ScanLevel(level), user=self.request.user.id),
             datetime.now(timezone.utc),
         )
 
