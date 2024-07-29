d136149d6d7acadf55dbeccbfb2d003cc9730fec
noamblitz <noamblitz@gmail.com>
2024-07-29 14:09:39 +0200

    flexible date in dir name

--- boefjes/boefjes/plugins/kat_maxmind_geoip/main.py
+++ boefjes/boefjes/plugins/kat_maxmind_geoip/main.py
@@ -1,6 +1,9 @@
 import hashlib
 import io
 import json
+import os
+import re
+import shutil
 import tarfile
 from datetime import datetime, timezone
 from os import getenv
@@ -12,7 +15,7 @@ import requests
 from boefjes.job_models import BoefjeMeta
 
 BASE_PATH = Path(getenv("OPENKAT_CACHE_PATH", Path(__file__).parent))
-GEOIP_PATH = BASE_PATH / "GeoLite2-City_20240712/GeoLite2-City.mmdb"
+GEOIP_PATH_PATTERN = r"GeoLite2-City_\d+/GeoLite2-City.mmdb"
 GEOIP_META_PATH = BASE_PATH / "geoip-meta.json"
 GEOIP_SOURCE_URL = "https://download.maxmind.com/geoip/databases/GeoLite2-City/download?suffix=tar.gz"
 GEOIP_CACHE_TIMEOUT = 7200  # in seconds
@@ -24,15 +27,15 @@ def run(boefje_meta: BoefjeMeta) -> list[tuple[set, bytes | str]]:
     input_ = boefje_meta.arguments["input"]
     hash_algorithm = getenv("HASHFUNC", HASHFUNC)
 
-    if not GEOIP_PATH.exists() or cache_out_of_date():
+    if not geoip_file_exists() or cache_out_of_date():
         geoip_meta = refresh_geoip(hash_algorithm)
     else:
         with GEOIP_META_PATH.open() as json_meta_file:
             geoip_meta = json.load(json_meta_file)
 
-    with maxminddb.open_database(
-        "boefjes/plugins/kat_maxmind_geoip/GeoLite2-City_20240712/GeoLite2-City.mmdb"
-    ) as reader:
+    geoip_path = find_geoip_path()
+
+    with maxminddb.open_database(geoip_path) as reader:
         results = reader.get(input_["address"])
 
     return [
@@ -68,10 +71,20 @@ def refresh_geoip(algo: str) -> dict:
     )
     response.raise_for_status()
 
+    remove_old_geolite_data()
+
     file_like_object = io.BytesIO(response.content)
 
     with tarfile.open("r:gz", fileobj=file_like_object) as tf:
-        tf.extract("GeoLite2-City_20240712/GeoLite2-City.mmdb", BASE_PATH)
+        geoip_file = None
+        for member in tf.getmembers():
+            if re.match(GEOIP_PATH_PATTERN, member.name):
+                geoip_file = member
+                break
+        if geoip_file:
+            tf.extract(geoip_file, BASE_PATH)
+        else:
+            raise FileNotFoundError("GeoLite2-City.mmdb not found in the tar archive")
 
     metadata = {
         "timestamp": datetime.now(timezone.utc).isoformat(),
@@ -82,3 +95,27 @@ def refresh_geoip(algo: str) -> dict:
     with open(GEOIP_META_PATH, "w") as meta_file:
         json.dump(metadata, meta_file)
     return metadata
+
+
+def find_geoip_path() -> str:
+    """Find the GeoLite2-City.mmdb file in the BASE_PATH"""
+    for path in BASE_PATH.glob("GeoLite2-City_*/GeoLite2-City.mmdb"):
+        return str(path)
+    raise FileNotFoundError("GeoLite2-City.mmdb file not found in BASE_PATH")
+
+
+def geoip_file_exists() -> bool:
+    """Check if the GeoLite2-City.mmdb file exists in the BASE_PATH"""
+    try:
+        find_geoip_path()
+        return True
+    except FileNotFoundError:
+        return False
+
+
+def remove_old_geolite_data():
+    """Removes old GeoLite2 directory"""
+    for root, dirs, files in os.walk(BASE_PATH, topdown=False):
+        for directory in dirs:
+            dir_path = os.path.join(root, directory)
+            shutil.rmtree(dir_path)
