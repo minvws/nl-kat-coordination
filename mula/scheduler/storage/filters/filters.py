aa5aa2d3ff783d7d5d26855706373f298deb859a
JP Bruins Slot <jpbruinsslot@gmail.com>
2024-07-31 14:43:17 +0200

    Update filters.py

--- mula/scheduler/storage/filters/filters.py
+++ mula/scheduler/storage/filters/filters.py
@@ -48,20 +48,7 @@ class Filter(BaseModel):
         "@?",
         "@@",
     ]
-    value: (
-        str
-        | int
-        | float
-        | bool
-        | datetime.datetime
-        | None
-        | list[str]
-        | list[int]
-        | list[float]
-        | list[bool]
-        | list[datetime.datetime]
-        | list[None]
-    )
+    value: str | int | float | bool | datetime.datetime | None | list[str] | list[int] | list[float] | list[bool] | list[datetime.datetime] | list[None]
 
 
 class FilterRequest(BaseModel):
