ff5d595cfde1362c6647ce9c43aa5e82be3e6d13
JP Bruins Slot <jpbruinsslot@gmail.com>
2024-08-05 13:21:21 +0200

    Restructure scheduler storage module (#3294)
    Co-authored-by: ammar92 <ammar.abdulamir@gmail.com>
    Co-authored-by: Jan Klopper <janklopper+underdark@gmail.com>

--- mula/tests/integration/test_task_store.py
+++ mula/tests/integration/test_task_store.py
@@ -20,6 +20,7 @@ class TaskStoreTestCase(unittest.TestCase):
 
         # Database
         self.dbconn = storage.DBConn(str(self.mock_ctx.config.db_uri))
+        self.dbconn.connect()
         models.Base.metadata.drop_all(self.dbconn.engine)
         models.Base.metadata.create_all(self.dbconn.engine)
 
