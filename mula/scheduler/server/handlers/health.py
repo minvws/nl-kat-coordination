a16cfc733a4436f05535aaa0259518617d28ee5b
JP Bruins Slot <jpbruinsslot@gmail.com>
2024-08-06 12:38:37 +0200

    Restructure scheduler server module (#3295)
    Co-authored-by: stephanie0x00 <9821756+stephanie0x00@users.noreply.github.com>
    Co-authored-by: ammar92 <ammar.abdulamir@gmail.com>
    Co-authored-by: Jan Klopper <janklopper+underdark@gmail.com>
    Co-authored-by: Weblate (bot) <hosted@weblate.org>
    Co-authored-by: LibreTranslate <noreply-mt-libretranslate@weblate.org>
    Co-authored-by: alster <alsternerd@users.noreply.hosted.weblate.org>
    Co-authored-by: Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
    Co-authored-by: Wim Benes <fryskefirefox@gmail.com>
    Co-authored-by: originalsouth <mail2benny@gmail.com>
    Co-authored-by: Jeroen Dekkers <jeroen@dekkers.ch>

---
+++ mula/scheduler/server/handlers/health.py
@@ -0,0 +1,36 @@
+from typing import Any
+
+import fastapi
+import structlog
+from fastapi import status
+
+from scheduler import context, models, version
+
+
+class HealthAPI:
+    def __init__(self, api: fastapi.FastAPI, ctx: context.AppContext) -> None:
+        self.logger: structlog.BoundLogger = structlog.getLogger(__name__)
+        self.api = api
+        self.ctx = ctx
+
+        self.api.add_api_route(
+            path="/health",
+            endpoint=self.health,
+            methods=["GET"],
+            response_model=models.ServiceHealth,
+            status_code=status.HTTP_200_OK,
+            description="Health check endpoint",
+        )
+
+    def health(self, externals: bool = False) -> Any:
+        response = models.ServiceHealth(
+            service="scheduler",
+            healthy=True,
+            version=version.__version__,
+        )
+
+        if externals:
+            for service in self.ctx.services.__dict__.values():
+                response.externals[service.name] = service.is_healthy()
+
+        return response
