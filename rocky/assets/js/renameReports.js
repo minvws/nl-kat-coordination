ff2f1a1633000e991772bfb20d53bd573ba6b8aa
TwistMeister <roelof.korporaal@gmail.com>
2024-08-22 12:45:03 +0200

    Cleanup renameReports.js

--- rocky/assets/js/renameReports.js
+++ rocky/assets/js/renameReports.js
@@ -28,7 +28,7 @@ function watchInputChanges() {
     input.addEventListener("change", function (event) {
       let button = input.closest("tr").querySelector(".reset-button");
 
-      if (input.defaultvalue == input.value) {
+      if (input.defaultvalue === input.value) {
         button.classList.add("hidden");
       } else {
         button.classList.remove("hidden");
@@ -36,37 +36,3 @@ function watchInputChanges() {
     });
   });
 }
-
-// onDomReady(initConfirmButtons);
-
-// export function initConfirmButtons(element) {
-//   let root = element || document;
-//   let confirm_buttons = root.querySelectorAll(".confirm-button");
-
-//   confirm_buttons.forEach((button) => initClickHandlers(button));
-// }
-
-// function initClickHandlers(button) {
-//   button.addEventListener("click", function (event) {
-//     const target = event.target.closest("dialog");
-
-//     editReportName(target);
-//   });
-// }
-
-// function editReportName(target) {
-//   const old_name_id = target.querySelector(".old-report-name").value;
-//   const reference_date = target.querySelector(".reference-date").value;
-//   const update_target_input = document.getElementById(old_name_id);
-//   const update_target_text = document.getElementById("text-" + old_name_id);
-
-//   let new_name = target.querySelector(".new-report-name").value;
-
-//   if (new_name) {
-//     if (reference_date) {
-//       new_name += " (" + reference_date + ")";
-//     }
-//     update_target_input.setAttribute("value", new_name);
-//     update_target_text.textContent = new_name;
-//   }
-// }
