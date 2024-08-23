723fd55e0f5a0e298d7c8b7c1a4ceb4570ba8652
Soufyan Abdellati <abdellatis149@gmail.com>
2024-08-23 09:38:32 +0000

    Added new OOI that has information about outdated software

--- boefjes/boefjes/plugins/kat_nikto/normalize.py
+++ boefjes/boefjes/plugins/kat_nikto/normalize.py
@@ -1,10 +1,11 @@
 import json
 import logging
+import re
 from collections.abc import Iterable
 
 from boefjes.job_models import NormalizerOutput
 from octopoes.models import Reference
-from octopoes.models.ooi.software import Software, SoftwareInstance
+from octopoes.models.ooi.software import OutdatedSoftwareInstance, Software, SoftwareInstance
 
 
 def scan_outdated_software(data: dict, ooi_ref):
@@ -14,9 +15,21 @@ def scan_outdated_software(data: dict, ooi_ref):
             if vulnerability["id"].startswith("6"):
                 software_name, found_version = vulnerability["msg"].split()[0].split("/")
 
+                # REGEX: get the text between "least " and ")" to get the latest version
+                match = re.search(r"least (.*)\)", vulnerability["msg"])
+                if match is None:
+                    logging.error("No version number found on supposedly outdated software")
+                    continue
+                newest_version = match.group(1)
                 software = Software(name=software_name, version=found_version)
+                software_instance = SoftwareInstance(ooi=ooi_ref, software=software.reference)
                 yield software
-                yield SoftwareInstance(ooi=ooi_ref, software=software.reference)
+                yield software_instance
+                yield OutdatedSoftwareInstance(
+                    ooi=ooi_ref,
+                    software_instance=software_instance.reference,
+                    newest_version=newest_version,
+                )
 
 
 def run(input_ooi: dict, raw: bytes) -> Iterable[NormalizerOutput]:
