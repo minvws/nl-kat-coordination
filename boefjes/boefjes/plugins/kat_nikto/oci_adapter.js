d57565a151f9b2c6720b31ec432ac7ad90b10c01
Soufyan Abdellati <abdellatis149@gmail.com>
2024-08-23 11:17:30 +0000

    Rewrote `oci_adapter.js` to work like `oci_adapter.py` does

--- boefjes/boefjes/plugins/kat_nikto/oci_adapter.js
+++ boefjes/boefjes/plugins/kat_nikto/oci_adapter.js
@@ -10,10 +10,7 @@ function b64encode(inp) {
   return Buffer.from(inp).toString("base64");
 }
 
-let out;
-let output_url;
-
-async function main() {
+function main() {
   const input_url = process.argv[process.argv.length - 1];
 
   // Getting the boefje input
@@ -26,52 +23,41 @@ async function main() {
     throw new Error(error);
   }
 
-  output_url = boefje_input.output_url;
-  const raws = run(boefje_input.boefje_meta);
-  console.log("RAWS: " + JSON.stringify(raws));
-  return {
-    status: "COMPLETED",
-    files: raws.map((x) => ({
-      content: b64encode(x[1]),
-      tags: x[0],
-    })),
-  };
-}
-
-main()
-  .then((value) => {
-    out = value;
-  })
-  .catch((reason) => {
+  let out = undefined;
+  let output_url = boefje_input.output_url;
+  try {
+    // Getting the raw files
+    const raws = run(boefje_input.boefje_meta);
+    out = {
+      status: "COMPLETED",
+      files: raws.map((x) => ({
+        content: b64encode(x[1]),
+        tags: x[0],
+      })),
+    };
+  } catch (error) {
     out = {
       status: "FAILED",
       files: [
         {
-          content: b64encode("main caught an error: " + reason),
+          content: b64encode("Boefje caught an error: " + error.message),
           tags: ["error/boefje"],
         },
       ],
     };
-  })
-  .finally(async () => {
-    console.log("Finishing with: " + out);
-    if (out == undefined) return console.error("`out` is undefined.");
+  }
 
-    try {
-      console.log("SENDING OUT WITH: " + JSON.stringify(out));
-      const cmd = `curl --request POST --url ${output_url} --header "Content-Type: application/json" --data ${JSON.stringify(
-        out,
-      ).replaceAll('"', '\\"')}`;
-      console.log(cmd);
-      try {
-        execSync(cmd).toString();
-        console.log("FINISHED");
-      } catch (error) {
-        console.error(
-          "Something went wrong outputting to the boefje api: " + error.message,
-        );
-      }
-    } catch (error) {
-      console.error("SECOND FETCH WENT WRONG: " + error);
-    }
-  });
+  // Example command
+  /*
+    curl --request POST \
+      --url http://boefje/api/v0/tasks/7342e8dd-b945-4185-aaec-787205b7b664 \
+      --header 'Content-Type: application/json' \
+      --data '{"status":"COMPLETED","files":[{"content":"BASE_64_ENCODED_CONTENT","tags":[]}]}'
+  */
+  const out_json = JSON.stringify(out);
+  const cmd = `curl --request POST --url ${output_url} --header "Content-Type: application/json" --data '${out_json}'`;
+
+  execSync(cmd);
+}
+
+main();
