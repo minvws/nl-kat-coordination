459dc3c1128b050452eff8253723052258476a67
Jeroen Dekkers <jeroen@dekkers.ch>
2024-08-29 15:59:57 +0200

    Add indemnification to API

--- rocky/tests/conftest.py
+++ rocky/tests/conftest.py
@@ -1785,3 +1785,12 @@ def boefje_dns_records():
         runnable_hash=None,
         produces={"boefje/dns-records"},
     )
+
+
+@pytest.fixture
+def drf_admin_client(create_drf_client, admin_user):
+    client = create_drf_client(admin_user)
+    # We need to set this so that the test client doesn't throw an
+    # exception, but will return error in the API we can test
+    client.raise_request_exception = False
+    return client
