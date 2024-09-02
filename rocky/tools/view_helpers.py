6ea6d06e7658393ec59052a97b774e384fa33d7d
Rieven <rsnmartis@gmail.com>
2024-09-02 15:42:32 +0200

    Fix import issue

--- rocky/tools/view_helpers.py
+++ rocky/tools/view_helpers.py
@@ -3,9 +3,9 @@ from datetime import date, datetime, timezone
 from typing import TypedDict
 from urllib.parse import urlencode, urlparse, urlunparse
 
-from django.http import HttpRequest, HttpResponseRedirectBase
+from django.http import HttpRequest
+from django.http.response import HttpResponseRedirectBase
 from django.urls.base import reverse, reverse_lazy
-from django.utils.encoding import iri_to_uri
 from django.utils.translation import gettext_lazy as _
 
 from octopoes.models.types import OOI_TYPES
