47576202109f1584f74b928a76abbf20dac5bb43
zcrt <115991818+zcrt@users.noreply.github.com>
2024-09-04 10:56:51 +0200

    feat: :boom: recalculate all bits

--- rocky/rocky/views/organization_list.py
+++ rocky/rocky/views/organization_list.py
@@ -1,8 +1,18 @@
+import logging
+from datetime import datetime
+
 from account.models import KATUser
+from django.conf import settings
+from django.contrib import messages
+from django.core.exceptions import PermissionDenied
 from django.db.models import Count
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import ListView
 from tools.models import Organization
 from tools.view_helpers import OrganizationBreadcrumbsMixin
+
+from octopoes.connector.octopoes import OctopoesAPIConnector
 
 
 class OrganizationListView(
@@ -18,3 +28,32 @@ class OrganizationListView(
             .prefetch_related("tags")
             .filter(id__in=[organization.id for organization in user.organizations])
         )
+
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Perform actions based on action type"""
+        if request.POST.get("action") == "recalculate":
+            if not self.request.user.has_perm("tools.can_recalculate_bits"):
+                raise PermissionDenied()
+            organizations = self.request.user.organizations
+            number_of_bits = 0
+            failed = []
+            start_time = datetime.now()
+            for organization in organizations:
+                try:
+                    number_of_bits += OctopoesAPIConnector(settings.OCTOPOES_API, organization.code).recalculate_bits()
+                except Exception as exc:
+                    failed += [f"{organization}, ({str(exc)})"]
+                    logging.warning("Failed recalculating bits for %s, %s", organization, exc)
+            duration = datetime.now() - start_time
+            message = f"Recalculated {number_of_bits} bits for {len(organizations)-len(failed)} organizations."
+            message += f" Duration: {duration}."
+            if failed:
+                message += f"\nFailed for {len(failed)} organisations: {', '.join(failed)}"
+            messages.add_message(
+                request,
+                messages.INFO,
+                _(message),
+            )
+            return self.get(request, *args, **kwargs)
+        else:
+            raise HttpResponseBadRequest("Unknown action")
