c3a3bb0ca43905da7e521f412e7ef2c3a6f27313
originalsouth <mail2benny@gmail.com>
2024-09-10 09:52:41 +0200

    Update octopoes/octopoes/core/service.py
    Co-authored-by: ammar92 <ammar.abdulamir@gmail.com>

--- octopoes/octopoes/core/service.py
+++ octopoes/octopoes/core/service.py
@@ -162,7 +162,7 @@ class OctopoesService:
 
     def _delete_ooi(self, reference: Reference, valid_time: datetime) -> None:
         referencing_origins = self.origin_repository.list_origins(valid_time, result=reference)
-        if not [ro for ro in referencing_origins if ro.origin_type != OriginType.AFFIRMATION]:
+        if not any(origin for origin in referencing_origins if origin.origin_type != OriginType.AFFIRMATION):
             self.ooi_repository.delete(reference, valid_time)
 
     def save_origin(
