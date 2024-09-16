8eb6e5568df550d8a9ae620ca19779cdb5a9c490
Benny <mail2benny@gmail.com>
2024-09-16 10:48:53 +0200

    Fix duplicate OOI references in result in origin

--- octopoes/octopoes/repositories/origin_repository.py
+++ octopoes/octopoes/repositories/origin_repository.py
@@ -63,6 +63,7 @@ class XTDBOriginRepository(OriginRepository):
         data = origin.model_dump()
         data[cls.pk_prefix] = origin.id
         data["type"] = origin.__class__.__name__
+        data["result"] = list(dict(zip(data["result"], [None] * len(data["result"]))).keys())
         return data
 
     @classmethod
