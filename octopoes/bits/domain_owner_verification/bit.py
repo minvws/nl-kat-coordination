7b0af990ed2307581e137d24d9cf5ede52df1279
Benny <mail2benny@gmail.com>
2024-09-17 11:24:20 +0200

    Fixed maybe @underdarknl?

--- octopoes/bits/domain_owner_verification/bit.py
+++ octopoes/bits/domain_owner_verification/bit.py
@@ -1,10 +1,9 @@
 from bits.definitions import BitDefinition, BitParameterDefinition
 from octopoes.models.ooi.dns.records import DNSNSRecord
-from octopoes.models.ooi.dns.zone import Hostname
 
 BIT = BitDefinition(
     id="domain-owner-verification",
     consumes=DNSNSRecord,
-    parameters=[BitParameterDefinition(ooi_type=Hostname, relation_path="name_server_hostname")],
+    parameters=[BitParameterDefinition(ooi_type=DNSNSRecord, relation_path="name_server_hostname")],
     module="bits.domain_owner_verification.domain_owner_verification",
 )
