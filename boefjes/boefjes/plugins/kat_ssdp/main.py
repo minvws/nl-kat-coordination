5e5823f185cbc16773166b023520163e9b6502fa
Soufyan Abdellati <abdellatis149@gmail.com>
2024-09-26 09:11:33 +0000

    Finished boefje, hacked solution for new issue
    Mentioned issue:
    https://github.com/minvws/nl-kat-coordination/issues/3582

--- boefjes/boefjes/plugins/kat_ssdp/main.py
+++ boefjes/boefjes/plugins/kat_ssdp/main.py
@@ -1,30 +1,26 @@
 import json
 from os import getenv
 
-from ssdpy import SSDPClient
-
-from boefjes.job_models import BoefjeMeta
-
 SEARCHTARGET_DEFAULT = "ssdp:all"
 TIMEOUT_DEFAULT = 10
 
 
-def build_arguments(host: str, ports: str) -> list[str]:
-    return {
-        "search_targets": int(getenv("SEARCHTARGET", SEARCHTARGET_DEFAULT)),
-        "timeout": int(getenv("TIMEOUT", TIMEOUT_DEFAULT)),
-    }
+def run_ssdp(search_targets: str, timeout: int) -> list:
+    from ssdpy import SSDPClient
 
-
-def run_ssdp(search_targets: str, timeout: int):
     client = SSDPClient()
     return client.m_search(st=search_targets, mx=timeout)
 
 
-def run(boefje_meta: BoefjeMeta) -> list[tuple[set, bytes | str]]:
+def run(_) -> list[tuple[set, bytes | str]]:
     return [
         (
             set(),
-            json.dumps(run_ssdp(**build_arguments())),
+            json.dumps(
+                run_ssdp(
+                    getenv("SEARCHTARGET", SEARCHTARGET_DEFAULT),
+                    int(getenv("TIMEOUT", TIMEOUT_DEFAULT)),
+                )
+            ),
         )
     ]
