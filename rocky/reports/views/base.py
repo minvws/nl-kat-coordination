2d8b329e687e3c7141f631dd1437e6edad87fd3e
Rieven <rsnmartis@gmail.com>
2024-09-30 14:48:00 +0200

    merge base

--- rocky/reports/views/base.py
+++ rocky/reports/views/base.py
@@ -9,6 +9,7 @@ import structlog
 from account.mixins import OrganizationView
 from django.conf import settings
 from django.contrib import messages
+from django.core.exceptions import SuspiciousOperation
 from django.forms import Form
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import redirect
@@ -17,7 +18,7 @@ from django.utils.http import urlencode
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import TemplateView
 from django_weasyprint import WeasyTemplateResponseMixin
-from katalogus.client import Boefje, KATalogusError, Plugin, get_katalogus
+from katalogus.client import Boefje, KATalogusClientV1, KATalogusError, Plugin, get_katalogus
 from pydantic import RootModel, TypeAdapter
 from tools.ooi_helpers import create_ooi
 from tools.view_helpers import BreadcrumbsMixin, PostRedirect, url_with_querystring
@@ -28,7 +29,7 @@ from octopoes.models.ooi.reports import ReportRecipe
 from reports.forms import OOITypeMultiCheckboxForReportForm
 from reports.report_types.aggregate_organisation_report.report import AggregateOrganisationReport
 from reports.report_types.concatenated_report.report import ConcatenatedReport
-from reports.report_types.definitions import AggregateReport, BaseReport, Report
+from reports.report_types.definitions import AggregateReport, BaseReport, Report, report_plugins_union
 from reports.report_types.helpers import (
     REPORTS,
     get_report_by_id,
@@ -39,7 +40,6 @@ from reports.report_types.multi_organization_report.report import MultiOrganizat
 from reports.utils import JSONEncoder, debug_json_keys
 from rocky.views.mixins import ObservedAtMixin, OOIList
 from rocky.views.ooi_view import BaseOOIListView, OOIFilterView
-from rocky.views.scheduler import SchedulerView
 
 REPORTS_PRE_SELECTION = {
     "clearance_level": ["2", "3", "4"],
@@ -116,7 +116,6 @@ class ReportBreadcrumbs(OrganizationView, BreadcrumbsMixin):
         context["breadcrumbs"] = self.get_breadcrumbs()
         context["next"] = self.get_next()
         context["previous"] = self.get_previous()
-        context["current"] = self.get_current()
         return context
 
 
@@ -127,6 +126,30 @@ class ReportsLandingView(ReportBreadcrumbs, TemplateView):
 
     def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
         return redirect(reverse("report_history", kwargs=self.get_kwargs()))
+
+
+def hydrate_plugins(report_types: list[type["BaseReport"]], katalogus: KATalogusClientV1) -> dict[str, list[Plugin]]:
+    merged_plugins = report_plugins_union(report_types)
+
+    return {
+        "required": sorted(katalogus.get_plugins(ids=list(merged_plugins["required"])), key=attrgetter("name")),
+        "optional": sorted(katalogus.get_plugins(ids=list(merged_plugins["optional"])), key=attrgetter("name")),
+    }
+
+
+def format_plugin_data(report_type_plugins: dict[str, list[Plugin]]):
+    return [
+        {
+            "required": required_optional == "required",
+            "enabled": plugin.enabled,
+            "name": plugin.name,
+            "scan_level": plugin.scan_level.value if isinstance(plugin, Boefje) else 0,
+            "type": plugin.type,
+            "description": plugin.description,
+        }
+        for required_optional, plugins in report_type_plugins.items()
+        for plugin in plugins
+    ]
 
 
 class BaseReportView(OOIFilterView):
@@ -183,62 +206,16 @@ class BaseReportView(OOIFilterView):
     def get_report_types(self) -> list[type[BaseReport]]:
         return [get_report_by_id(report_type_id) for report_type_id in self.selected_report_types]
 
-    def get_report_plugins_from_katalogus(self, plugins: dict[str, set[str]]) -> dict[str, list[Plugin]]:
-        katalogus_plugins: dict[str, Any] = {"required": [], "optional": []}
-
-        for required_optional, plugin_ids in plugins.items():
-            if plugin_ids:
-                katalogus_plugins[required_optional] = sorted(
-                    get_katalogus(self.organization.code).get_plugins(ids=list(plugin_ids)), key=attrgetter("name")
-                )
-
-        return katalogus_plugins
-
-    def get_plugins_from_report_type(self) -> dict[str, list[Plugin]] | None:
-        """
-        Returns plugins from KAT-alogus from the selected report types.
-        """
-
-        report_types = self.get_report_types()
-        plugins: dict[str, Any] = {"required": set(), "optional": set()}
-
-        for report_type in report_types:
-            for required_optional, report_type_plugin_ids in report_type.plugins.items():
-                plugins[required_optional].update(report_type_plugin_ids)  # also removes duplicates
-
-        # remove optional plugins that is also in the set of required plugins
-        for plugin_id in plugins["required"]:
-            if plugin_id in plugins["optional"]:
-                plugins["optional"].remove(plugin_id)
+    def get_plugin_data_for_saving(self) -> list[dict]:
         try:
-            return self.get_report_plugins_from_katalogus(plugins)
+            report_type_plugins = hydrate_plugins(self.get_report_types(), get_katalogus(self.organization.code))
         except KATalogusError as error:
             return messages.error(self.request, error.message)
 
-    def get_plugin_ids(self, report_type_plugins: dict[str, list[Plugin]]) -> dict[str, list[str]]:
-        plugin_ids: dict[str, list[str]] = {"required": [], "optional": []}
-        for required_optional, plugins in report_type_plugins.items():
-            plugin_ids[required_optional] = [plugin.id for plugin in plugins]
-        return plugin_ids
+        return format_plugin_data(report_type_plugins)
 
-    def get_plugin_data_for_saving(self) -> list[dict]:
-        plugin_data = []
-        report_type_plugins = self.get_plugins_from_report_type()
-        if report_type_plugins is not None:
-            for required_optional, plugins in report_type_plugins.items():
-                for plugin in plugins:
-                    plugin_data.append(
-                        {
-                            "required": required_optional == "required",
-                            "enabled": plugin.enabled,
-                            "name": plugin.name,
-                            "scan_level": plugin.scan_level.value if isinstance(plugin, Boefje) else 0,
-                            "type": plugin.type,
-                            "description": plugin.description,
-                        }
-                    )
-
-        return plugin_data
+    def get_observed_at(self):
+        return self.observed_at if self.observed_at < datetime.now(timezone.utc) else datetime.now(timezone.utc)
 
     def show_report_names(self) -> bool:
         recurrence_choice = self.request.POST.get("choose_recurrence", "once")
@@ -247,54 +224,6 @@ class BaseReportView(OOIFilterView):
     def is_scheduled_report(self) -> bool:
         recurrence_choice = self.request.POST.get("choose_recurrence", "")
         return recurrence_choice == "repeat"
-
-    def save_report_raw(self, data: dict) -> str:
-        report_data_raw_id = self.bytes_client.upload_raw(
-            raw=ReportDataDict(data).model_dump_json().encode(),
-            manual_mime_types={"openkat/report"},
-        )
-
-        return report_data_raw_id
-
-    def save_report_ooi(
-        self,
-        report_data_raw_id: str,
-        report_type: type[BaseReport],
-        input_oois: list[str],
-        parent: Reference | None,
-        has_parent: bool,
-        observed_at: datetime,
-        name: str,
-    ) -> ReportOOI:
-        if not name or name.isspace():
-            name = report_type.name
-        report_ooi = ReportOOI(
-            name=str(name),
-            report_type=str(report_type.id),
-            template=report_type.template_path,
-            report_id=uuid4(),
-            organization_code=self.organization.code,
-            organization_name=self.organization.name,
-            organization_tags=list(self.organization.tags.all()),
-            data_raw_id=report_data_raw_id,
-            date_generated=datetime.now(timezone.utc),
-            input_oois=input_oois,
-            observed_at=observed_at,
-            parent_report=parent,
-            has_parent=has_parent,
-        )
-
-        create_ooi(
-            api_connector=self.octopoes_api_connector,
-            bytes_client=self.bytes_client,
-            ooi=report_ooi,
-            observed_at=observed_at,
-        )
-
-        return report_ooi
-
-    def get_observed_at(self):
-        return self.observed_at if self.observed_at < datetime.now(timezone.utc) else datetime.now(timezone.utc)
 
     def create_report_recipe(self, report_name_format: str, subreport_name_format: str, schedule: str) -> ReportRecipe:
         report_recipe = ReportRecipe(
@@ -423,7 +352,12 @@ class ReportPluginView(BaseReportView, ReportBreadcrumbs, TemplateView):
 
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
-        self.plugins = self.get_plugins_from_report_type()
+        self.plugins = None
+
+        try:
+            self.plugins = hydrate_plugins(self.get_report_types(), get_katalogus(self.organization.code))
+        except KATalogusError as error:
+            messages.error(self.request, error.message)
 
     def post(self, request, *args, **kwargs):
         if self.plugins is None:
@@ -443,19 +377,16 @@ class ReportPluginView(BaseReportView, ReportBreadcrumbs, TemplateView):
         return self.get(request, *args, **kwargs)
 
     def all_plugins_enabled(self) -> bool:
-        enabled_plugins = self.plugins_enabled()
-        return enabled_plugins["required"] and enabled_plugins["optional"]
+        return all(self.plugins_enabled().values())
 
     def plugins_enabled(self) -> dict[str, bool]:
-        enabled_plugins_data: dict[str, bool] = {"required": False, "optional": False}
-        enabled_plugins = []
         if self.plugins is not None:
-            for required_optional, plugins in self.plugins.items():
-                if plugins:
-                    for plugin in plugins:
-                        enabled_plugins.append(plugin.enabled)
-                    enabled_plugins_data[required_optional] = all(enabled_plugins)
-        return enabled_plugins_data
+            return {
+                "required": all([plugin.enabled for plugin in self.plugins["required"]]),
+                "optional": all([plugin.enabled for plugin in self.plugins["optional"]]),
+            }
+
+        return {"required": False, "optional": False}
 
     def get_plugins_data(self):
         report_types: dict[str, Any] = {}
@@ -516,20 +447,13 @@ class ReportPluginView(BaseReportView, ReportBreadcrumbs, TemplateView):
         return context
 
 
-class ReportFinalSettingsView(BaseReportView, ReportBreadcrumbs, SchedulerView, TemplateView):
+class ReportFinalSettingsView(BaseReportView, ReportBreadcrumbs, TemplateView):
     report_type: type[BaseReport] | None = None
-    task_type = "report"
-    is_a_scheduled_report = False
-    show_listes_report_names = False
 
     def post(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
         if not self.get_report_type_ids():
             messages.error(request, self.NONE_REPORT_TYPE_SELECTION_MESSAGE)
             return PostRedirect(self.get_previous())
-
-        self.is_a_scheduled_report = self.is_scheduled_report()
-        self.show_listes_report_names = self.show_report_names()
-
         return super().get(request, *args, **kwargs)
 
     @staticmethod
@@ -573,29 +497,19 @@ class ReportFinalSettingsView(BaseReportView, ReportBreadcrumbs, SchedulerView, 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["reports"] = self.get_report_names()
-
-        context["report_schedule_form_recurrence_choice"] = self.get_report_schedule_form_recurrence_choice()
-        context["report_schedule_form_recurrence"] = self.get_report_schedule_form_recurrence()
-
-        context["report_parent_name_form"] = self.get_report_parent_name_form()
-        context["report_child_name_form"] = self.get_report_child_name_form()
-
-        context["show_listed_report_names"] = self.show_listes_report_names
-        context["is_scheduled_report"] = self.is_a_scheduled_report
-
         context["created_at"] = datetime.now()
         return context
 
 
-class SaveReportView(BaseReportView, ReportBreadcrumbs, SchedulerView):
-    task_type = "report"
-
+class SaveReportView(BaseReportView):
     def post(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
         old_report_names = request.POST.getlist("old_report_name")
-        report_names = request.POST.getlist("report_name", [])
+        report_names = request.POST.getlist("report_name")
         reference_dates = request.POST.getlist("reference_date")
 
-        if self.show_report_names() and report_names:
+        if "" in report_names:
+            raise SuspiciousOperation(_("Empty name should not be possible."))
+        else:
             final_report_names = list(zip(old_report_names, self.finalise_report_names(report_names, reference_dates)))
             report_ooi = self.save_report(final_report_names)
 
@@ -604,22 +518,6 @@ class SaveReportView(BaseReportView, ReportBreadcrumbs, SchedulerView):
                 + "?"
                 + urlencode({"report_id": report_ooi.reference})
             )
-        elif self.is_scheduled_report():
-            report_name_format = request.POST.get("parent_report_name", "")
-            subreport_name_format = request.POST.get("child_report_name", "")
-
-            recurrence = request.POST.get("recurrence", "")
-
-            schedule = self.convert_recurrence_to_cron_expressions(recurrence)
-
-            report_recipe = self.create_report_recipe(report_name_format, subreport_name_format, schedule)
-
-            self.create_report_schedule(report_recipe)
-
-            return redirect(reverse("report_history", kwargs={"organization_code": self.organization.code}))
-
-        messages.error(request, _("Empty name should not be possible."))
-        return PostRedirect(self.get_previous())
 
     @staticmethod
     def finalise_report_names(report_names: list[str], reference_dates: list[str]) -> list[str]:
