b27350016926ed83375545d4f665addaa99fb66e
Madelon D <maddohmen@hotmail.com>
2024-10-10 10:14:25 +0200

    Use generator expression

--- rocky/tools/templatetags/findings_count.py
+++ rocky/tools/templatetags/findings_count.py
@@ -7,7 +7,4 @@ register = template.Library()
 
 @register.filter
 def total_findings(data: dict[str, Any]) -> int:
-    total_findings = 0
-    for ip in data.values():
-        total_findings += int(ip["summary"]["total_findings"])
-    return total_findings
+    return sum(int(ip["summary"]["total_findings"]) for ip in data.values())
