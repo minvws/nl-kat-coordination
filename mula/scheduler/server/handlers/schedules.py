f5b40dfd8865219535571e3a78930e24f577d0c0
JP Bruins Slot <jpbruinsslot@gmail.com>
2024-10-16 13:31:23 +0200

    Add seach endpoint for schedules for scheduler

--- mula/scheduler/server/handlers/schedules.py
+++ mula/scheduler/server/handlers/schedules.py
@@ -5,7 +5,7 @@ from typing import Any
 import fastapi
 import pydantic
 import structlog
-from fastapi import status
+from fastapi import Body, Query, status
 
 from scheduler import context, models, schedulers, storage
 from scheduler.server import serializers, utils
@@ -55,6 +55,15 @@ class ScheduleAPI:
             response_model_exclude_unset=True,
             status_code=200,
             description="Update a schedule",
+        )
+
+        self.api.add_api_route(
+            path="/schedules/search",
+            endpoint=self.search,
+            methods=["POST"],
+            response_model=utils.PaginatedResponse,
+            status_code=200,
+            description="Search schedules",
         )
 
     def list(
@@ -246,3 +255,28 @@ class ScheduleAPI:
             ) from exc
 
         return updated_schedule
+
+    def search(self, filters: storage.filters.FilterRequest | None = None):
+        if filters is None:
+            raise fastapi.HTTPException(
+                status_code=fastapi.status.HTTP_400_BAD_REQUEST, detail="missing search filters"
+            )
+
+        try:
+            results, count = self.ctx.datastores.schedule_store.get_schedules(filters=filters)
+        except storage.filters.errors.FilterError as exc:
+            raise fastapi.HTTPException(
+                status_code=fastapi.status.HTTP_400_BAD_REQUEST, detail=f"invalid filter(s) [exception: {exc}]"
+            ) from exc
+        except storage.errors.StorageError as exc:
+            raise fastapi.HTTPException(
+                status_code=fastapi.status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"error occurred while accessing the database [exception: {exc}]",
+            ) from exc
+        except Exception as exc:
+            self.logger.exception(exc)
+            raise fastapi.HTTPException(
+                status_code=fastapi.status.HTTP_500_INTERNAL_SERVER_ERROR, detail="failed to search schedules"
+            ) from exc
+
+        return results, count
