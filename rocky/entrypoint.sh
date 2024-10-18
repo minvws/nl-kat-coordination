dd3a795c51923af749ac3811b320ae0e49f4dfbc
trideeindhoven <j.hermans@epsys.nl>
2024-10-18 13:45:41 +0200

    fix equal sign

--- rocky/entrypoint.sh
+++ rocky/entrypoint.sh
@@ -4,13 +4,13 @@ set -e
 # Make env variable comparison case insensitive
 shopt -s nocasematch
 
-if [ "$DATABASE_MIGRATION" = "1" ] || [[ $DATABASE_MIGRATION == "true" ]]; then
+if [ "$DATABASE_MIGRATION" == "1" ] || [[ $DATABASE_MIGRATION == "true" ]]; then
     python manage.py migrate --noinput
 fi
 
-if [ "$1" = "web" ]; then
+if [ "$1" == "web" ]; then
     exec granian --interface wsgi rocky.wsgi:application --host 0.0.0.0
-elif [ "$1" = "worker" ]; then
+elif [ "$1" == "worker" ]; then
     exec python3 manage.py worker
 fi
 
