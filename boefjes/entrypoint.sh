f6418170c1043d9676b45afbb2cc62833443109e
trideeindhoven <j.hermans@epsys.nl>
2024-10-18 13:38:47 +0200

    Update entrypoint.sh

--- boefjes/entrypoint.sh
+++ boefjes/entrypoint.sh
@@ -4,19 +4,19 @@ set -e
 # Make env variable comparison case insensitive
 shopt -s nocasematch
 
-if [ "$1" = "boefje" ]; then
+if [ "$1" == "boefje" ]; then
     exec python -m boefjes boefje
-elif [ "$1" = "normalizer" ]; then
+elif [ "$1" == "normalizer" ]; then
     exec python -m boefjes normalizer
 fi
 
 # The migrations and seed are for the KATalogus. They are not inside the if because this way
 # they can also be run when overruling the default cmd
-if [ "$DATABASE_MIGRATION" = "1" ] || [[ $DATABASE_MIGRATION == "true" ]]; then
+if [ "$DATABASE_MIGRATION" == "1" ] || [[ $DATABASE_MIGRATION == "true" ]]; then
     python -m alembic --config /app/boefjes/boefjes/alembic.ini upgrade head
 fi
 
-if [ "$1" = "katalogus" ]; then
+if [ "$1" == "katalogus" ]; then
     exec python -m uvicorn --host 0.0.0.0 boefjes.katalogus.root:app
 fi
 
