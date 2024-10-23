6ef997f063bf472bd1bfe7e24f3f3e4a6ac2ae54
JP Bruins Slot <jpbruinsslot@gmail.com>
2024-10-23 13:03:13 +0200

    Update mula/scheduler/server/errors.py
    Co-authored-by: Jan Klopper <janklopper+underdark@gmail.com>

--- mula/scheduler/server/errors.py
+++ mula/scheduler/server/errors.py
@@ -6,23 +6,23 @@ from scheduler import storage
 
 
 def exception_handler(request: fastapi.Request, exc: Exception):
+    status_code=fastapi.status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail=f"An internal error occurred: {exc}"
+
     if isinstance(exc, storage.filters.errors.FilterError):
-        return JSONResponse(
-            status_code=fastapi.status.HTTP_400_BAD_REQUEST, content={"detail": f"Invalid filter(s): {exc}"}
-        )
+        status_code=fastapi.status.HTTP_400_BAD_REQUEST
+        detail=f"Invalid filter(s): {exc}"
     elif isinstance(exc, storage.errors.StorageError):
-        return JSONResponse(
-            status_code=fastapi.status.HTTP_500_INTERNAL_SERVER_ERROR,
-            content={"detail": f"A database error occurred: {exc}"},
-        )
+        status_code=fastapi.status.HTTP_500_INTERNAL_SERVER_ERROR
+        detail=f"A database error occurred: {exc}"
     elif isinstance(exc, ValidationError):
-        return JSONResponse(
-            status_code=fastapi.status.HTTP_400_BAD_REQUEST, content={"detail": f"Validation error occurred: {exc}"}
-        )
+        status_code=fastapi.status.HTTP_400_BAD_REQUEST
+        detail=f"Validation error occurred: {exc}"
     elif isinstance(exc, fastapi.HTTPException):
-        return fastapi.responses.JSONResponse(status_code=exc.status_code, content={"detail": exc.detail})
-    else:
-        return JSONResponse(
-            status_code=fastapi.status.HTTP_500_INTERNAL_SERVER_ERROR,
-            content={"detail": f"An internal error occurred: {exc}"},
-        )
+        status_code=exc.status_code
+        detail=exc.detail
+
+    return JSONResponse(
+        status_code=status_code,
+        content={"detail": detail})
+    ) 
