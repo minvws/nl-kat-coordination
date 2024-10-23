426a80e2881564b84177ca6e2b165eb57a0d2ac1
TwistMeister <roelof.korporaal@gmail.com>
2024-10-23 13:48:25 +0200

    Improve logic around report bulk action modals

--- rocky/assets/js/reportActionForms.js
+++ rocky/assets/js/reportActionForms.js
@@ -1,8 +1,4 @@
 export function renderRenameSelection(modal, selection) {
-  let form_element = document.getElementById("rename-form");
-  let csrf_token_element = form_element.querySelector(
-    "input[name='csrfmiddlewaretoken']",
-  );
   let report_names = getReportNames(selection);
   let references = [];
 
@@ -10,56 +6,32 @@ export function renderRenameSelection(modal, selection) {
     references.push(input_element.value);
   });
 
-  let content_element = modal.querySelector(".content");
-  content_element.innerHTML = "";
+  let table_element = document.getElementById("rename-table");
+  let table_body = table_element.querySelector("tbody");
+  let table_row = table_element.querySelector("tr.rename-table-row");
+  console.log(table_row);
 
-  let header_element = document.createElement("h3");
-  header_element.innerText = "Rename the following report(s):";
-
-  let table_element = document.createElement("table");
-  let table_heading_row_element = document.createElement("tr");
-  let th_type_element = document.createElement("th");
-  th_type_element.innerText = "Report type";
-  let th_name_element = document.createElement("th");
-  th_name_element.innerText = "Name";
-  let th_date_element = document.createElement("th");
-  th_date_element.innerText = "Add reference date";
-
-  table_heading_row_element.appendChild(th_type_element);
-  table_heading_row_element.appendChild(th_name_element);
-  table_heading_row_element.appendChild(th_date_element);
-  table_element.appendChild(table_heading_row_element);
+  table_body.innerHTML = "";
 
   for (let i = 0; i < references.length; i++) {
-    let table_row_element = document.createElement("tr");
-    let td_type_element = document.createElement("td");
-    let td_name_element = document.createElement("td");
-    let td_date_element = document.createElement("td");
+    let table_row_copy = table_row.cloneNode(true);
+    console.log(table_row_copy);
 
-    let name_input_element = document.createElement("input");
-    name_input_element.setAttribute("type", "text");
+    let type_td = table_row_copy.querySelector("td.type");
+    let name_input_element = table_row_copy.querySelector(".report-name-input");
+    let reference_input_element = table_row_copy.querySelector(
+      ".report-reference-input",
+    );
+    let date_td = table_row_copy.querySelector("td.date");
+
     name_input_element.setAttribute("value", report_names[i]);
-    name_input_element.setAttribute("name", "report_name");
-
-    let reference_input_element = document.createElement("input");
-    reference_input_element.setAttribute("type", "hidden");
     reference_input_element.setAttribute("value", references[i]);
-    reference_input_element.setAttribute("name", "report_reference");
 
-    td_type_element.innerText = "type";
-    td_name_element.appendChild(reference_input_element);
-    td_name_element.appendChild(name_input_element);
-    td_date_element.innerText = "date";
-    table_row_element.appendChild(td_type_element);
-    table_row_element.appendChild(td_name_element);
-    table_row_element.appendChild(td_date_element);
-    table_element.appendChild(table_row_element);
+    type_td.innerText = "type";
+    date_td.innerText = "date";
+
+    table_body.appendChild(table_row_copy);
   }
-
-  form_element.appendChild(csrf_token_element);
-  form_element.appendChild(table_element);
-  content_element.appendChild(header_element);
-  content_element.appendChild(form_element);
 }
 
 export function renderDeleteSelection(modal, selection) {
