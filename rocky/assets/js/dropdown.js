b2b2d6627975efe93bd9ae15e5695f682b089cf4
Rieven <rsnmartis@gmail.com>
2024-10-24 17:17:59 +0200

    fix JS voor more specific dropdown targets and fix some HTML inconsitencies

--- rocky/assets/js/dropdown.js
+++ rocky/assets/js/dropdown.js
@@ -1,35 +1,31 @@
 const dropdowns = document.querySelectorAll(".dropdown");
-let activeDropdown = null; // To track the currently active dropdown
-
-const closeActiveDropdown = () => {
-  if (activeDropdown) {
-    const activeButton = activeDropdown.querySelector(".dropdown-button");
-    const activeList = activeDropdown.querySelector(".dropdown-list");
-
-    activeButton.setAttribute("aria-expanded", "false");
-    activeList.classList.remove("open");
-
-    activeDropdown = null;
-  }
-};
 
 dropdowns.forEach((dropdown) => {
   const dropdownButton = dropdown.querySelector(".dropdown-button");
-  const dropdownList = dropdown.querySelector(".dropdown-list");
 
   const toggle = (e) => {
+    const activeButton = document.querySelector(
+      ".dropdown-button[aria-expanded='true']",
+    );
     const isOpen = dropdownButton.getAttribute("aria-expanded") === "true";
+    const dropdownList = document.getElementById(
+      dropdownButton.getAttribute("aria-controls"),
+    );
 
-    closeActiveDropdown();
+    if (activeButton && activeButton !== dropdownButton) {
+      const activeList = document.getElementById(
+        activeButton.getAttribute("aria-controls"),
+      );
+      activeButton.setAttribute("aria-expanded", "false");
+      activeList.classList.remove("open");
+    }
 
-    if (!isOpen) {
-      dropdownButton.setAttribute("aria-expanded", "true");
-      dropdownList.classList.add("open");
-      activeDropdown = dropdown;
-    } else {
+    if (isOpen) {
       dropdownButton.setAttribute("aria-expanded", "false");
       dropdownList.classList.remove("open");
-      activeDropdown = null;
+    } else {
+      dropdownButton.setAttribute("aria-expanded", "true");
+      dropdownList.classList.add("open");
     }
 
     e.stopPropagation();
@@ -38,8 +34,16 @@ dropdowns.forEach((dropdown) => {
   dropdownButton.addEventListener("click", toggle);
 });
 
-document.addEventListener("click", (e) => {
-  if (activeDropdown && !activeDropdown.contains(e.target)) {
-    closeActiveDropdown();
+document.addEventListener("click", () => {
+  const activeButton = document.querySelector(
+    ".dropdown-button[aria-expanded='true']",
+  );
+
+  if (activeButton) {
+    const activeList = document.getElementById(
+      activeButton.getAttribute("aria-controls"),
+    );
+    activeButton.setAttribute("aria-expanded", "false");
+    activeList.classList.remove("open");
   }
 });
