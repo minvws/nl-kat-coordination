d52859d75b094b8acd8e51a641e0602dbb72b165
Ammar <ammar.abdulamir@gmail.com>
2024-11-06 08:39:30 +0100

    Updates

--- boefjes/boefjes/plugins/kat_wappalyzer/update.py
+++ boefjes/boefjes/plugins/kat_wappalyzer/update.py
@@ -1,8 +1,16 @@
+"""This script is used to update the technologies.json file in the Wappalyzer plugin.
+It downloads the latest technology data from https://github.com/enthec/webappanalyzer, a project that maintains
+Wappalyzer technologies files
+"""
+
 import json
 
 from Wappalyzer.data.update import get_technology_data
 
 if __name__ == "__main__":
+    # get the latest technology data
     data = get_technology_data()
+
+    # write the data to the technologies.json file
     with open("technologies.json", "w") as f:
         json.dump(data, f)
