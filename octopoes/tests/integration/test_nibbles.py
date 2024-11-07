8ff6fac79c101709d62b34bb657bf06c9957033e
Benny <mail2benny@gmail.com>
2024-11-07 15:34:15 +0100

    Add unit test

--- octopoes/tests/integration/test_nibbles.py
+++ octopoes/tests/integration/test_nibbles.py
@@ -13,11 +13,13 @@ from octopoes.models.ooi.network import Network
 if os.environ.get("CI") != "1":
     pytest.skip("Needs XTDB multinode container.", allow_module_level=True)
 
+NMAX = 13
 dummy_nibble = NibbleDefinition(name="dummy", signature=[NibbleParameterDefinition(ooi_type=Network)])
 
 
 def dummy(network: Network):
-    if len(network.name) < 27:
+    global NMAX
+    if len(network.name) < NMAX:
         new_name = network.name + "I"
         return Network(name=new_name)
 
@@ -25,7 +27,7 @@ def dummy(network: Network):
 dummy_nibble.payload = getattr(sys.modules[__name__], "dummy")
 
 
-def test_unicode_network(xtdb_octopoes_service: OctopoesService, event_manager: Mock, valid_time: datetime):
+def test_dummy_nibble(xtdb_octopoes_service: OctopoesService, event_manager: Mock, valid_time: datetime):
     xtdb_octopoes_service.nibbles.nibbles = [dummy_nibble]
     network = Network(name="internet")
     xtdb_octopoes_service.ooi_repository.save(network, valid_time)
@@ -40,6 +42,6 @@ def test_unicode_network(xtdb_octopoes_service: OctopoesService, event_manager: 
     xtdb_octopoes_service.scan_profile_repository.save(sp, new_sp, valid_time)
     event_manager.complete_process_events(xtdb_octopoes_service)
 
-    ctx = 28 - len(network.name)
+    ctx = 1 + NMAX - len(network.name)
     assert xtdb_octopoes_service.ooi_repository.list_oois({Network}, valid_time).count == ctx
     assert xtdb_octopoes_service.ooi_repository.list_oois({OOI}, valid_time).count == 3 * ctx
