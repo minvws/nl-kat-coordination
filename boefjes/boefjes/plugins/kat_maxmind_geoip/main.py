22556759c032602b4edfb1137125c8a331bea5fb
Jan Klopper <janklopper+underdark@gmail.com>
2024-11-27 13:05:28 +0100

    Dont check for Locations on local Ip's.

--- boefjes/boefjes/plugins/kat_maxmind_geoip/main.py
+++ boefjes/boefjes/plugins/kat_maxmind_geoip/main.py
@@ -6,6 +6,7 @@ import re
 import shutil
 import tarfile
 from datetime import datetime, timezone
+from ipaddress import ip_address
 from os import getenv
 from pathlib import Path
 
@@ -25,8 +26,13 @@ REQUEST_TIMEOUT = 30
 
 def run(boefje_meta: BoefjeMeta) -> list[tuple[set, bytes | str]]:
     input_ = boefje_meta.arguments["input"]
+    ip = input_["address"]
     hash_algorithm = getenv("HASHFUNC", HASHFUNC)
 
+    # if the address is private, we do not need a Location
+    if not ip_address(ip).is_global:
+        return [(set(), json.dumps("IP address is private, no location possible"))]
+    
     if not geoip_file_exists() or cache_out_of_date():
         geoip_meta = refresh_geoip(hash_algorithm)
     else:
