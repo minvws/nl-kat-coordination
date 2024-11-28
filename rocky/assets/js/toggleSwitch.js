4d7ff38886e8e2ee9a532d283ada6b9187f68572
TwistMeister <roelof.korporaal@gmail.com>
2024-11-28 12:10:40 +0100

    Implement toggle switch JS on report OOI selection

---
+++ rocky/assets/js/toggleSwitch.js
@@ -0,0 +1,42 @@
+import { onDomReady } from "../js/imports/utils.js";
+
+onDomReady(initToggle);
+
+function initToggle() {
+  document.querySelector(".manual-selection-form").classList.add("hidden");
+  let switch_button_list = document.getElementsByClassName(
+    "toggle-switch-button",
+  );
+
+  for (let i = 0; i < switch_button_list.length; i++) {
+    console.log(switch_button_list[i]);
+    switch_button_list[i].addEventListener("click", (event) => {
+      toggle();
+    });
+  }
+}
+function toggle() {
+  let manual_form = document.querySelector(".manual-selection-form");
+  let live_set_form = document.querySelector(".live-set-form");
+
+  if (manual_form.classList.contains("hidden")) {
+    document
+      .querySelector(".manual-selection-button")
+      .parentElement.setAttribute("aria-current", "true");
+    document
+      .querySelector(".live-set-button")
+      .parentElement.setAttribute("aria-current", "false");
+  }
+
+  if (live_set_form.classList.contains("hidden")) {
+    document
+      .querySelector(".live-set-button")
+      .parentElement.setAttribute("aria-current", "true");
+    document
+      .querySelector(".manual-selection-button")
+      .parentElement.setAttribute("aria-current", "false");
+  }
+
+  manual_form.classList.toggle("hidden");
+  live_set_form.classList.toggle("hidden");
+}
