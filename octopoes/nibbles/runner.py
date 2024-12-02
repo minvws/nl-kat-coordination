0ebdc9d304bc4ed4cf33f5ef6f89b39fe8ee168d
Benny <mail2benny@gmail.com>
2024-12-02 12:44:46 +0100

    Add OOI caching

--- octopoes/nibbles/runner.py
+++ octopoes/nibbles/runner.py
@@ -25,6 +25,23 @@ def merge_with(func: Callable[[set[T], set[T]], set[T]], d1: dict[U, set[T]], d2
     return {k: func(d1.get(k, set()), d2.get(k, set())) for k in set(d1) | set(d2)}
 
 
+def merge_results(
+    d1: dict[OOI, dict[str, dict[tuple[Any, ...], set[OOI]]]], d2: dict[OOI, dict[str, dict[tuple[Any, ...], set[OOI]]]]
+) -> dict[OOI, dict[str, dict[tuple[Any, ...], set[OOI]]]]:
+    return {
+        key: {
+            nibble_id: {
+                arg: set(d1.get(key, {}).get(nibble_id, {}).get(arg, set()))
+                | set(d2.get(key, {}).get(nibble_id, {}).get(arg, set()))
+                for arg in set(d1.get(key, {}).get(nibble_id, {}).keys())
+                | set(d2.get(key, {}).get(nibble_id, {}).keys())
+            }
+            for nibble_id in set(d1.get(key, {}).keys()) | set(d2.get(key, {}).keys())
+        }
+        for key in set(d1.keys()) | set(d2.keys())
+    }
+
+
 def flatten(items: Iterable[OOI | Iterable[OOI | None] | None]) -> Iterable[OOI]:
     for item in items:
         if isinstance(item, OOI):
@@ -60,6 +77,7 @@ class NibblesRunner:
         self.origin_repository = origin_repository
         self.scan_profile_repository = scan_profile_repository
         self.perform_writes = perform_writes
+        self.cache: dict[OOI, dict[str, dict[tuple[Any, ...], set[OOI]]]] = {}
         self.update_nibbles()
 
     def update_nibbles(self):
@@ -87,11 +105,12 @@ class NibblesRunner:
                 return_value |= {nibble.id: results}
         nibblet_nibbles = {self.nibbles[nibblet.method] for nibblet in nibblets}
         for nibble in filter(lambda x: type(ooi) in x.signature and x not in nibblet_nibbles, self.nibbles.values()):
+            if len(nibble.signature) > 1 and self.cache:
+                self._write(valid_time)
             args = self.ooi_repository.nibble_query(ooi, nibble, valid_time)
             results = {tuple(arg): set(flatten([nibble(arg)])) for arg in args}
             return_value |= {nibble.id: results}
-        # TODO: we could cache the writes for single OOI nibbles
-        self._write({ooi: return_value}, valid_time)
+        self.cache = merge_results(self.cache, {ooi: return_value})
         return return_value
 
     def _cleared(self, ooi: OOI, valid_time: datetime) -> bool:
@@ -99,9 +118,9 @@ class NibblesRunner:
         target_nibbles = filter(lambda x: type(ooi) in x.signature, self.nibbles.values())
         return any(nibble.min_scan_level < ooi_level for nibble in target_nibbles)
 
-    def _write(self, inferences: dict[OOI, dict[str, dict[tuple[Any, ...], set[OOI]]]], valid_time: datetime):
+    def _write(self, valid_time: datetime):
         if self.perform_writes:
-            for source_ooi, results in inferences.items():
+            for source_ooi, results in self.cache.items():
                 self.ooi_repository.save(source_ooi, valid_time)
                 for nibble_id, run_result in results.items():
                     for arg, result in run_result.items():
@@ -116,6 +135,7 @@ class NibblesRunner:
                         for ooi in result:
                             self.ooi_repository.save(ooi, valid_time=valid_time)
                         self.origin_repository.save(nibble_origin, valid_time=valid_time)
+            self.cache = {}
 
     def infer(self, stack: list[OOI], valid_time: datetime) -> dict[OOI, dict[str, dict[tuple[Any, ...], set[OOI]]]]:
         inferences: dict[OOI, dict[str, dict[tuple[Any, ...], set[OOI]]]] = {}
@@ -129,4 +149,6 @@ class NibblesRunner:
                     stack += [o for o in blocks if o not in blockset]
                     blockset |= blocks
                     inferences |= {ooi: results}
+        if self.cache:
+            self._write(valid_time)
         return inferences
