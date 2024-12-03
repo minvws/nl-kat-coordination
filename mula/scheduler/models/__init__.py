61773b4458f719bb4290ce3b34be37cfb15bb422
Jeroen Dekkers <jeroen@dekkers.ch>
2024-12-04 00:04:49 +0100

    Fix missing import

--- mula/scheduler/models/__init__.py
+++ mula/scheduler/models/__init__.py
@@ -1,9 +1,9 @@
 from .base import Base
 from .boefje import Boefje, BoefjeMeta
-from .events import RawDataReceivedEvent
+from .events import RawData, RawDataReceivedEvent
 from .health import ServiceHealth
 from .normalizer import Normalizer
-from .ooi import OOI, MutationOperationType, ScanProfileMutation
+from .ooi import OOI, MutationOperationType, ScanProfile, ScanProfileMutation
 from .organisation import Organisation
 from .plugin import Plugin
 from .queue import Queue
