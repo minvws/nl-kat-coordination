b63730d3b32dfb946a17819c34c03b28defeb2e6
TwistMeister <roelof.korporaal@gmail.com>
2024-12-05 10:12:37 +0100

    Improved toggle script and made generic

--- rocky/assets/js/toggleSwitch.js
+++ rocky/assets/js/toggleSwitch.js
@@ -1,41 +1,51 @@
 import { onDomReady } from "../js/imports/utils.js";
 
-onDomReady(initToggle);
+onDomReady(initToggleSwitches);
 
-function initToggle() {
-  document.querySelector(".manual-selection-form").classList.add("hidden");
-  let switch_button_list = document.getElementsByClassName(
-    "toggle-switch-button",
-  );
+function initToggleSwitches() {
+  // Switches get stitches ;-)
+  let toggle_switches = document.querySelectorAll(".toggle-switch");
 
-  for (let i = 0; i < switch_button_list.length; i++) {
-    switch_button_list[i].addEventListener("click", (event) => {
-      toggle();
-    });
+  for (let i = 0; i < toggle_switches.length; i++) {
+    let options = toggle_switches[i].querySelectorAll(".toggle-switch-button");
+
+    for (let j = 0; j < options.length; j++) {
+      let option = options[j];
+
+      // Hide all elements linked to toggle switch options.
+      document
+        .getElementById(option.getAttribute("data-target-id"))
+        .classList.add("hidden");
+
+      // Add click listener to switch options.
+      option.addEventListener("click", (event) => {
+        toggle(event.target, options);
+      });
+    }
+
+    // Initially show first option contents from toggle-switch.
+    document
+      .getElementById(options[0].getAttribute("data-target-id"))
+      .classList.remove("hidden");
   }
 }
-function toggle() {
-  let manual_form = document.querySelector(".manual-selection-form");
-  let live_set_form = document.querySelector(".live-set-form");
 
-  if (manual_form.classList.contains("hidden")) {
+function toggle(target, options) {
+  let target_li = target.closest("li");
+
+  // Check if target isn't already the active one.
+  if (!target_li.hasAttribute("aria-current", "true")) {
+    for (let i = 0; i < options.length; i++) {
+      // Toggle all options to "non active" state.
+      options[i].closest("li").removeAttribute("aria-current", "false");
+      document
+        .getElementById(options[i].getAttribute("data-target-id"))
+        .classList.add("hidden");
+    }
+    // Toggle selected option (target) to active state.
+    target_li.setAttribute("aria-current", "true");
     document
-      .querySelector(".manual-selection-button")
-      .parentElement.setAttribute("aria-current", "true");
-    document
-      .querySelector(".live-set-button")
-      .parentElement.setAttribute("aria-current", "false");
+      .getElementById(target.getAttribute("data-target-id"))
+      .classList.remove("hidden");
   }
-
-  if (live_set_form.classList.contains("hidden")) {
-    document
-      .querySelector(".live-set-button")
-      .parentElement.setAttribute("aria-current", "true");
-    document
-      .querySelector(".manual-selection-button")
-      .parentElement.setAttribute("aria-current", "false");
-  }
-
-  manual_form.classList.toggle("hidden");
-  live_set_form.classList.toggle("hidden");
 }
