1495aefa801e836fcd15447f3d2b96f428de219d
Benny <mail2benny@gmail.com>
2024-12-10 15:36:53 +0100

    Retire port_common

--- octopoes/nibbles/port_common/nibble.py
+++
@@ -1,4 +0,0 @@
-from nibbles.definitions import NibbleDefinition, NibbleParameter
-from octopoes.models.ooi.network import IPPort
-
-NIBBLE = NibbleDefinition(name="port-common", signature=[NibbleParameter(object_type=IPPort)])
