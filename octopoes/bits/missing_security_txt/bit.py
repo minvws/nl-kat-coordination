79b806a6df83c9b908b03eacd4330ccd2518b7e8
Jan Klopper <jan@underdark.nl>
2024-12-30 15:54:22 +0100

    linting

--- octopoes/bits/missing_security_txt/bit.py
+++ octopoes/bits/missing_security_txt/bit.py
@@ -4,8 +4,6 @@ from octopoes.models.ooi.web import SecurityTXT, Website
 BIT = BitDefinition(
     id="missing_security_txt",
     consumes=Website,
-    parameters=[
-        BitParameterDefinition(ooi_type=SecurityTXT, relation_path="website"),
-    ],
+    parameters=[BitParameterDefinition(ooi_type=SecurityTXT, relation_path="website")],
     module="bits.missing_security_txt.missing_security_txt",
 )
