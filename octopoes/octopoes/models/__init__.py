1b8a820a5554ffba7645a9f00e292d272772b8a0
Benny <mail2benny@gmail.com>
2024-12-30 10:54:22 +0100

    Better python hashing of OOIs

--- octopoes/octopoes/models/__init__.py
+++ octopoes/octopoes/models/__init__.py
@@ -240,7 +240,7 @@ class OOI(BaseModel):
             return str(value)
 
     def __hash__(self):
-        return hash(self.primary_key)
+        return hash(self.model_dump_json())
 
 
 OOIClassType = TypeVar("OOIClassType")
