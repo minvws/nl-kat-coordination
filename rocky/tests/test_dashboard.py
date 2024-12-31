eca40ea82f2149ed19c1d9f1f05056acced590ad
Madelon D <maddohmen@hotmail.com>
2024-12-31 11:12:55 +0100

    Add setup for tests

--- rocky/tests/test_dashboard.py
+++ rocky/tests/test_dashboard.py
@@ -15,3 +15,19 @@ def test_crisis_room_findings_dashboard(rf, mocker, client_member, findings_dash
     print(response.__dict__)
 
     assert response.status_code == 302
+
+
+def test_get_organizations_findings():
+    """Test if the highest risk level is collected, only critical and high (maximum 25) finding types are returned."""
+
+
+def test_collect_findings_dashboard():
+    """Test if the method returns the right dict format. Only the most recent report should be visible in the dict."""
+
+
+def test_get_organizations_findings_summary():
+    """Test if the summary gives the right results. The finding_types and occurrences should be counted correctly."""
+
+
+def test_get_user_organizations():
+    """Test if the correct organizations are collected by the selected user."""
