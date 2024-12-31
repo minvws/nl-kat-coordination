c9448bc52b97fa05911a97c649ab34fea8ce0ec6
Benny <mail2benny@gmail.com>
2024-12-31 12:52:32 +0100

    Sync upstream dissallowed-csp-hostnames bit --> nibble

--- octopoes/nibbles/disallowed_csp_hostnames/disallowed_csp_hostnames.py
+++ octopoes/nibbles/disallowed_csp_hostnames/disallowed_csp_hostnames.py
@@ -34,6 +34,8 @@ def nibble(input_ooi: HTTPHeaderHostname, config: Config | None) -> Iterator[OOI
 
     disallowed_domains.extend(disallowed_hostnames_from_config)
     hostnameparts = hostname.lower().split(".")
+
+    # For e.g. ["www", "example", "com"], check "www.example.com", "example.com" and "com"
     for i in range(len(hostnameparts)):
         if ".".join(hostnameparts[i:]) in disallowed_domains:
             ft = KATFindingType(id="KAT-DISALLOWED-DOMAIN-IN-CSP")
