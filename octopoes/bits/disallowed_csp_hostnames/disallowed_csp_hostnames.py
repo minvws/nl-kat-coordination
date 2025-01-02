031bd7bbf4fb066fe8e7ccd4431c74c1f2f6449b
Jan Klopper <janklopper+underdark@gmail.com>
2025-01-02 13:30:09 +0100

    Update disallowed_csp_hostnames.py

--- octopoes/bits/disallowed_csp_hostnames/disallowed_csp_hostnames.py
+++ octopoes/bits/disallowed_csp_hostnames/disallowed_csp_hostnames.py
@@ -32,7 +32,7 @@ def run(input_ooi: HTTPHeaderHostname, additional_oois: list, config: dict[str, 
 
     disallowed_domains.extend(disallowed_hostnames_from_config)
     hostnameparts = hostname.lower().split(".")
-    
+
     # For e.g. ["www", "example", "com"], check "www.example.com", "example.com" and "com"
     for i in range(len(hostnameparts)):
         if ".".join(hostnameparts[i:]) in disallowed_domains:
