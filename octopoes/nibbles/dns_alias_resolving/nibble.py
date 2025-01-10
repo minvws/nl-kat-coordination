bb91258617b29064b65af45a3e066e73b0724634
Soufyan Abdellati <abdellatis149@gmail.com>
2025-01-10 14:08:16 +0100

    First version of new query (before removing hostname)

--- octopoes/nibbles/dns_alias_resolving/nibble.py
+++ octopoes/nibbles/dns_alias_resolving/nibble.py
@@ -4,36 +4,82 @@ from octopoes.models.ooi.dns.records import DNSCNAMERecord
 from octopoes.models.ooi.dns.zone import Hostname, ResolvedHostname
 
 
-def query(params: list[Reference]) -> str:
-    hostname, dns_record, resolved_hostname = list(f'"{target}"' for target in params)
-    return f"""
-        {{
-        :query {{
-                    :find [(pull ?var [*])]
-                    :where [
-                        (or
-                            (and
-                                [?var :object_type "Hostname"]
-                                [?var :Hostname/primary_key {hostname}]
-                                [?var :Hostname/name ?hostname]
-                            )
+def query(params: list[Reference | None]) -> str:
+    hostname, dns_record, resolved_hostname = params
 
-                            (and
-                                [?var :object_type "DNSCNAMERecord"]
-                                [?var :DNSCNAMERecord/primary_key {dns_record}]
-                                [?var :DNSCNAMERecord/target_hostname ?hostname]
-                            )
+    def pull(query: str) -> str:
+        return f"""
+                {{
+                    :query {{
+                        :find [(pull ?hostname [*]) (pull ?dns_record [*]) (pull ?resolved_hostname [*])] :where [
+                            {query}
+                        ]
+                    }}
+                }}
+        """
 
-                            (and
-                                [?var :object_type "ResolvedHostname"]
-                                [?var :ResolvedHostname/primary_key {resolved_hostname}]
-                                [?var :ResolvedHostname/hostname ?hostname]
-                            )
-                        )
-                    ]
-            }}
-        }}
+    dns_record_from_hostname = """
+        [?dns_record :object_type "DNSCNAMERecord"]
+        [?dns_record :DNSCNAMERecord/target_hostname ?hostname]
     """
+
+    resolved_hostname_from_hostname = """
+        [?resolved_hostname :object_type "ResolvedHostname"]
+        [?resolved_hostname :ResolvedHostname/hostname ?hostname]
+    """
+
+    sgn = "".join(str(int(isinstance(target, Reference))) for target in params)
+    if sgn == "100":
+        return pull(
+            f"""
+                [?hostname :object_type "Hostname"]
+                [?hostname :Hostname/primary_key "{hostname}"]
+
+                {dns_record_from_hostname}
+
+                {resolved_hostname_from_hostname}
+            """
+        )
+    elif sgn == "010":
+        return pull(
+            f"""
+                [?dns_record :object_type "DNSCNAMERecord"]
+                [?dns_record :DNSCNAMERecord/primary_key "{dns_record}"]
+                [?dns_record :DNSCNAMERecord/target_hostname ?hostname]
+
+                {resolved_hostname_from_hostname}
+            """
+        )
+    elif sgn == "001":
+        return pull(
+            f"""
+                [?resolved_hostname :object_type "ResolvedHostname"]
+                [?resolved_hostname :ResolvedHostname/primary_key "{resolved_hostname}"]
+                [?resolved_hostname :ResolvedHostname/hostname ?hostname]
+
+                {dns_record_from_hostname}
+            """
+        )
+    elif sgn == "111":
+        return pull(
+            f"""
+                [?hostname :object_type "Hostname"]
+                [?hostname :Hostname/primary_key "{hostname}"]
+                [?dns_record :object_type "DNSCNAMERecord"]
+                [?dns_record :DNSCNAMERecord/primary_key "{dns_record}"]
+                [?resolved_hostname :object_type "ResolvedHostname"]
+                [?resolved_hostname :ResolvedHostname/primary_key "{resolved_hostname}"]
+            """
+        )
+    else:
+        # ? Is there a reason for this to be an option?
+        return pull(
+            """
+                [?hostname :object_type "Hostname"]
+                [?dns_record :object_type "DNSCNAMERecord"]
+                [?resolved_hostname :object_type "ResolvedHostname"]
+            """
+        )
 
 
 NIBBLE = NibbleDefinition(
