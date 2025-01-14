46c3a383d61d513e70dee29496980987c9f1fdb8
JP Bruins Slot <jpbruinsslot@gmail.com>
2025-01-14 13:10:48 +0100

    Update tests

--- boefjes/tests/test_api.py
+++ boefjes/tests/test_api.py
@@ -11,7 +11,6 @@ from tests.loading import get_dummy_data
 
 def _mocked_scheduler_client(tmp_path: Path):
     return MockSchedulerClient(
-        queue_response=get_dummy_data("scheduler/queues_response.json"),
         boefje_responses=[get_dummy_data("scheduler/pop_response_boefje_no_ooi.json")],
         normalizer_responses=[],
         log_path=tmp_path / "patch_task_log",
@@ -38,7 +37,7 @@ def test_boefje_input_running(api, tmp_path):
     assert response.status_code == 200
     assert response.json() == {
         "task_id": "70da7d4f-f41f-4940-901b-d98a92e9014b",
-        "output_url": "http://placeholder:8006/api/v0/tasks/70da7d4f-f41f-4940-901b-d98a92e9014b",
+        "output_url": "http://placeholder:1234/api/v0/tasks/70da7d4f-f41f-4940-901b-d98a92e9014b",
         "boefje_meta": {
             "arguments": {"oci_arguments": []},
             "boefje": {"id": "dns-records", "version": None},
