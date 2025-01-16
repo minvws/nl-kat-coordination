4fa7934ae8092dda6a3a2c9b13f93088fea2f2f7
Rieven <rsnmartis@gmail.com>
2025-01-16 18:28:31 +0100

    fix test

--- rocky/tests/onboarding/test_onboarding_organization_steps.py
+++ rocky/tests/onboarding/test_onboarding_organization_steps.py
@@ -310,6 +310,7 @@ def test_onboarding_ooi_detail_scan_create_report_schedule(
     member = request.getfixturevalue(member)
 
     mocker.patch("account.mixins.OrganizationView.get_katalogus")
+    mocker.patch("onboarding.views.scheduler_client")
     mock_organization_view_octopoes().get.return_value = url
     mock_bytes_client().upload_raw.return_value = "raw_id"
 
