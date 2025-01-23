546e631048e4492548072a3ce2a99f038579886b
Rieven <rsnmartis@gmail.com>
2025-01-23 16:37:06 +0100

    cosmetic fixes

--- rocky/reports/views/report_overview.py
+++ rocky/reports/views/report_overview.py
@@ -93,7 +93,7 @@ class ScheduledReportsView(BreadcrumbsReportOverviewView, SchedulerView, ListVie
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["total_schedules"] = len(self.object_list)
+        context["total_report_schedules"] = len(self.object_list)
         return context
 
 
