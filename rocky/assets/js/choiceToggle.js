9b3a94e8cfbc85a22f189d130110cd35e7a72312
Jan Klopper <janklopper+underdark@gmail.com>
2025-02-07 09:46:34 +0100

    Update choiceToggle.js

--- rocky/assets/js/choiceToggle.js
+++ rocky/assets/js/choiceToggle.js
@@ -1,21 +1,25 @@
-function toggleChoice(form, group, value) {
+function toggleChoice(form, group, active_value) {
   // Groups are input/label pairs i their respective parent Div's
   // find all groups that are bound to this toggler by its data-choicegroup atribute value
-  let groupfields = form.querySelectorAll('.'+group);
-  groupfields.forEach(function (groupfield) {
-    // find the label, form there we find the bounding parent div.
-    let associatedLabel = form.querySelector("label[for='" + groupfield.id + "']");
+  let all_groups = form.querySelectorAll('.'+group);
+  all_groups.forEach(function (groupfield) {
+    // find the label, from there we find the bounding parent div.
+    let associated_label = form.querySelector(
+      "label[for='" + groupfield.id + "']"
+    );
     // lets hide them all initially
-    associatedLabel.parentNode.classList.add("hidden");
+    associated_label.parentNode.classList.add("hidden");
   });
 
-  if(value){
+  if (active_value) {
     // find all groups that should be visible
-    let active_groups = form.querySelectorAll('.'+group+'.'+value);
+    let active_groups = form.querySelectorAll("." + group + "." + active_value);
     active_groups.forEach(function (active_group) {
-      // find the label, form there we find the bounding parent div.
-      let associated_active_Label = form.querySelector("label[for='" + active_group.id + "']");
-      associated_active_Label.parentNode.classList.remove("hidden");
+      // find the label, from there we find the bounding parent div.
+      let associated_active_label = form.querySelector(
+        "label[for='" + active_group.id + "']"
+      );
+      associated_active_label.parentNode.classList.remove("hidden");
     });
   }
 }
@@ -27,16 +31,16 @@ function initChoiceTogglers() {
     
     // are there any currently active choices?
     let initial = form.querySelector("input.radio-choice:checked");
-    if (initial){
+    if (initial) {
       toggleChoice(form, initial.dataset.choicegroup, initial.value);
     }
     // lets catch all change events on the forms, and filter out those that are created by inputs with out radio-choice class
     form.addEventListener("change", function (event) {
       let tag = event.target;
-      if (tag.tagName == "INPUT" && tag.classList.contains('radio-choice')){
-        let visibleGroup = tag.value;
-        let toggleGroup = tag.dataset.choicegroup;
-        toggleChoice(tag.form, toggleGroup, visibleGroup);
+      if (tag.tagName == "INPUT" && tag.classList.contains("radio-choice")){
+        let visible_group = tag.value;
+        let toggle_group = tag.dataset.choicegroup;
+        toggleChoice(tag.form, visible_group, visible_group);
       }
     });
   });
