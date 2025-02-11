b1f6188d94d27a6b185db6cea53dfda05e15cdf8
Madelon D <maddohmen@hotmail.com>
2025-02-11 14:13:19 +0100

    Create dashboard automatically after creating organization

--- rocky/rocky/signals.py
+++ rocky/rocky/signals.py
@@ -1,5 +1,6 @@
 import datetime
 
+from crisis_room.management.commands.dashboards import get_or_create_default_dashboard
 from django.conf import settings
 from django.contrib.admin.models import LogEntry
 from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
@@ -115,6 +116,7 @@ def organization_pre_save(sender, instance, *args, **kwargs):
 @receiver(post_save, sender=Organization)
 def organization_post_save(sender, instance, *args, **kwargs):
     octopoes_client = _get_healthy_octopoes(instance.code)
+    get_or_create_default_dashboard(instance)
 
     try:
         valid_time = datetime.datetime.now(datetime.timezone.utc)
