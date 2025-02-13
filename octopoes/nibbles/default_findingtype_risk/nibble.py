33384cd55fc940355b7e88e234415414713fb4da
noamblitz <noamblitz@gmail.com>
2025-02-13 14:09:41 +0100

    Do not run default finding type nibble in cache

--- octopoes/nibbles/default_findingtype_risk/nibble.py
+++ octopoes/nibbles/default_findingtype_risk/nibble.py
@@ -1,4 +1,27 @@
 from nibbles.definitions import NibbleDefinition, NibbleParameter
+from octopoes.models import Reference
 from octopoes.models.ooi.findings import FindingType
 
-NIBBLE = NibbleDefinition(id="default-findingtype-risk", signature=[NibbleParameter(object_type=FindingType)])
+
+def query(targets: list[Reference | None]) -> str:
+    return f"""
+                {{
+                    :query {{
+                        :find[(pull ?findingtype[*])]
+                        :where
+                        [
+                            [?findingtype :KATFindingType/primary_key "{targets[0]}"]
+                        ]
+                    }}
+                }}
+    """
+
+
+NIBBLE = NibbleDefinition(
+    id="default-findingtype-risk",
+    signature=[
+        NibbleParameter(object_type=FindingType, parser="[*][?object_type == 'FindingType'][]"),
+        NibbleParameter(object_type=int, parser="[*][?object_type == 'KATFindingType'][] | [length(@)]"),
+    ],
+    query=query,
+)
