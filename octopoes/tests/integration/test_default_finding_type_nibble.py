d58165fb6eac319bb0441f48769a72b6be223e53
noamblitz <noamblitz@gmail.com>
2025-02-19 12:03:23 +0100

    failing resetting pending finding type nibble

---
+++ octopoes/tests/integration/test_default_finding_type_nibble.py
@@ -0,0 +1,108 @@
+import os
+from datetime import datetime
+from unittest.mock import Mock
+
+import pytest
+from nibbles.default_findingtype_risk.nibble import NIBBLE as default_findingtype_risk_nibble
+from nibbles.missing_spf.nibble import NIBBLE as missing_spf_nibble
+from nibbles.runner import NibblesRunner
+
+from octopoes.core.service import OctopoesService
+from octopoes.models.ooi.dns.zone import Hostname
+from octopoes.models.ooi.findings import Finding, KATFindingType
+from octopoes.models.ooi.network import Network
+
+if os.environ.get("CI") != "1":
+    pytest.skip("Needs XTDB multinode container.", allow_module_level=True)
+
+STATIC_IP = ".".join((4 * "1 ").split())
+
+
+def test_default_findingtype_risk_simple(
+    xtdb_octopoes_service: OctopoesService, event_manager: Mock, valid_time: datetime
+):
+    nibbler = NibblesRunner(
+        xtdb_octopoes_service.ooi_repository,
+        xtdb_octopoes_service.origin_repository,
+        xtdb_octopoes_service.nibbler.nibble_repository,
+    )
+    xtdb_octopoes_service.nibbler.disable()
+    nibbler.nibbles = {default_findingtype_risk_nibble.id: default_findingtype_risk_nibble}
+
+    network = Network(name="internet")
+    xtdb_octopoes_service.ooi_repository.save(network, valid_time)
+
+    hostname = Hostname(name="example.com", network=network.reference)
+    xtdb_octopoes_service.ooi_repository.save(hostname, valid_time)
+
+    finding_type = KATFindingType(id="KAT-DUMMY-FINDING")
+    xtdb_octopoes_service.ooi_repository.save(finding_type, valid_time)
+
+    finding = Finding(ooi=hostname.reference, finding_type=finding_type.reference)
+    xtdb_octopoes_service.ooi_repository.save(finding, valid_time)
+
+    event_manager.complete_process_events(xtdb_octopoes_service)
+    xtdb_finding_type = xtdb_octopoes_service.ooi_repository.get(finding_type.reference, valid_time)
+
+    assert xtdb_finding_type.risk_score is None
+
+    result = nibbler.infer([xtdb_finding_type], valid_time)
+    event_manager.complete_process_events(xtdb_octopoes_service)
+
+    assert len(result) == 1
+    assert list(result.keys())[0].risk_score == 0.0
+
+
+def test_default_findingtype_risk_should_not_go_back_to_pending(
+    xtdb_octopoes_service: OctopoesService, event_manager: Mock, valid_time: datetime
+):
+    nibbler = NibblesRunner(
+        xtdb_octopoes_service.ooi_repository,
+        xtdb_octopoes_service.origin_repository,
+        xtdb_octopoes_service.nibbler.nibble_repository,
+    )
+    xtdb_octopoes_service.nibbler.disable()
+    nibbler.nibbles = {
+        default_findingtype_risk_nibble.id: default_findingtype_risk_nibble,
+        missing_spf_nibble.id: missing_spf_nibble,
+    }
+
+    network = Network(name="internet")
+    xtdb_octopoes_service.ooi_repository.save(network, valid_time)
+
+    hostname = Hostname(name="example.com", network=network.reference)
+    xtdb_octopoes_service.ooi_repository.save(hostname, valid_time)
+
+    event_manager.complete_process_events(xtdb_octopoes_service)
+    xtdb_hostname = xtdb_octopoes_service.ooi_repository.get(hostname.reference, valid_time)
+
+    result = nibbler.infer([xtdb_hostname], valid_time)
+    event_manager.complete_process_events(xtdb_octopoes_service)
+
+    assert len(result) == 2
+
+    # default risk score should be given by default_findingtype_risk_nibble
+    assert list(result.keys())[1].risk_score == 0
+
+    # simulate a change in the risk score by a boefje
+    xtdb_finding_type = xtdb_octopoes_service.ooi_repository.get("KATFindingType|KAT-NO-SPF", valid_time)
+    xtdb_finding_type.risk_score = 5.0
+    xtdb_octopoes_service.ooi_repository.save(xtdb_finding_type, valid_time)
+    event_manager.complete_process_events(xtdb_octopoes_service)
+
+    # check that save is successful
+    xtdb_finding_type = xtdb_octopoes_service.ooi_repository.get("KATFindingType|KAT-NO-SPF", valid_time)
+    assert xtdb_finding_type.risk_score == 5.0
+
+    # another hostname is made with same finding type
+    hostname2 = Hostname(name="example2.com", network=network.reference)
+    xtdb_octopoes_service.ooi_repository.save(hostname2, valid_time)
+
+    event_manager.complete_process_events(xtdb_octopoes_service)
+    xtdb_hostname2 = xtdb_octopoes_service.ooi_repository.get(hostname2.reference, valid_time)
+
+    result = nibbler.infer([xtdb_hostname2], valid_time)
+    event_manager.complete_process_events(xtdb_octopoes_service)
+
+    # make sure that the risk score is not reset to 0
+    assert list(result.keys())[1].risk_score == 5.0
