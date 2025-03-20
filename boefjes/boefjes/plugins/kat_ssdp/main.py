9dc7795cc5e6a8d0cfc1129d25b438a965f63398
Soufyan Abdellati <abdellatis149@gmail.com>
2025-03-20 09:46:59 +0100

    Fixed import location since #3621 got implemented

--- boefjes/boefjes/plugins/kat_ssdp/main.py
+++ boefjes/boefjes/plugins/kat_ssdp/main.py
@@ -1,13 +1,13 @@
 import json
 from os import getenv
 
+from ssdpy import SSDPClient
+
 SEARCHTARGET_DEFAULT = "ssdp:all"
 TIMEOUT_DEFAULT = 10
 
 
 def run_ssdp(search_targets: str, timeout: int) -> list[dict[str, str]]:
-    from ssdpy import SSDPClient
-
     client = SSDPClient()
     return client.m_search(st=search_targets, mx=timeout)
 
