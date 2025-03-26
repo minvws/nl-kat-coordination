eef401ea90b594f12cf166baa7b6e8b62a2bde6d
noamblitz <noamblitz@gmail.com>
2025-03-26 17:40:20 +0100

    fix test

--- octopoes/tests/integration/test_ooi_deletion.py
+++ octopoes/tests/integration/test_ooi_deletion.py
@@ -229,7 +229,7 @@ def test_deletion_events_after_nxdomain(
     event_manager.complete_process_events(xtdb_octopoes_service)
 
     assert len(list(filter(lambda x: x.operation_type.value == "delete", event_manager.queue))) == 0
-    assert xtdb_octopoes_service.ooi_repository.list_oois({OOI}, valid_time).count == 9
+    assert xtdb_octopoes_service.ooi_repository.list_oois({OOI}, valid_time).count == 10
 
     nxd = NXDOMAIN(hostname=hostname.reference)
     xtdb_octopoes_service.ooi_repository.save(nxd, valid_time)
