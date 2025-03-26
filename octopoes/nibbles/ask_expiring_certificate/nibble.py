68393243a8e694dbdd2e6d1ea25e473ec8d141f4
noamblitz <noamblitz@gmail.com>
2025-03-26 16:22:01 +0100

    add question

---
+++ octopoes/nibbles/ask_expiring_certificate/nibble.py
@@ -0,0 +1,4 @@
+from nibbles.definitions import NibbleDefinition, NibbleParameter
+from octopoes.models.ooi.network import Network
+
+NIBBLE = NibbleDefinition(id="ask-expiring-certificate", signature=[NibbleParameter(object_type=Network)])
