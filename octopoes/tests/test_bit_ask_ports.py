e2c2a4d0142f1f8fdb9c1b9f8882c0a17714d8d2
noamblitz <noamblitz@gmail.com>
2025-04-02 16:24:49 +0200

    fix asking nibble test

--- octopoes/tests/test_bit_ask_ports.py
+++ octopoes/tests/test_bit_ask_ports.py
@@ -1,11 +1,11 @@
-from nibbles.ask_port_specification.ask_port_specification import nibble
+from nibbles.ask_network_questions.ask_network_questions import nibble
 
 from octopoes.models.ooi.network import Network
 from octopoes.models.ooi.question import Question
 
 
-def test_port_classification_tcp_80():
+def test_ask_network_questions():
     results = list(nibble(Network(name="test1")))
 
-    assert len(results) == 1
+    assert len(results) == 3
     assert isinstance(results[0], Question)
