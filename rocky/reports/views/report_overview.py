67d5ae0bb6c9f97c46ab6c9449bf21a33aed813b
Rieven <rsnmartis@gmail.com>
2025-04-03 16:19:50 +0200

    Update rocky/reports/views/report_overview.py
    Co-authored-by: Jan Klopper <janklopper+underdark@gmail.com>

--- rocky/reports/views/report_overview.py
+++ rocky/reports/views/report_overview.py
@@ -103,7 +103,7 @@ class ScheduledReportsView(BreadcrumbsReportOverviewView, SchedulerView, ListVie
             self.delete_report_schedule(schedule_id)
             try:
                 self.octopoes_api_connector.delete(
-                    Reference.from_str(f"{recipe_pk}"), valid_time=datetime.now(timezone.utc)
+                    Reference.from_str(recipe_pk), valid_time=datetime.now(timezone.utc)
                 )
                 logger.info(
                     "Schedule and reportRecipe deleted", event_code="0800083", schedule_id=schedule_id, recipe=recipe_pk
