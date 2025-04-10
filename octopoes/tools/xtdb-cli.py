minvws/nl-kat-coordination

commit c6e3efa259f35522c1b1706539c7565960a2ebf1

Add a cli command to evict reports due to a bug when upgrading from v1.17.0 to 1.18.0 (#4169)

octopoes/tools/xtdb-cli.py
@@ -5,7 +5,8 @@ import json
 import logging
 
 import click
-from xtdb_client import XTDBClient
+
+from .xtdb_client import XTDBClient
 
 logger = logging.getLogger(__name__)
 
@@ -267,6 +268,22 @@ def slowest_queries(ctx: click.Context):
     client: XTDBClient = ctx.obj["client"]
 
     click.echo(json.dumps(client.slowest_queries()))
+
+
+@cli.command(help="Deletes all reports with an evict.")
+@click.pass_context
+def evict_all_reports(ctx: click.Context):
+    client: XTDBClient = ctx.obj["client"]
+
+    reports = client.query('{:query {:find [ ?var ] :where [[?var :object_type "Report" ]]}}')
+
+    transactions = []
+
+    for report in reports:
+        transactions.append(("evict", report[0], datetime.datetime.now(tz=datetime.timezone.utc).isoformat()))
+
+    client.submit_tx(transactions)
+    click.echo("Evicted reports")
 
 
 if __name__ == "__main__":

boefjes/tools/upgrade_v1_17_0.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""Migration script for v1.16.0 due to a bug in the garbage collection. To be removed in later versions.
+"""Migration script for v1.17.0 due to a bug in the garbage collection. To be removed in later versions.
 https://github.com/minvws/nl-kat-coordination/issues/2875"""
 
 import json

commit f783973ee3bd253719ba89e4a42e397d176c4de1

Fix a regression introduced in #4169 blocking the ability of of `octopoes/tools/xtdb-cli.py` to be called (#4299)
Co-authored-by: Donny Peeters <46660228+Donnype@users.noreply.github.com>
Co-authored-by: Stephanie <stephanie@serenitea.nl>

octopoes/tools/xtdb-cli.py
@@ -5,8 +5,7 @@ import json
 import logging
 
 import click
-
-from .xtdb_client import XTDBClient
+from xtdb_client import XTDBClient
 
 logger = logging.getLogger(__name__)
 