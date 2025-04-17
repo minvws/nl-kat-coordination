5dbd3667ec6107050389dec6304cef7a566e25eb
Jan Klopper <janklopper+underdark@gmail.com>
2025-04-17 12:59:16 +0200

    Update ooi_detail.py

--- rocky/rocky/views/ooi_detail.py
+++ rocky/rocky/views/ooi_detail.py
@@ -37,7 +37,7 @@ class OOIDetailView(BaseOOIDetailView, OOIRelatedObjectManager, OOIFindingManage
             return self.indemnification_error()
         else:
             try:
-                clearance_level = int(self.request.POST.get("level"), "")
+                clearance_level = int(self.request.POST.get("level", ""))
                 self.raise_clearance_level(self.ooi, clearance_level)  # returns appropriate messages
             except (ValueError, TypeError):
                 messages.error(self.request, _("There was an error setting this clearance level, not a number."))
