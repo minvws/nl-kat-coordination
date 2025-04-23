80d2080f6498f6cb6b955de65ac861cea705fbe7
Soufyan Abdellati <abdellatis149@gmail.com>
2025-04-23 11:16:10 +0200

    Make abuseipdb boefje work

--- boefjes/boefjes/plugins/kat_abuseipdb/main.py
+++ boefjes/boefjes/plugins/kat_abuseipdb/main.py
@@ -9,17 +9,28 @@ REQUEST_TIMEOUT = 60
 
 
 def run(boefje_meta: BoefjeMeta) -> list[tuple[set, bytes | str]]:
-    """Make request to InternetDB."""
-    ip = boefje_meta.arguments["input"]["address"]
-    ipinfo = ip_address(ip)
-    if ipinfo.is_private or ipinfo.is_multicast or ipinfo.is_reserved:
-        return [({"info/boefje"}, "Skipping private/multicast/reserved IP address")]
+    """Make request to AbuseIPDB."""
 
-    url = f"https://api.abuseipdb.com/api/v2/check?ipAddress={ip_address}"
-    headers = {"Key": getenv("ABUSEIPDB_API", ""), "Accept": "application/json"}
+    if boefje_meta.arguments["input"]["network"]["name"] != "internet":
+        return [({"info/boefje"}, "Skipping non-internet network")]
 
-    response = httpx.get(url, headers=headers, timeout=int(getenv("REQUEST_TIMEOUT", REQUEST_TIMEOUT)))
-    if response.status_code != httpx.codes.NOT_FOUND:
-        response.raise_for_status()
+    ip_raw = str(boefje_meta.arguments["input"]["address"])
+    ip = ip_address(ip_raw)
+
+    # Skip private, multicast, or reserved IP addresses
+    if ip.is_private or ip.is_multicast or ip.is_reserved:
+        return [({"info/boefje"}, "Skipping private, multicast, or reserved IP address")]
+
+    api_key = getenv("ABUSEIPDB_API", "")
+    if api_key == "":
+        return [({"error/boefje"}, "No AbuseIPDB API key provided")]
+
+    response = httpx.get(
+        "https://api.abuseipdb.com/api/v2/check",
+        params={"ipAddress": ip_raw, "verbose": ""},
+        headers={"Key": api_key, "Accept": "application/json"},
+        timeout=int(getenv("REQUEST_TIMEOUT", REQUEST_TIMEOUT)),
+    )
+    response.raise_for_status()
 
     return [(set(), response.content)]
