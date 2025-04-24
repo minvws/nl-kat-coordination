5556bff9747026482694b9ee54443fa4b6ed1218
Jan Klopper <janklopper+underdark@gmail.com>
2025-04-24 12:27:59 +0200

    Update ooi_detail.py

--- rocky/rocky/views/ooi_detail.py
+++ rocky/rocky/views/ooi_detail.py
@@ -38,7 +38,7 @@ class OOIDetailView(BaseOOIDetailView, OOIRelatedObjectManager, OOIFindingManage
             return
         try:
             clearance_level = int(self.request.POST["level"])
-            self.raise_clearance_level(self.ooi, clearance_level)  # returns appropriate messages
+            self.can_raise_clearance_level(self.ooi, clearance_level)  # returns appropriate messages
         except (ValueError, KeyError):
             messages.error(
                 self.request, _("Cannot set clearance level. It must be provided and must be a valid number.")
