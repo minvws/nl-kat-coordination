6aead89c43783e86d3e08387c88dcd5df66a99b9
Soufyan Abdellati <abdellatis149@gmail.com>
2025-04-25 10:29:54 +0200

    add MVP of abuseipdb boefje

--- boefjes/boefjes/plugins/kat_abuseipdb/normalize.py
+++ boefjes/boefjes/plugins/kat_abuseipdb/normalize.py
@@ -26,36 +26,26 @@ def run(input_ooi: dict, raw: bytes) -> Iterable[OOI]:
         data: dict[str, Any] = result["data"]
         reportcount = int(data.get("totalReports", 0))
         if reportcount > 0:
-            confidence = str(data.get("abuseConfidenceScore", "Unknown"))
-            reportdate = str(data.get("lastReportedAt", "Unknown"))
-            ft = KATFindingType(id="KAT-ABUSE-REPORTS-DETECTED")
-            finding = Finding(
-                finding_type=ft.reference,
-                ooi=input_ooi_reference,
-                description=(
-                    f"IP {input_ooi_reference.human_readable} is listed {reportcount} times "
-                    f"for abuse at AbuseIPDB.com, last reported at: {reportdate} with a "
-                    f"confidence of: {confidence}."
-                ),
-            )
-            yield ft
-            yield finding
-
-            # Get more detailed findings using the report categories
-            # Each report has a list of categories that the report reports see @ammar92
+            found_risks: set[str] = set()
+            # Get the categories of reports
             for report in data.get("reports", []):
                 for category in report.get("categories", []):
                     report_info = report_categories.get(str(category), None)
                     if report_info:
-                        ft = KATFindingType(
-                            id="AbuseIPDB-REPORT",
-                            recommendation="Make sure to check out https://www.abuseipdb.com/categories",
-                            source="https://www.abuseipdb.com",
-                        )
-                        finding = Finding(
-                            finding_type=ft.reference,
-                            ooi=input_ooi_reference,
-                            description=f"{report_info['title']} — {report_info['description']}",
-                        )
-                        yield ft
-                        yield finding
+                        found_risks.add(report_info["title"])
+
+            confidence = str(data.get("abuseConfidenceScore", "Unknown"))
+            reportdate = str(data.get("lastReportedAt", "Unknown"))
+            ft = KATFindingType(id="ABUSEIPDB-REPORT")
+            finding = Finding(
+                finding_type=ft.reference,
+                ooi=input_ooi_reference,
+                proof=f"https://www.abuseipdb.com/check/{data['ipAddress']}",
+                description=(
+                    f"IP {input_ooi_reference.human_readable} is listed {reportcount} times "
+                    f"for abuse at AbuseIPDB.com, last reported at: {reportdate} with a "
+                    f"confidence of: {confidence}. Risk categories: {','.join(found_risks)}."
+                ),
+            )
+            yield ft
+            yield finding
