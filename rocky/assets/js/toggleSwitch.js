bbab0e44849be083fc078e17badbe9c7c54b38ef
Jan Klopper <janklopper+underdark@gmail.com>
2025-04-29 14:56:53 +0200

    Update toggleSwitch.js

--- rocky/assets/js/toggleSwitch.js
+++ rocky/assets/js/toggleSwitch.js
@@ -1,59 +1,136 @@
-import { onDomReady } from "../js/imports/utils.js";
+/*
+ *   This content is licensed according to the W3C Software License at
+ *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
+ *
+ *   File:   tabs-manual.js
+ *
+ *   Desc:   Tablist widget that implements ARIA Authoring Practices
+ */
 
-onDomReady(initToggleSwitches);
+'use strict';
 
-function initToggleSwitches() {
-  // Switches get stitches ;-)
-  // Grab all toggle switches
-  let toggle_switches = document.querySelectorAll(".toggle-switch");
+class TabsManual {
+  constructor(groupNode) {
+    this.tablistNode = groupNode;
 
-  for (let i = 0; i < toggle_switches.length; i++) {
-    initButtonsAndContentBlocks(toggle_switches[i]);
+    this.tabs = [];
+
+    this.firstTab = null;
+    this.lastTab = null;
+
+    this.tabs = Array.from(this.tablistNode.querySelectorAll('[role=tab]'));
+    this.tabpanels = [];
+
+    for (var i = 0; i < this.tabs.length; i += 1) {
+      var tab = this.tabs[i];
+      var tabpanel = document.getElementById(tab.getAttribute('aria-controls'));
+
+      tab.tabIndex = -1;
+      tab.setAttribute('aria-selected', 'false');
+      this.tabpanels.push(tabpanel);
+
+      tab.addEventListener('keydown', this.onKeydown.bind(this));
+      tab.addEventListener('click', this.onClick.bind(this));
+
+      if (!this.firstTab) {
+        this.firstTab = tab;
+      }
+      this.lastTab = tab;
+    }
+
+    this.setSelectedTab(this.firstTab);
   }
-}
 
-function initButtonsAndContentBlocks(toggle_switch) {
-  // Get all the toggle switch options
-  let options = toggle_switch.querySelectorAll(".toggle-switch-button");
-
-  for (let i = 0; i < options.length; i++) {
-    let option = options[i];
-
-    // Hide all elements linked to toggle switch options.
-    document
-      .getElementById(option.getAttribute("data-target-id"))
-      .classList.add("hidden");
-
-    // Add click listener to switch options.
-    option.addEventListener("click", (event) => {
-      toggle(event.target, options);
-    });
-
-    // Show option that on init has "aria-current".
-    if (option.parentElement.hasAttribute("aria-current", "true")) {
-      document
-      .getElementById(option.getAttribute("data-target-id"))
-      .classList.remove("hidden");
+  setSelectedTab(currentTab) {
+    for (var i = 0; i < this.tabs.length; i += 1) {
+      var tab = this.tabs[i];
+      if (currentTab === tab) {
+        tab.setAttribute('aria-selected', 'true');
+        tab.removeAttribute('tabindex');
+        this.tabpanels[i].classList.remove('is-hidden');
+      } else {
+        tab.setAttribute('aria-selected', 'false');
+        tab.tabIndex = -1;
+        this.tabpanels[i].classList.add('is-hidden');
+      }
     }
   }
-}
 
-function toggle(target, options) {
-  let target_li = target.closest("li");
+  moveFocusToTab(currentTab) {
+    currentTab.focus();
+  }
 
-  // Check if target isn't already the active one.
-  if (!target_li.hasAttribute("aria-current", "true")) {
-    for (let i = 0; i < options.length; i++) {
-      // Toggle all options to "non active" state.
-      options[i].closest("li").removeAttribute("aria-current", "false");
-      document
-        .getElementById(options[i].getAttribute("data-target-id"))
-        .classList.add("hidden");
+  moveFocusToPreviousTab(currentTab) {
+    var index;
+
+    if (currentTab === this.firstTab) {
+      this.moveFocusToTab(this.lastTab);
+    } else {
+      index = this.tabs.indexOf(currentTab);
+      this.moveFocusToTab(this.tabs[index - 1]);
     }
-    // Toggle selected option (target) to active state.
-    target_li.setAttribute("aria-current", "true");
-    document
-      .getElementById(target.getAttribute("data-target-id"))
-      .classList.remove("hidden");
+  }
+
+  moveFocusToNextTab(currentTab) {
+    var index;
+
+    if (currentTab === this.lastTab) {
+      this.moveFocusToTab(this.firstTab);
+    } else {
+      index = this.tabs.indexOf(currentTab);
+      this.moveFocusToTab(this.tabs[index + 1]);
+    }
+  }
+
+  /* EVENT HANDLERS */
+
+  onKeydown(event) {
+    var tgt = event.currentTarget,
+      flag = false;
+
+    switch (event.key) {
+      case 'ArrowLeft':
+        this.moveFocusToPreviousTab(tgt);
+        flag = true;
+        break;
+
+      case 'ArrowRight':
+        this.moveFocusToNextTab(tgt);
+        flag = true;
+        break;
+
+      case 'Home':
+        this.moveFocusToTab(this.firstTab);
+        flag = true;
+        break;
+
+      case 'End':
+        this.moveFocusToTab(this.lastTab);
+        flag = true;
+        break;
+
+      default:
+        break;
+    }
+
+    if (flag) {
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  }
+
+  // Since this example uses buttons for the tabs, the click onr also is activated
+  // with the space and enter keys
+  onClick(event) {
+    this.setSelectedTab(event.currentTarget);
   }
 }
+
+// Initialize tablist
+
+window.addEventListener('load', function () {
+  var tablists = document.querySelectorAll('[role=tablist].manual');
+  for (var i = 0; i < tablists.length; i++) {
+    new TabsManual(tablists[i]);
+  }
+});
