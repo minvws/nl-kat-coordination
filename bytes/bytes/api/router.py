498e2c83e73cc144a3760785ee721a4332b3c392
Jan Klopper <janklopper+underdark@gmail.com>
2025-05-02 22:53:31 +0200

    Update router.py

--- bytes/bytes/api/router.py
+++ bytes/bytes/api/router.py
@@ -184,7 +184,7 @@ def create_raw(
                 raw_ids[raw.name] = raw_id
 
             all_parsed_mime_types.append(parsed_mime_types)
-            if raw.content: # dont publish rawFile evnts for empty raw flies, no useful data to normalize present.
+            if raw.content: # dont publish rawFile events for empty raw flies, no useful data to normalize present.
                 event = RawFileReceived(
                     organization=meta.organization,
                     raw_data=RawDataMeta(id=raw_id, boefje_meta=raw_data.boefje_meta, mime_types=raw_data.mime_types),
