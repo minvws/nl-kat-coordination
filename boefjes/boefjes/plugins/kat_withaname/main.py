c0f2c67a30c3bf5adb03eeb12c960da09115f960
Soufyan Abdellati <abdellatis149@gmail.com>
2025-05-09 13:29:16 +0200

    Check if OOI is an IPaddress before checking for privacy of ip

--- boefjes/boefjes/plugins/kat_withaname/main.py
+++ boefjes/boefjes/plugins/kat_withaname/main.py
@@ -8,9 +8,11 @@ REQUEST_TIMEOUT = 60
 
 def run(boefje_meta: dict) -> list[tuple[set, bytes | str]]:
     """Make request to witha.name."""
-    ip = ip_address(boefje_meta["arguments"]["input"]["address"])
-    if ip.is_private or ip.is_multicast or ip.is_reserved:
-        return [({"info/boefje"}, "Skipping private/multicast/reserved IP address")]
+
+    if boefje_meta["arguments"]["input"]["object_type"] in ["IPAddressV4", "IPAddressV6"]:
+        ip = ip_address(boefje_meta["arguments"]["input"]["address"])
+        if ip.is_private or ip.is_multicast or ip.is_reserved:
+            return [({"info/boefje"}, "Skipping private/multicast/reserved IP address")]
 
     response = httpx.get("https://witha.name/data/last.json", timeout=int(getenv("REQUEST_TIMEOUT", REQUEST_TIMEOUT)))
 
