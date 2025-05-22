df77607875277705965d10749c5a0f14001dc0f9
JP Bruins Slot <jpbruinsslot@gmail.com>
2025-05-22 07:57:26 +0200

    Readd

--- mula/scheduler/schedulers/schedulers/boefje.py
+++ mula/scheduler/schedulers/schedulers/boefje.py
@@ -727,7 +727,7 @@ class BoefjeScheduler(Scheduler):
             enabled=True,
             with_duplicates=True,
         )
-        if len(configs) == 0:
+        if not configs:
             self.logger.debug(
                 "No configs found for boefje",
                 boefje_id=boefje_task.boefje.id,
