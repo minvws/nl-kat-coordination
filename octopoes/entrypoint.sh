0736867a60d26b337b67281ad8562b9595bdb9a1
Jan Klopper <janklopper+underdark@gmail.com>
2025-06-05 14:31:29 +0200

    Update entrypoint.sh

--- octopoes/entrypoint.sh
+++ octopoes/entrypoint.sh
@@ -6,10 +6,8 @@ shopt -s nocasematch
 
 if [ "$1" = "web" ]; then
     exec uvicorn octopoes.api.api:app --host 0.0.0.0 --port 80
-elif [ "$1" = "scanprofiles" ]; then
-    exec python -m octopoes.tasks.scanprofiles --loglevel=DEBUG
 elif [ "$1" = "worker" ]; then
-    exec celery -A octopoes.tasks.tasks worker --loglevel=INFO
+    exec python -m octopoes.tasks.scanprofiles --loglevel=NFO
 fi
 
 exec "$@"
