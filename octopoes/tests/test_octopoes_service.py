0926916fe7ecae5dc510d255c5d8007430b2ede4
Benny <mail2benny@gmail.com>
2025-06-12 07:56:57 +0200

    Mock origin repo

--- octopoes/tests/test_octopoes_service.py
+++ octopoes/tests/test_octopoes_service.py
@@ -100,6 +100,7 @@ def test_on_create_scan_profile(octopoes_service, new_data, old_data, bit_runner
     octopoes_service.ooi_repository.load_bulk.return_value = {}
     octopoes_service.nibbler = Mock()
     octopoes_service.nibbler.infer.return_value = {}
+    octopoes_service.origin_repository.list_nibblets_by_parameter.return_value = {}
 
     mock_oois = [Mock(reference="test1"), Mock(reference="test2")]
     bit_runner().run.return_value = mock_oois
