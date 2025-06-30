227b2227e266b2985357bafde2fbe5f33e1b16da
Donny Peeters <donny@bitestreams.com>
2025-06-30 17:33:39 +0200

    Consistency

--- rocky/export_migrations/0117.crisis_room.0006_rename_dashboarddata_dashboarditem.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "crisis_room_dashboarddata" RENAME TO "crisis_room_dashboarditem";
-ALTER TABLE "crisis_room_dashboarditem" RENAME COLUMN "query_from" TO "source";
-CREATE UNIQUE INDEX "unique_findings_dashboard_per_dashboard" ON "crisis_room_dashboarditem" ("dashboard_id") WHERE "findings_dashboard";
