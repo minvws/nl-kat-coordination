b381c4673e55f88a6e1e33413819923156abf743
Donny Peeters <donny@bitestreams.com>
2025-07-07 14:52:55 +0200

    Containerize pdio_subfinder

---
+++ boefjes/boefjes/plugins/kat_pdio_subfinder/main.py
@@ -0,0 +1,22 @@
+import logging
+import os
+import subprocess
+
+
+def run(boefje_meta: dict) -> list[tuple[set, bytes | str]]:
+    """Create image from docker and run subfinder with only active domains output."""
+    rate_limit = int(os.getenv("SUBFINDER_RATE_LIMIT", "0"))
+    input_ = boefje_meta["arguments"]["input"]
+    hostname = input_["name"]
+
+    logging.info("Using subfinder with rate limit %s", rate_limit)
+
+    return [
+        (
+            {"openkat/pdio-subfinder"},
+            subprocess.run(
+                ["/usr/local/bin/subfinder", "-silent", "-active", "-rate-limit", str(rate_limit), "-d", hostname],
+                capture_output=True,
+            ).stdout.decode(),
+        )
+    ]
