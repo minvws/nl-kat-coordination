a20e5c0cec68b0cd23d303ed065d1f0f3128fc8e
Ammar <ammar.abdulamir@gmail.com>
2025-07-08 15:09:38 +0200

    (Mula) Update Docker setup to use `uv` for running services and modify entrypoint script

--- mula/entrypoint.sh
+++ mula/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 shopt -s nocasematch
 
 if [ "$DATABASE_MIGRATION" = "1" ] || [[ $DATABASE_MIGRATION == "true" ]]; then
-    python -m alembic --config /app/scheduler/scheduler/storage/migrations/alembic.ini upgrade head
+    (cd /app/scheduler && uv run alembic --config scheduler/storage/migrations/alembic.ini upgrade head)
 fi
 
 exec "$@"
