ee2a98876f4c967a1ff8301f4d26d2020419ce2e
Ammar <ammar.abdulamir@gmail.com>
2025-07-09 15:25:29 +0200

    (Boefje) Refactor Docker setup to integrate `uv` for service execution and update entrypoint script

--- boefjes/entrypoint.sh
+++ boefjes/entrypoint.sh
@@ -5,19 +5,20 @@ set -e
 shopt -s nocasematch
 
 if [ "$1" = "boefje" ]; then
-    exec python -m boefjes boefje "${@:2}"
+    exec uv run --module boefjes boefje "${@:2}"
 elif [ "$1" = "normalizer" ]; then
-    exec python -m boefjes normalizer "${@:2}"
+    exec uv run --module boefjes normalizer "${@:2}"
 fi
 
-# The migrations and seed are for the KATalogus. They are not inside the if because this way
+# The migrations and seed are for the KATalogus. They are not inside the if clause because this way
 # they can also be run when overruling the default cmd
 if [ "$DATABASE_MIGRATION" = "1" ] || [[ $DATABASE_MIGRATION == "true" ]]; then
-    python -m alembic --config /app/boefjes/boefjes/alembic.ini upgrade head
+    echo Run the migrations for the boefjes database
+    (cd /app/boefjes && uv run alembic --config boefjes/alembic.ini upgrade head)
 fi
 
 if [ "$1" = "katalogus" ]; then
-    exec python -m uvicorn --host 0.0.0.0 boefjes.katalogus.root:app
+    exec uv run --module uvicorn --host 0.0.0.0 boefjes.katalogus.root:app
 fi
 
 exec "$@"
