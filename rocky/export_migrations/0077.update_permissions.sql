0cbc4d8a31ae0174876d43c5dc2f059161a8312a
Donny Peeters <donny@bitestreams.com>
2025-07-22 15:37:57 +0200

    Add more missing content type entries

--- rocky/export_migrations/0077.update_permissions.sql
+++ rocky/export_migrations/0077.update_permissions.sql
@@ -1,6 +1,9 @@
 INSERT INTO public.django_content_type (app_label, model) VALUES
   ('crisis_room', 'dashboarditem'),
-  ('crisis_room', 'dashboard')
+  ('crisis_room', 'dashboard'),
+  ('tools', 'organizationtag'),
+  ('account', 'katuser'),
+  ('account', 'authtoken')
 ON CONFLICT DO NOTHING;
 
 -- Note: this list was generated with the following query, dumping it here in case we need to dig it up again:
