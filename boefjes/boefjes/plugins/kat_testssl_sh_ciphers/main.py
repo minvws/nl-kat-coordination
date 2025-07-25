057e18328f95a221fe92a94d112f04561dbacca6
Donny Peeters <donny@bitestreams.com>
2025-07-25 14:44:06 +0200

    Handle multiple TLS capable services as well

--- boefjes/boefjes/plugins/kat_testssl_sh_ciphers/main.py
+++ boefjes/boefjes/plugins/kat_testssl_sh_ciphers/main.py
@@ -4,21 +4,30 @@ from ipaddress import ip_address
 from pathlib import Path
 
 
+TLS_CAPABLE_SERVICES = ("https", "ftps", "smtp", "smtps", "imaps", "pop3s", "ssh", "rpd")
+STARTTLS_CAPABLE_SERVICES = (
+    "ftp", "smtp", "lmtp", "pop3", "imap", "xmpp", "telnet", "ldap", "nntp", "postgres", "mysql"
+)
+
+
 def run(boefje_meta: dict) -> list[tuple[set, bytes | str]]:
     input_ = boefje_meta["arguments"]["input"]
     ip_port = input_["ip_port"]["port"]
     address = input_["ip_port"]["address"]["address"]
-    scheme = input_["service"]["name"]
+    servicename = input_["service"]["name"]
 
-    if scheme != "https":
-        return [({"info/boefje"}, "Skipping check due to non-TLS scheme")]
+    if servicename not in TLS_CAPABLE_SERVICES + STARTTLS_CAPABLE_SERVICES:
+        return [({"info/boefje"}, "Skipping check due to non-TLS/STARTTLS service")]
+
+    cmd = ["/usr/local/bin/testssl.sh"] + boefje_meta["arguments"]["oci_arguments"]
+
+    if servicename in STARTTLS_CAPABLE_SERVICES:
+        cmd.extend(["--starttls", servicename])
 
     if ip_address(address).version == 6:
-        cmd = (
-            ["/usr/local/bin/testssl.sh"] + boefje_meta["arguments"]["oci_arguments"] + ["-6", f"[{address}]:{ip_port}"]
-        )
+        cmd.extend(["-6", f"[{address}]:{ip_port}"])
     else:
-        cmd = ["/usr/local/bin/testssl.sh"] + boefje_meta["arguments"]["oci_arguments"] + [f"[{address}]:{ip_port}"]
+        cmd.append(f"[{address}]:{ip_port}")
 
     env = os.environ.copy()
 
