d676c2523ee0bf3f538c8f35f53e1785556abc67
Donny Peeters <donny@bitestreams.com>
2025-07-25 16:23:24 +0200

    Update view name

--- plugins/urls.py
+++ plugins/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path("<slug:pk>/", PluginDetailView.as_view(), name="plugin_detail"),
     path("<slug:plugin_id>/cover-image", PluginCoverImageView.as_view(), name="plugin_cover_image"),
     path("enabled-plugin", EnabledPluginView.as_view(), name="plugin_enabled"),
-    path("enabled-plugin/<slug:pk>", EnabledPluginUpdateView.as_view(), name="edit_plugin_enabled"),
+    path("enabled-plugin/<slug:pk>", EnabledPluginUpdateView.as_view(), name="edit_enabled_plugin"),
 ]
