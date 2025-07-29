1c251889b08ff508e1d99220f390a35824a4f189
Soufyan Abdellati <abdellatis149@gmail.com>
2025-07-29 11:57:00 +0200

    Fixed edge cases and made GH token required

--- boefjes/boefjes/plugins/kat_octoscan/boefje.Dockerfile
+++ boefjes/boefjes/plugins/kat_octoscan/boefje.Dockerfile
@@ -1,6 +1,6 @@
 FROM openkat/boefje-base:latest
 
-COPY --from=golang:1.13-alpine /usr/local/go/ /usr/local/go/
+COPY --from=golang:1.24 /usr/local/go/ /usr/local/go/
 
 ARG OCI_IMAGE=ghcr.io/minvws/openkat/octoscan:latest
 ENV OCI_IMAGE=$OCI_IMAGE
@@ -8,6 +8,9 @@ ENV OCI_IMAGE=$OCI_IMAGE
 USER root
 RUN apt-get update && apt-get install -y git
 
-RUN git clone https://github.com/sullo/nikto
+RUN git clone https://github.com/synacktiv/octoscan /octoscan && \
+    cd /octoscan && \
+    /usr/local/go/bin/go mod tidy && \
+    /usr/local/go/bin/go build
 
 COPY ./boefjes/plugins/kat_octoscan ./kat_octoscan
--- boefjes/boefjes/plugins/kat_octoscan/main.py
+++ boefjes/boefjes/plugins/kat_octoscan/main.py
@@ -1,43 +1,55 @@
 import os
 import subprocess
 
+REPO_INSTALLATION_PATH = "/data/repo"
+
 
 def run(boefje_meta: dict) -> list[tuple[set, bytes | str]]:
-    hostname = str(boefje_meta["arguments"]["input"]["netloc"])
-
     # Creating the URL to check
     input_ooi = boefje_meta["arguments"]["input"]
     port = f":{input_ooi['port']}" if input_ooi["port"] else ""
     url = f"{input_ooi['scheme']}://{input_ooi['netloc']['name']}{port}{input_ooi['path']}"
 
-    # TODO: Is it okay to use human_readable here?
     git_check_cmd = ["git", "ls-remote", url]
 
     # See if hostname is a git repository
-    subprocess.run(git_check_cmd, capture_output=True, env={"GIT_TERMINAL_PROMPT": "0"}).check_returncode()
+    git_check_result = subprocess.run(git_check_cmd, capture_output=True, env={"GIT_TERMINAL_PROMPT": "0"})
 
+    # Code 128 means that the repository does not exist or ls-remote has failed
+    # In that case, we return an empty list
+    if git_check_result.returncode == 128:
+        return [({"openkat/nmap-output"}, "[]")]
+    git_check_result.check_returncode()
+
+    org, repo = url.split("/")[-2:]
     download_cmd = [
-        "./octoscan",
+        "/octoscan/octoscan",
         "dl",
         "--default-branch",
         "--output-dir",
-        "/data/repo",
+        REPO_INSTALLATION_PATH,
         "--org",
-        "minvws",
+        org,
         "--repo",
-        "nl-kat-coordination",
+        repo,
+        "--token",
+        os.getenv("REPO_TOKEN", ""),
     ]
-
-    if "REPO_TOKEN" in os.environ:
-        download_cmd.extend(["--token", os.getenv("REPO_TOKEN", "")])
 
     # Download the repository's GitHub action workflows
     subprocess.run(download_cmd, capture_output=True).check_returncode()
 
-    scan_cmd = ["./octoscan", "scan", hostname, "--format", "json"]
+    scan_cmd = ["/octoscan/octoscan", "scan", REPO_INSTALLATION_PATH, "--format", "json"]
 
     scan_output = subprocess.run(scan_cmd, capture_output=True)
 
-    scan_output.check_returncode()
+    # Octoscan returns 0 when no problems are found, 1 when there is an error, and 2 when problems are found
+    if scan_output.returncode == 1:
+        raise Exception(
+            f"Return code: {scan_output.returncode}; "
+            f"Command: {scan_cmd}; "
+            f"STDERR: {scan_output.stderr.decode()}; "
+            f"STDIN: {scan_output.stdout.decode()}"
+        )
 
     return [({"openkat/nmap-output"}, scan_output.stdout.decode())]
