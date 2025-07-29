b75037f636d5bbba6328ea22cdcd147b58eaef34
Donny Peeters <46660228+Donnype@users.noreply.github.com>
2025-07-29 04:35:15 +0200

    Update main.py
    Co-authored-by: Jeroen Dekkers <jeroen@dekkers.ch>

--- boefjes/boefjes/plugins/kat_testssl_sh_ciphers/main.py
+++ boefjes/boefjes/plugins/kat_testssl_sh_ciphers/main.py
@@ -3,7 +3,13 @@ import subprocess
 from ipaddress import ip_address
 from pathlib import Path
 
-TLS_CAPABLE_SERVICES = ("https",)
+TLS_CAPABLE_SERVICES = (
+    "https",
+    "ftps",
+    "smtps",
+    "imaps",
+    "pops",
+)
 STARTTLS_CAPABLE_SERVICES = (
     "ftp",
     "smtp",
