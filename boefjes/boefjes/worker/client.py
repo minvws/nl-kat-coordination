9790746813df3a0afb65ed4120fec10c8b79f29e
Jeroen Dekkers <jeroen@dekkers.ch>
2025-08-01 00:29:15 +0200

    Consistent default value

--- boefjes/boefjes/worker/client.py
+++ boefjes/boefjes/worker/client.py
@@ -56,7 +56,7 @@ class BoefjeAPIClient(SchedulerClientInterface, BoefjeStorageInterface):
         response = self._session.patch(f"/api/v0/scheduler/tasks/{task_id}", json={"status": status.value})
         self._verify_response(response)
 
-    def get_task(self, task_id: uuid.UUID, hydrate: bool = False) -> Task:
+    def get_task(self, task_id: uuid.UUID, hydrate: bool = True) -> Task:
         response = self._session.get(f"/api/v0/scheduler/tasks/{task_id}")
         self._verify_response(response)
 
