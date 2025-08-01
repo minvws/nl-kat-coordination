44df0727f9e597564c581eefa4c9b25cdccf4681
ammar92 <ammar.abdulamir@gmail.com>
2025-08-01 12:48:58 +0200

    Apply suggestion from @dekkers
    Co-authored-by: Jeroen Dekkers <jeroen@dekkers.ch>

--- boefjes/boefjes/plugins/kat_ssl_scan/main.py
+++ boefjes/boefjes/plugins/kat_ssl_scan/main.py
@@ -24,7 +24,7 @@ def run(boefje_meta: dict) -> list[tuple[set, bytes | str]]:
 
     command = ["/usr/bin/sslscan", "--no-colour", "--show-sigs"]
     if servicename in STARTTLS_CAPABLE_SERVICES:
-        command.append(f"-starttls-{servicename}")
+        command.append(f"--starttls-{servicename}")
     elif servicename == "rpd":
         command.append("-rdp")
 
