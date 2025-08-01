f2727fbacd7bc63e30567789442edff307f5d1e9
Jan Klopper <janklopper+underdark@gmail.com>
2025-08-01 12:58:30 +0200

    Also use SSLScan on IPService objects (#4696)
    Signed-off-by: Jan Klopper <janklopper+underdark@gmail.com>
    Co-authored-by: Jeroen Dekkers <jeroen@dekkers.ch>
    Co-authored-by: ammar92 <ammar.abdulamir@gmail.com>

--- boefjes/boefjes/plugins/kat_ssl_scan/main.py
+++ boefjes/boefjes/plugins/kat_ssl_scan/main.py
@@ -1,15 +1,40 @@
 import subprocess
 
+TLS_CAPABLE_SERVICES = ("https", "ftps", "smtp", "smtps", "imaps", "pop3s", "ms-wbt-server")
+STARTTLS_CAPABLE_SERVICES = ("pop3", "ftp", "imap", "smtp", "mysql", "ldap", "xmpp")
+
 
 def run(boefje_meta: dict) -> list[tuple[set, bytes | str]]:
     input_ = boefje_meta["arguments"]["input"]
-    hostname = input_["hostname"]["name"]
+    hostname = None
+    if "hostname" in input_:
+        # we are dealing with a website
+        hostname = input_["hostname"]["name"]
+        ip = input_["ip_service"]["ip_port"]["address"]["address"]
+        port = input_["ip_service"]["ip_port"]["port"]
+        servicename = input_["ip_service"]["service"]["name"]
+    else:
+        # we are dealing with an IP-service
+        ip = input_["ip_port"]["address"]["address"]
+        port = input_["ip_port"]["port"]
+        servicename = input_["service"]["name"]
 
-    scheme = input_["ip_service"]["service"]["name"]
-    if scheme != "https":
-        return [({"info/boefje"}, "Skipping check due to non-TLS scheme")]
+    if servicename not in TLS_CAPABLE_SERVICES + STARTTLS_CAPABLE_SERVICES:
+        return [({"info/boefje"}, "Skipping check due to non-TLS/STARTTLS service")]
 
-    output = subprocess.run(["/usr/bin/sslscan", "--xml=-", hostname], capture_output=True)
+    command = ["/usr/bin/sslscan", "--no-colour", "--show-sigs"]
+    if servicename in STARTTLS_CAPABLE_SERVICES:
+        command.append(f"--starttls-{servicename}")
+    elif servicename == "ms-wbt-server":
+        command.append("--rdp")
+
+    if hostname:
+        command.extend(["--sni-name=", hostname])
+
+    target = f"[{ip}]:{port}"
+
+    command.extend(["--xml=-", target])
+    output = subprocess.run(command, capture_output=True)
     output.check_returncode()
 
     return [({"openkat/ssl-scan-output"}, output.stdout.decode())]
