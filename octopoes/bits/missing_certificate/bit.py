faa0ce3ded7000b9c3d0cbec24e0c4ae63ebbbd5
Jeroen Dekkers <jeroen@dekkers.ch>
2025-08-20 23:10:12 +0200

    Remove missing certificate bit

--- octopoes/bits/missing_certificate/bit.py
+++
@@ -1,6 +0,0 @@
-from bits.definitions import BitDefinition
-from octopoes.models.ooi.web import Website
-
-BIT = BitDefinition(
-    id="missing-certificate", consumes=Website, parameters=[], module="bits.missing_certificate.missing_certificate"
-)
