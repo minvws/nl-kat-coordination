afa594c4e1198fbd7f63eec9acad0d6cd97c7da4
Donny Peeters <donny@bitestreams.com>
2025-08-29 08:33:37 +0200

    Plugin delete

--- plugins/urls.py
+++ plugins/urls.py
@@ -7,12 +7,14 @@ from plugins.views import (
     PluginDetailView,
     PluginListView,
     PluginCreateView,
+    PluginDeleteView,
 )
 
 urlpatterns = [
     path("plugins/", PluginListView.as_view(), name="plugin_list"),
     path("plugins/add", PluginCreateView.as_view(), name="add_plugin"),
     path("plugins/<slug:pk>/", PluginDetailView.as_view(), name="plugin_detail"),
+    path("plugins/<slug:pk>/delete", PluginDeleteView.as_view(), name="delete_plugin"),
     path("plugins/<slug:plugin_id>/cover-image", PluginCoverImageView.as_view(), name="plugin_cover_image"),
     path("enabled-plugin/", EnabledPluginView.as_view(), name="plugin_enabled"),
     path("enabled-plugin/<slug:pk>/", EnabledPluginUpdateView.as_view(), name="edit_enabled_plugin"),
--- plugins/views.py
+++ plugins/views.py
@@ -7,7 +7,7 @@ from django.urls import reverse, reverse_lazy
 from django.utils.http import url_has_allowed_host_and_scheme
 from django.utils.translation import gettext_lazy as _
 from django.views import View
-from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
 
 from katalogus.worker.repository import get_local_repository
 from plugins.models import EnabledPlugin, Plugin
@@ -74,6 +74,21 @@ class PluginCreateView(CreateView):
     model = Plugin
     fields = ["plugin_id", "name", "description", "scan_level", "oci_image", "oci_arguments", "recurrences"]
     template_name = "plugin_form.html"
+
+    def form_invalid(self, form):
+        return redirect(reverse("plugin_list"))
+
+    def get_success_url(self, **kwargs):
+        redirect_url = self.get_form().data.get("current_url")
+
+        if redirect_url and url_has_allowed_host_and_scheme(redirect_url, allowed_hosts=None):
+            return redirect_url
+
+        return reverse_lazy("plugin_list")
+
+
+class PluginDeleteView(DeleteView):
+    model = Plugin
 
     def form_invalid(self, form):
         return redirect(reverse("plugin_list"))
