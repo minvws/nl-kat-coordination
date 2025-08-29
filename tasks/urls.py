9cf5b70cf3538f23f2422ddcffd6987ae3323888
Donny Peeters <donny@bitestreams.com>
2025-08-29 15:58:49 +0200

    Also be able to run disabled schedules once

--- tasks/urls.py
+++ tasks/urls.py
@@ -13,13 +13,13 @@ from tasks.views import (
 )
 
 urlpatterns = [
-    path("new-tasks", TaskListView.as_view(), name="new_task_list"),
+    path("new-tasks/", TaskListView.as_view(), name="new_task_list"),
     path("new-tasks/<slug:pk>/", TaskDetailView.as_view(), name="task_detail"),
-    path("new-tasks/<slug:task_id>/reschedule", TaskRescheduleView.as_view(), name="reschedule_task"),
+    path("new-tasks/<slug:task_id>/reschedule/", TaskRescheduleView.as_view(), name="reschedule_task"),
     path("schedule/", ScheduleListView.as_view(), name="schedule_list"),
-    path("schedule/add", ScheduleCreateView.as_view(), name="add_schedule"),
+    path("schedule/add/", ScheduleCreateView.as_view(), name="add_schedule"),
     path("schedule/<slug:pk>/", ScheduleDetailView.as_view(), name="schedule_detail"),
-    path("schedule/<slug:pk>/edit", ScheduleUpdateView.as_view(), name="edit_schedule"),
-    path("schedule/<slug:pk>/delete", ScheduleDeleteView.as_view(), name="delete_schedule"),
-    path("schedule/<slug:schedule_id>/run", ScheduleRunView.as_view(), name="run_schedule"),
+    path("schedule/<slug:pk>/edit/", ScheduleUpdateView.as_view(), name="edit_schedule"),
+    path("schedule/<slug:pk>/delete/", ScheduleDeleteView.as_view(), name="delete_schedule"),
+    path("schedule/<slug:schedule_id>/run/", ScheduleRunView.as_view(), name="run_schedule"),
 ]
