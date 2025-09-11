638ac30006a50e6a8deab4a4f898e31831a455f8
Donny Peeters <donny@bitestreams.com>
2025-09-11 15:08:35 +0200

    First working container integration test

---
+++ tests/integration/test_containers.py
@@ -0,0 +1,27 @@
+import pytest
+
+from files.models import File
+from plugins.models import Plugin
+from plugins.runner import PluginRunner
+
+
+@pytest.mark.django_db(transaction=True)
+def test_hello_world():
+    plugin = Plugin.objects.create(
+        name="testing plugins",
+        plugin_id="test",
+        oci_image="hello-world:linux",
+        oci_arguments=["/hello"],
+    )
+
+    hello_world = PluginRunner().run(plugin.plugin_id, None, output="-")
+    assert "Hello from Docker!" in hello_world
+
+    assert File.objects.count() == 0
+
+    new_out = PluginRunner().run(plugin.plugin_id, None)
+    assert "Upload completed. Server responded with: 201 Created" in new_out
+
+    assert File.objects.count() == 1
+    file = File.objects.first()
+    assert file.file.read().decode() == hello_world
