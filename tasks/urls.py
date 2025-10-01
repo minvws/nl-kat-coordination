1ea6c170bf43fc3efbc60b5b505f9870457a1524
Donny Peeters <donny@bitestreams.com>
2025-10-02 00:43:02 +0200

    Fix task urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -20,11 +20,11 @@ from tasks.views import (
 )
 
 urlpatterns = [
-    path("new-tasks/", TaskListView.as_view(), name="task_list"),
-    path("new-tasks/add/", TaskCreateView.as_view(), name="add_task"),
-    path("new-tasks/<slug:pk>/", TaskDetailView.as_view(), name="task_detail"),
-    path("new-tasks/<slug:task_id>/reschedule/", TaskRescheduleView.as_view(), name="reschedule_task"),
-    path("new-tasks/<slug:task_id>/cancel/", TaskCancelView.as_view(), name="cancel_task"),
+    path("tasks/", TaskListView.as_view(), name="task_list"),
+    path("tasks/add/", TaskCreateView.as_view(), name="add_task"),
+    path("tasks/<slug:pk>/", TaskDetailView.as_view(), name="task_detail"),
+    path("tasks/<slug:task_id>/reschedule/", TaskRescheduleView.as_view(), name="reschedule_task"),
+    path("tasks/<slug:task_id>/cancel/", TaskCancelView.as_view(), name="cancel_task"),
     path("schedule/", ScheduleListView.as_view(), name="schedule_list"),
     path("schedule/add/", ScheduleCreateView.as_view(), name="add_schedule"),
     path("schedule/<slug:pk>/", ScheduleDetailView.as_view(), name="schedule_detail"),
