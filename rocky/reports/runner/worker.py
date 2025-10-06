bb44df9efab623d1df63c17c8d35dccac7e03509
Jan Klopper <janklopper+underdark@gmail.com>
2025-10-06 10:44:00 +0200

    Dont crash on that have no PID, when reporting on stopped workers.
    We are seeing errors, and subsequent restart loops for missing workers. Instead of recreating the missing workers, we crash on the missing PID when trying to emit a warning.

--- rocky/reports/runner/worker.py
+++ rocky/reports/runner/worker.py
@@ -119,9 +119,14 @@ class SchedulerWorkerManager(WorkerManager):
             except ValueError:
                 closed = True  # worker is closed, so we create a new one
 
-            logger.warning(
-                "Worker[pid=%s, %s] not alive, creating new worker...", worker.pid, _format_exit_code(worker.exitcode)
-            )
+            try:
+                logger.warning(
+                    "Worker[pid=%s, %s] not alive, creating new worker...", worker.pid, _format_exit_code(worker.exitcode)
+                )
+            except ValueError:
+                logger.warning(
+                    "Worker[pid=Unknown, %s] not alive, creating new worker...", _format_exit_code(worker.exitcode)
+                )
 
             if not closed:  # Closed workers do not have a pid, so cleaning up would fail
                 self._cleanup_pending_worker_task(worker)
