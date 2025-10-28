210ee81655210fadcff4842b49e419d39b47722e
Donny Peeters <donny@bitestreams.com>
2025-10-28 23:06:10 +0100

    Fix unit test Fix make lang CI

--- tests/objects/test_objects.py
+++ tests/objects/test_objects.py
@@ -250,13 +250,7 @@ def test_to_dict(xtdb):
     sw = Software.objects.create(name="openssh")
     port.software.add(sw)
     port.save()
-    assert to_xtdb_dict(net) == {
-        "name": "internet",
-        "_id": net.id,
-        "declared": False,
-        "scan_level": None,
-        "organizations": [],
-    }
+    assert to_xtdb_dict(net) == {"name": "internet", "_id": net.id, "declared": False, "scan_level": None}
     assert to_xtdb_dict(host) == {
         "name": "test.com",
         "network_id": net.id,
@@ -264,9 +258,8 @@ def test_to_dict(xtdb):
         "root": True,
         "declared": False,
         "scan_level": None,
-        "organizations": [],
     }
-    assert to_xtdb_dict(sw) == {"_id": sw.id, "cpe": None, "name": "openssh", "ports": [port.id], "version": None}
+    assert to_xtdb_dict(sw) == {"_id": sw.id, "cpe": None, "name": "openssh", "version": None}
 
 
 def test_bulk_insert_hostnames(xtdb):
