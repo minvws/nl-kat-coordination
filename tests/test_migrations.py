58820bae11ae93f0cb6b889ec43ade9255ed0061
Donny Peeters <donny@bitestreams.com>
2025-10-29 00:10:38 +0100

    Fix migration check v1 new plugin build pipeline

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -8,7 +8,18 @@ pytestmark = pytest.mark.django_db
 
 def test_for_missing_migrations(xtdb):
     output = StringIO()
-    call_command("makemigrations", no_input=True, dry_run=True, stdout=output)
+    call_command(
+        "makemigrations",
+        "openkat",
+        "onboarding",
+        "tasks",
+        "files",
+        "plugins",
+        "reports",
+        no_input=True,
+        dry_run=True,
+        stdout=output,
+    )
     lines = output.getvalue().strip().splitlines()
 
-    assert lines[0] == "No changes detected"
+    assert lines[0].startswith("No changes detected")
